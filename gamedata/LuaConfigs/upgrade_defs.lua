a8851f064b9cec87c91a6eb82314eb95fd1372bf
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2008-06-17 04:29:50 +0000

    Adding upgrade style build tree for vehs, cut turret traverse rates for all units by half, added arty notification back in (guns over 87mm will appear on map as a blob for 5 seconds after firing). Changed AEC Mk III to AEC MK II with 6pdr (stats from ZiS-2, they're close), added placeholder jumbo.

---
+++ gamedata/LuaConfigs/upgrade_defs.lua
@@ -0,0 +1,124 @@
+--[[
+
+Syntax is like this:
+local upgradeDefs = {
+	unitname = <upgrades>,
+	unitname = <upgrades>,
+	...
+	unitname = <upgrades>
+}
+<upgrades>
+	{
+		<upgrade option 1>, -- first upgrade option
+		<upgrade option 2>, -- second upgrade option
+		...
+		<upgrade option n>  -- nth upgrade option
+	}
+
+<upgrade options>
+		{
+			into  = 'unitname', -- what it upgrades into, case sensitive
+			mcost = ###,        -- metal cost of upgrading. Optional, defaults to 0
+			ecost = ###,        -- energy cost of upgrading. Optional, defaults to 0
+			time  = ###,        -- seconds taken to upgrade. Optional, defaults to 0
+			
+				-- runs a function upon defined event. Optional, defaults to nothing
+				-- run when a unit finishes upgrading
+			onUpgrade = function(oldUnitID, newUnitID, upgradeUnit) <statments> end,
+				-- run when a unit starts upgrading, or stops stalling
+			onStart   = function(oldUnitID, newUnitID, upgradeUnit) <statments> end,
+				-- run when the Stop command is sent if upgrading
+			onStop    = function(oldUnitID, newUnitID, upgradeUnit) <statments> end,
+				-- run when a unit starts stalling (ie. no resources)
+			onStall   = function(oldUnitID, newUnitID, upgradeUnit) <statments> end,
+		}
+
+
+
+  Note: All unitnames are case sensitive!
+]]--
+
+	-- If the unit is a HQ, put this in the definition
+	--[[
+			onUpgrade = function(oldUnitID, newUnitID, _)
+				HQGuardTransfer(oldUnitID, newUnitID)
+			end
+	]]--
+	-- If the unit also has E-WEBs guarding it, put this below HQGuardTransfer
+	-- HQEWEBTransfer(oldUnitID, newUnitID)
+
+	
+--[[local function HQGuardTransfer(oldID, newID)
+	if Script.LuaRules.HQ_GUARD_IDUpdate ~= nil then
+		Script.LuaRules.HQ_GUARD_IDUpdate({oldID = oldID, newID = newID})
+	end
+end
+local function HQEWEBTransfer(oldID, newID)
+	if Script.LuaRules.HQ_EWEB_IDUpdate ~= nil then
+		Script.LuaRules.HQ_EWEB_IDUpdate({oldID = oldID, newID = newID})
+	end
+end]]--
+
+
+local upgradeDefs = {
+	rustankyard = {
+		{
+			into = 'rustankyard1',
+			mcost = 2500,
+			ecost = 0,
+			time = 30,
+			buttonname = 'Upgrade',
+		},
+	},
+	ustankyard = {
+		{
+			into = 'ustankyard1',
+			mcost = 2500,
+			ecost = 0,
+			time = 30,
+			buttonname = 'Upgrade',
+		},
+	},
+	gertankyard = {
+		{
+			into = 'gertankyard1',
+			mcost = 2500,
+			ecost = 0,
+			time = 30,
+			buttonname = 'Upgrade',
+		},
+	},
+	gertankyard1 = {
+		{
+			into = 'gertankyard2',
+			mcost = 2500,
+			ecost = 0,
+			time = 30,
+			buttonname = 'Upgrade',
+		}
+	},
+	gbrtankyard = {
+		{
+			into = 'gbrtankyard1',
+			mcost = 2500,
+			ecost = 0,
+			time = 30,
+			buttonname = 'Upgrade',
+		}
+	},
+}
+
+
+-------------------------------------------------
+-- Dont touch below here
+-------------------------------------------------
+
+for _, upgrades in pairs(upgradeDefs) do
+	for _, upgrade in ipairs(upgrades) do
+		if (upgrade.mcost == nil) then upgrade.mcost = 0 end
+		if (upgrade.ecost == nil) then upgrade.ecost = 0 end
+		if (upgrade.time == nil)  then upgrade.time  = 0 end
+	end
+end
+
+return upgradeDefs
