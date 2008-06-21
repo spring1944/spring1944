9fb8c7661b539d0ad7454fffd93d38673b3f7053
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2008-06-21 19:47:40 +0000

    just committing my work on the new inf script and the older saberdude nonsense that was on my laptop.

---
+++ scripts/infposes.h
@@ -0,0 +1,19 @@
+#define RIFLE_STANCE1 \
+	turn ruparm to x-axis <-60> now;\
+	turn ruparm to y-axis <0> now;\
+	turn ruparm to z-axis <0> now;\
+	turn rloarm to x-axis <-30> now;\
+	turn rloarm to y-axis <20> now;\
+	turn rloarm to z-axis <-50> now;\
+	turn luparm to x-axis <-10> now;\
+	turn luparm to y-axis <0> now;\
+	turn luparm to z-axis <0> now;\
+	turn lloarm to x-axis <-85>now;\
+	turn lloarm to y-axis <0> now;\
+	turn lloarm to z-axis <0> now;\
+	turn gun to x-axis <0.000000> now;\
+	turn gun to y-axis <0.000000> now;\
+	turn gun to z-axis <0.000000> now;\
+	turn head to y-axis <0.000000> now;\
+	turn head to x-axis <0.000000> now;\
+	turn head to z-axis <0.000000> now;
