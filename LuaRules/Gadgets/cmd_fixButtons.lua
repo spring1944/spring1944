aa8da702e49f097518ee57fe10044b34d9742c6a
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2008-06-25 03:21:13 +0000

    * Added functionality to cmd_salvage.lua to hide 'Cloak' button on infantry and 'On/Off' on artillery * Renamed to cmd_fixButtons.lua * May require tweaking!

--- LuaRules/Gadgets/cmd_fixButtons.lua
+++ LuaRules/Gadgets/cmd_fixButtons.lua
@@ -3,8 +3,8 @@
 
 function gadget:GetInfo()
   return {
-    name      = "Salvage",
-    desc      = "Replaces 'Reclaim' Command with 'Salvage'",
+    name      = "Button Manipulator",
+    desc      = "Replaces 'Reclaim' Command with 'Salvage', hides Cloak and On/Off",
     author    = "FLOZi",
     date      = "Jun 24, 2008",
     license   = "CC attribution-noncommerical 3.0 or later",
@@ -20,16 +20,31 @@ if (gadgetHandler:IsSyncedCode()) then
 
 local EditUnitCmdDesc = Spring.EditUnitCmdDesc
 local FindUnitCmdDesc = Spring.FindUnitCmdDesc
+local RemoveUnitCmdDesc = Spring.RemoveUnitCmdDesc
+
+local CMD_RECLAIM = CMD.RECLAIM
+local CMD_CLOAK		= CMD.CLOAK
+local CMD_ONOFF		= CMD.ONOFF
 
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
   local ud = UnitDefs[unitDefID]
   if (ud.speed > 0 and ud.canReclaim) then
-		local unitCmdDesc = FindUnitCmdDesc(unitID, CMD.RECLAIM)
-		if (unitCmdDesc) then 
-			EditUnitCmdDesc(unitID, unitCmdDesc, {name = "Salvage", tooltip = "Salvage: Salvage wrecks back into Command Points"}) 
+		local unitReclaimCmdDesc = FindUnitCmdDesc(unitID, CMD_RECLAIM)
+		if (unitReclaimCmdDesc) then 
+			EditUnitCmdDesc(unitID, unitReclaimCmdDesc, {name = "Salvage", tooltip = "Salvage: Salvage wrecks back into Command Points"}) 
 		end
-  end
+	elseif (ud.speed > 0 and ud.canCloak) then
+		local unitCloakCmdDesc = FindUnitCmdDesc(unitID, CMD_CLOAK)
+		if (unitCloakCmdDesc) then
+			RemoveUnitCmdDesc(unitID, unitCloakCmdDesc)
+		end
+	elseif (ud.jammerRadius) then
+		local unitOnOffCmdDesc = FindUnitCmdDesc(unitID, CMD_ONOFF)
+		if (unitOnOffCmdDesc) then
+			RemoveUnitCmdDesc(unitID, unitOnOffCmdDesc)
+		end
+	end
 end
 
 end
