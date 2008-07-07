cf59c58961e021232f494f82f1727542a12b0a43
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2008-07-07 21:31:39 +0000

    * Widgetised the resupply timer -- Won't be accurate if turned off and on again atm :P

--- LuaRules/Gadgets/game_logisticsSupply.lua
+++ LuaRules/Gadgets/game_logisticsSupply.lua
@@ -4,7 +4,7 @@ function gadget:GetInfo()
 		desc = "Adds supplies to a team on a fixed schedule",
 		author = "Nemo (B. Tyler)",
 		date = "2008-03-07",
-		license = "Public domain",
+		license = "CC-BY-NC",
 		layer = 1,
 		enabled = true
 	}
@@ -15,7 +15,6 @@ if (gadgetHandler:IsSyncedCode()) then
 --SYNCED
 
 local hq = {}
-local timeLeft = 0
 
 function gadget:GameFrame(t)
 	if (t % (5*30) < 0.1) then
@@ -27,15 +26,8 @@ function gadget:GameFrame(t)
 				--print("refillAmount gameframe:", refillAmount)
 				--print("arrivalGap gameframe:", arrivalGap)
 				Spring.AddTeamResource (teamID, 'e', refillAmount)
-				timeLeft = arrivalGap
 			end
 		end
-	end
-	if (t % 30 == 0) then
-		if timeLeft > 0 then
-			timeLeft = timeLeft - 1
-		end
-		SendToUnsynced("timeLeft", timeLeft)
 	end
 end
 
@@ -43,14 +35,14 @@ function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 	local u = unitID
 	local ud = UnitDefs[unitDefID]
 	if (ud.customParams.hq == '1') then
-	print("add HQ", ud.customParams.hq, u)
+	--print("add HQ", ud.customParams.hq, u)
 	hq[u] = {
       refillAmount = ud.customParams.refillamount,
 	  arrivalGap = ud.customParams.arrivalgap, 	
 	  teamID = Spring.GetUnitTeam(u)	  
     }
-	print("refillAmount spawn:", hq[u].refillAmount)
-	print("arrivalGap spawn:", hq[u].arrivalGap)
+	--print("refillAmount spawn:", hq[u].refillAmount)
+	--print("arrivalGap spawn:", hq[u].arrivalGap)
 	end
 end
 
@@ -60,41 +52,5 @@ function gadget:UnitDestroyed(u) -- you can omit unneeded arguments if they
 end
 
 else
-
 --UNSYNCED
---[[
-local floor = math.floor
-
-local uTimeLeft = 0
-local vsx, vsy = gadgetHandler:GetViewSizes()
-
--- the 'f' suffixes are fractions  (and can be nil)
-local color  = { 1.0, 1.0, 0.25 }
-local xposf  = 0.5 --0.99
-local xpos   = xposf * vsx
-local yposf  = 0.010 --0.048 --0.032
-local ypos   = yposf * vsy
-local sizef  = 0.03 --0.015
-local size   = sizef * vsy
-
-function gadget:DrawScreen()
-  gl.Color(color)
-	local minutes = floor(uTimeLeft / 60)
-	local seconds = uTimeLeft % 60
-	local timeString = string.format('Resupply in: %02i:%02i', minutes, seconds)
-
-	vsx, vsy = gl.GetViewSizes()
-	xpos   = xposf * vsx
-	ypos   = yposf * vsy
-	size   = sizef * vsy
-	gl.Text(timeString, xpos, ypos, size, "ocn")
-end
-
-function RecvFromSynced(...)
-	if arg[2] == "timeLeft" then
-		uTimeLeft = arg[3]
-	end
-end
-
---return false]]--
 end
---
+++ LuaUI/Widgets/gui_resupplyTimer.lua
@@ -0,0 +1,68 @@
+function widget:GetInfo()
+	return {
+		name = "1944 Resupply Timer",
+		desc = "Adds a countdown timer to the next logistics refill",
+		author = "Craig Lawrence",
+		date = "6th July 2008",
+		license = "CC-BY-NC",
+		layer = 1,
+		enabled = true
+	}
+end
+
+local vsx, vsy = widgetHandler:GetViewSizes()
+local floor = math.floor
+
+-- the 'f' suffixes are fractions  (and can be nil)
+local color  = { 1.0, 1.0, 0.25 }
+local xposf  = 0.5
+local xpos   = xposf * vsx
+local yposf  = 0.010
+local ypos   = yposf * vsy
+local sizef  = 0.03
+local size   = sizef * vsy
+
+local timeLeft = 0
+local arrivalGap = 300 -- default to 5 minutes
+
+function widget:GameFrame(t)
+	if t == 1 then
+		vsx, vsy = widgetHandler:GetViewSizes()
+		UpdateGeometry()
+	end
+	if (t % 30 == 0) then
+		if timeLeft > 0 then
+			timeLeft = timeLeft - 1
+		end
+	end
+	if (t % (arrivalGap*30) < 0.1) then
+		timeLeft = arrivalGap
+	end
+end
+
+function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+	local ud = UnitDefs[unitDefID]
+	if (ud.customParams.hq == '1') then
+		arrivalGap = tonumber(ud.customParams.arrivalgap)
+	end
+end
+
+function widget:DrawScreen()
+	local minutes = floor(timeLeft / 60)
+	local seconds = timeLeft % 60
+	local timeString = string.format('Resupply in: %02i:%02i', minutes, seconds)
+	gl.Color(color)
+	gl.Text(timeString, xpos, ypos, size, "ocn")
+end
+
+function widget:ViewResize(viewSizeX, viewSizeY)
+  vsx = viewSizeX
+  vsy = viewSizeY
+	UpdateGeometry()
+end
+
+function UpdateGeometry()
+	xpos = xposf * vsx
+	ypos = yposf * vsy
+	size = sizef * vsy
+end
