dc2a48b82c94a52f33ff47212245ea15880f101d
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2008-12-17 01:48:07 +0000

    * Possible fix for unit_squadSpawner.lua error --LuaRules::RunCallIn: error = 2, GameFrame, [string "LuaRules/Gadgets/unit_squadspawner.lua"]:77: attempt to index local 'states' (a nil value)

--- LuaRules/Gadgets/unit_squadSpawner.lua
+++ LuaRules/Gadgets/unit_squadSpawner.lua
@@ -74,11 +74,13 @@ if (gadgetHandler:IsSyncedCode()) then
 
 				for _,unit in ipairs(squad_members) do
 					local newUnitID = CreateUnit(unit.unitname,unit.x,unit.y,unit.z,unit.heading,unit.team)
-					if(states.movestate ~= nil) then
-						Spring.GiveOrderToUnit(newUnitID, CMD.FIRE_STATE, { states.firestate }, 0)
-						Spring.GiveOrderToUnit(newUnitID, CMD.MOVE_STATE, { states.movestate }, 0)
+					if (states) then
+						if(states.movestate ~= nil) then
+							Spring.GiveOrderToUnit(newUnitID, CMD.FIRE_STATE, { states.firestate }, 0)
+							Spring.GiveOrderToUnit(newUnitID, CMD.MOVE_STATE, { states.movestate }, 0)
+						end
+						table.insert(squad_units,newUnitID)
 					end
-					table.insert(squad_units,newUnitID)
 				end
 
 					-- If its a valid queue
