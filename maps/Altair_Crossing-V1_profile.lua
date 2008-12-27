7357a11266790ca530a102e888615edbdf6b1066
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2008-12-27 16:16:45 +0000

    * new map profile, Altair_Crossing-V1_profile.lua

---
+++ maps/Altair_Crossing-V1_profile.lua
@@ -0,0 +1,154 @@
+local resources = {
+{
+	x = 1984,
+	z = 80,
+	feature = nil
+},
+{
+	x = 64,
+	z = 128,
+	feature = nil
+},
+{
+	x = 2368,
+	z = 128,
+	feature = nil
+},
+{
+	x = 3088,
+	z = 128,
+	feature = nil
+},
+{
+	x = 1760,
+	z = 160,
+	feature = nil
+},
+{
+	x = 3920,
+	z = 160,
+	feature = nil
+},
+{
+	x = 912,
+	z = 256,
+	feature = nil
+},
+{
+	x = 3520,
+	z = 752,
+	feature = nil
+},
+{
+	x = 544,
+	z = 832,
+	feature = nil
+},
+{
+	x = 3712,
+	z = 880,
+	feature = nil
+},
+{
+	x = 336,
+	z = 944,
+	feature = nil
+},
+{
+	x = 1504,
+	z = 1536,
+	feature = nil
+},
+{
+	x = 336,
+	z = 1776,
+	feature = nil
+},
+{
+	x = 3792,
+	z = 1824,
+	feature = nil
+},
+{
+	x = 544,
+	z = 1904,
+	feature = nil
+},
+{
+	x = 336,
+	z = 2000,
+	feature = nil
+},
+{
+	x = 3632,
+	z = 2016,
+	feature = nil
+},
+{
+	x = 3856,
+	z = 2112,
+	feature = nil
+},
+{
+	x = 2368,
+	z = 2352,
+	feature = nil
+},
+{
+	x = 336,
+	z = 2992,
+	feature = nil
+},
+{
+	x = 3712,
+	z = 3072,
+	feature = nil
+},
+{
+	x = 560,
+	z = 3120,
+	feature = nil
+},
+{
+	x = 3536,
+	z = 3264,
+	feature = nil
+},
+{
+	x = 96,
+	z = 3968,
+	feature = nil
+},
+{
+	x = 3216,
+	z = 3968,
+	feature = nil
+},
+{
+	x = 3936,
+	z = 3968,
+	feature = nil
+},
+{
+	x = 864,
+	z = 4000,
+	feature = nil
+},
+{
+	x = 1568,
+	z = 4000,
+	feature = nil
+},
+{
+	x = 2352,
+	z = 4000,
+	feature = nil
+},
+{
+	x = 1888,
+	z = 4016,
+	feature = nil
+},
+}
+
+return resources
