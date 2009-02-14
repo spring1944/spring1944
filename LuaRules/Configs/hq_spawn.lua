54e70c8c555bcbb6162ffbb24839147deeb299f7
nemonobody <nemonobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-02-14 21:40:05 +0000

    Base spawning code.

---
+++ LuaRules/Configs/hq_spawn.lua
@@ -0,0 +1,55 @@
+local hqDefs = {
+	ushq = {
+			spread = 25,
+			units = {
+				"us_platoon_hq",
+				"usstorage",
+				"usstorage",
+				"rus61k_stationary",
+				"rus61k_stationary",
+				"rus61k_stationary",
+				"usgmctruck",
+				"ushqengineer",
+			},
+	},
+	
+	gbrhq = {
+			spread = 25,
+			units = {
+				"gbr_platoon_hq",
+				"gbrstorage",
+				"gbrstorage",
+				"rus61k_stationary",
+				"rus61k_stationary",
+				"rus61k_stationary",
+				"gbrhqengineer",
+			},
+	},
+	
+	gerhqbunker = {
+			spread = 25,
+			units = {
+				"ger_platoon_hq",
+				"gerstorage",
+				"gerstorage",
+				"gerflak38_stationary",
+				"gerflak38_stationary",
+				"gerflak38_stationary",
+				"gerhqengineer",
+				},
+	},
+	ruscommander = {
+			spread = 25,
+			units = {
+				"rus_platoon_rifle",
+				"russtorage",
+				"russtorage",
+				"ruszis5",
+				"rus61k_stationary",
+				"rus61k_stationary",
+				"rus61k_stationary",
+				"ruscommissar",
+			},
+		},
+}
+return hqDefs
---
+++ LuaRules/Gadgets/game_baseSpawn.lua
@@ -0,0 +1,68 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--  file: startpos_clear.lua
+--  brief: Removes any features around the  Commanders start positions
+--  author: Maelstrom
+--
+--  Copyright (C) 2007.
+--  Licensed under the terms of the Creative Commons Attribution-Noncommercial 3.0 Unported
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function gadget:GetInfo()
+	return {
+		name      = "Base Spawner",
+		desc      = "Spawns around the HQ units",
+		author    = "B. Tyler",
+		date      = "31th Jan. 2009",
+		license   = "CC BY-NC",
+		layer     = -5,
+		enabled   = true --  loaded by default?
+	}
+end
+
+if (gadgetHandler:IsSyncedCode()) then
+
+---------------------------------------------------------------------------
+	local hqDefs = VFS.Include("LuaRules/Configs/hq_spawn.lua")
+		-- Removes this gadget if the game has started.
+		-- Only needs to run once at game start
+	--[[function gadget:GameFrame(n)
+		if n<2 then
+			gadgetHandler:RemoveGadget()
+		end
+	end]]--
+	
+	function gadget:UnitFinished(unitID, unitDefID, teamID)
+		local ud = UnitDefs[unitDefID]
+		local unitDefName = ud.name
+		if ud.customParams.hq == "1" then
+			local spawnList = hqDefs[unitDefName]
+			if not spawnList then return end
+			Spring.Echo(spawnList.units[1])
+			local spread = spawnList.spread
+			local px, py, pz = Spring.GetUnitPosition(unitID)
+			local xmin = px - ((2*ud.xsize) * spawnList.spread) / 2
+			local xmax = px + ((2*ud.xsize) * spawnList.spread) / 2
+			local zmin = pz - ((2*ud.zsize) * spawnList.spread) / 2
+			local zmax = pz + ((2*ud.zsize) * spawnList.spread) / 2
+			for blah, unitName in ipairs(spawnList.units) do
+				Spring.Echo(unitName)
+				local x = math.random(xmin, xmax)
+				local z = math.random(zmin, zmax)
+				local occupied = Spring.GetUnitsInCylinder(x, z, spread, teamID)
+				while (occupied[1] ~= nil) do
+					x = math.random(xmin, xmax)
+					z = math.random(zmin, zmax)
+					occupied = Spring.GetUnitsInCylinder(x, z, spread, teamID)
+				end
+				Spring.CreateUnit(unitName, x, py, z, 0, teamID)
+			end			
+		end
+	end	
+	
+else -- UNSYNCED
+
+end
