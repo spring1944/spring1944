0aefea8f6fd3d14e750455172d3b672292c92532
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-03-06 12:51:01 +0000

    * Added modified gamedata/download_builds.lua for loading monolithic buildOptions * buildOptions are now loaded from gamedata/builddefs.lua * Deleted Sidedata.tdf, moved sidedata.lua.x -> sidedata.lua

---
+++ gamedata/builddefs.lua
@@ -0,0 +1,576 @@
+buildoptions = 
+{
+
+				--------------------
+				-- american units --
+				--------------------
+
+	ushq = 
+	{
+		"ushqengineer",
+		--"usairengineer",
+		"us_platoon_hq",
+		--"us_platoon_hq_rifle",
+		--"us_platoon_hq_assault",
+	},
+
+	uscp =
+	{
+		"ushqengineer",
+		"usengineer",
+		"usgmctruck",
+		--"usjeep",
+	},
+
+	ushqengineer =
+	{
+		"usbarracks",
+--		"usflag",
+		"usstorage",
+		"usgmctruck",
+		"apminesign",
+		"atminesign",
+		--"sandbags",
+		"tankobstacle",
+		"rubberdingy",
+		"pontoonraft",
+	},
+
+	usbarracks =
+	{
+		"ushqengineer",
+		"usengineer",
+		"us_platoon_rifle",
+		"us_platoon_assault",
+		"us_platoon_mg",
+		"us_platoon_at",
+		"us_platoon_scout",
+		"us_platoon_sniper",
+		"us_platoon_flame",
+		"us_platoon_mortar",
+		"usm8gun_bax",
+	},
+
+	usengineer =
+	{
+		"usvehicleyard",
+		"usgunyard",
+		--"uscp",
+--		"usflag",
+		"usstorage",
+		"usgmctruck",
+		"atminesign",
+		"apminesign",
+		"tankobstacle",
+		--"sandbags",
+		"rubberdingy",
+		"pontoonraft",
+
+	},
+
+	usvehicleyard =
+	{
+		"usgmcengvehicle",
+		"usm3halftrack",
+		"usdukw",
+		"usm8greyhound",
+		"usm8scott",
+	},
+
+	usgunyard =
+	{
+		"usgmctruck",
+		"usm8gun_gunyard",
+		"usm5gun_truck",
+		"usm2gun_truck",
+		"usm1bofors_truck",
+	},
+
+
+	usgmcengvehicle =
+	{
+		"usbarracks",
+		"usgunyard",
+		"usvehicleyard",
+		--"usspyard",
+		"ustankyard",
+		"usstorage",
+		"ussupplydepot",
+		"tankobstacle",
+		"usradar",
+	},
+
+	ustankyard =
+	{
+		"usm5stuart",
+		"usm4a4sherman",
+		"usm10wolverine",
+	},
+	
+	ustankyard1 =
+	{
+		"usm4a376sherman",
+		"usm4a3105sherman",
+		"usm4jumbo",
+	},
+
+	usspyard =
+	{
+		"usm8scott",
+		"usm4a3105sherman",
+		"usm10wolverine",
+		"usm7priest",
+	},
+
+	ussupplydepot =
+	{
+		"usm3halftrack",
+	},
+
+	usairfield =
+	{
+		"usl4",
+		"usp51dmustang",
+		"usp51dmustangga",
+		"usp47thunderbolt",
+	},
+
+				--------------------
+				-- german units   --
+				--------------------
+
+	gerhqbunker =
+	{
+		"gerhqengineer",
+		--"gerairengineer",
+		"ger_platoon_hq",
+		--"ger_platoon_hq_rifle",
+		--"ger_platoon_hq_assault",
+	},
+
+	gerhqengineer =
+	{
+		"gerbarracks",
+--		"gerflag",
+		"gerstorage",
+		"apminesign",
+		"atminesign",
+		--"sandbags",
+		"tankobstacle",
+		"geropelblitz",
+		"gersturmboot",
+		"pontoonraft",
+	},
+
+	gerbarracks =
+	{
+		"gerhqengineer",
+		"gerengineer",
+		"ger_platoon_rifle",
+		"ger_platoon_assault",
+		"ger_platoon_mg",
+		"ger_platoon_scout",
+		"ger_platoon_at",
+		"ger_platoon_sniper",
+		"ger_platoon_mortar",
+		"gerleig18_bax",
+	},
+
+	gerengineer =
+	{
+		"gervehicleyard",
+		"gerbarracks",
+		"gergunyard",
+--		"gerflag",
+		"gerstorage",
+		"atminesign",
+		"apminesign",
+		--"sandbags",
+		"tankobstacle",
+		"geropelblitz",
+		"gersturmboot",
+		"pontoonraft",
+	},
+
+	gervehicleyard =
+	{
+		"gersdkfz9",
+		--"gersupplytruck",
+		"gersdkfz250",
+		"gersdkfz251",
+		"gersdkfz10",
+		"germarder",
+	},
+
+	gergunyard =
+	{
+		"geropelblitz",
+		--"gerflak38_truck",
+		"gerleig18_gunyard",
+		"gerpak40_truck",
+		"gerlefh18_truck",
+		"gernebelwerfer_truck",
+		"gerflak38_truck",
+	},
+
+	gersdkfz9 =
+	{
+		"gerbarracks",
+		"gergunyard",
+		"gervehicleyard",
+		--"gerspyard",
+		"gertankyard",
+		--"gerairfield",
+		"gerstorage",
+		"gersupplydepot",
+		"tankobstacle",
+		"gerradar",
+	},
+
+	gertankyard =
+	{
+		"gerpanzeriii",
+		"gerstugiii",
+		"gertiger",
+	},
+	
+	gertankyard1 =
+	{
+		"gerpanzeriv",
+		"gerjagdpanzeriv",
+		"gerwespe",
+		"gertiger",
+	},
+	
+	gertankyard2 =
+	{
+		"gerpanther",
+		"gertigerii",
+	},
+
+	gerspyard =
+	{
+		"germarder",
+		"gerstugiii",
+		"gerjagdpanzeriv",
+		"gerwespe",
+	},
+
+	gersupplydepot =
+	{
+		"gersdkfz251",
+	},
+
+	gerairfield =
+	{
+		"gerfi156",
+		"gerbf109",
+		"gerfw190",
+		"gerju87g",
+		"gerfw190g",
+	},
+
+				----------------------
+				----/british units----
+				----------------------
+
+	gbrhq =
+	{
+		"gbrhqengineer",
+		--"usairengineer",
+		"gbr_platoon_hq",
+		--"gbr_platoon_hq_rifle",
+		--"gbr_platoon_hq_assault",
+	},
+
+	gbrhqengineer =
+	{
+		"gbrbarracks",
+--		"gbrflag",
+		--"gbrresource",
+		"apminesign",
+		"atminesign",
+		--"sandbags",
+		"tankobstacle",
+		"gbrstorage",
+		"gbrbedfordtruck",
+		"rubberdingy",
+		"pontoonraft",
+	},
+
+	gbrbarracks =
+	{
+		"gbrhqengineer",
+		"gbrengineer",
+		"gbr_platoon_rifle",
+		"gbr_platoon_assault",
+		"gbr_platoon_mg",
+		"gbr_platoon_scout",
+		"gbr_platoon_at",
+		"gbr_platoon_sniper",
+		"gbr_platoon_mortar",
+		"gbr_platoon_commando",
+	},
+
+	gbrgunyard =
+	{
+		"gbrbedfordtruck",
+		"gbr17pdr_truck",
+		"gbr25pdr_truck",
+		"gbrbofors_truck",
+	},
+
+	gbrheavygunyard =
+	{
+		"gbrbedfordtruck",
+		"gbr17pdr_truck",
+		"gbr25pdr_truck",
+	},
+
+	
+	gbrcommandoc =
+	{
+	"gbrsatchelcharge",
+	"gbrlz",
+	},
+
+	gbrcommando =
+	{
+	"gbrsatchelcharge",
+	},
+
+	gbrlz =
+	{
+	"gbr_platoon_commando_lz",
+	},
+
+	gbrengineer =
+	{
+		"gbrvehicleyard",
+		"gbrgunyard",
+--		"gbrflag",
+		"gbrstorage",
+		"atminesign",
+		"apminesign",
+		--"sandbags",
+		"tankobstacle",
+		--"gbrstorage",
+		"gbrbedfordtruck",
+		"rubberdingy",
+		"pontoonraft",
+
+	},
+
+	gbrvehicleyard =
+	{
+		"gbrmatadorengvehicle",
+		--"gbrsupplytruck",
+		"gbrm5halftrack",
+		"gbrdaimler",
+		"gbrkangaroo",
+	},
+
+	gbrmatadorengvehicle =
+	{
+		"gbrbarracks",
+		"gbrgunyard",
+		"gbrvehicleyard",
+		--"gbrspyard",
+		"gbrtankyard",
+		--"gbrairfield",
+		"gbrstorage",
+		"gbrsupplydepot",
+		"tankobstacle",
+		"gbrradar",
+	},
+
+	gbrtankyard =
+	{
+		"gbraecmkii",
+		"gbrcromwell",
+		"gbrcromwellmkvi",
+		"gbrshermanfirefly",
+	},
+	
+	gbrtankyard1 =
+	{
+		"gbrkangaroo",
+		--"gbrshermanfirefly",
+		"gbrsexton",
+		"gbrm10achilles",
+		"gbrchurchillmkvii",
+	},
+
+	gbrspyard =
+	{
+		"gbrm10achilles",
+		"gbrsexton",
+	},
+
+
+	gbrsupplydepot =
+	{
+		"gbrm5halftrack",
+	},
+
+	gbrairfield =
+	{
+		"gbrauster",
+		"gbrspitfiremkxiv",
+		"gbrtyphoon",
+		"gbrspitfiremkix",
+	},
+
+				--------------------
+				-- soviet units   --
+				--------------------
+
+
+	ruscommander =
+	{
+--		"rusflag",
+		--"rusresource",
+		"ruscommissar",
+		"ruspshack",
+		"rusbarracks",
+		"rusgunyard",
+		"ruszis5",
+		"atminesign",
+		"apminesign",
+		"tankobstacle",
+		"russtorage",
+		--"sandbags",
+		"ruspg117",
+		"pontoonraft",
+	},
+
+	ruspshack =
+	{
+		--"ruscommissar1",
+		"rus_platoon_partisan",
+	},
+
+
+	rusbarracks =
+	{
+		"ruscommissar",
+		"rusengineer",
+		"rus_platoon_rifle",
+		--"rus_platoon_big_rifle",
+		"rus_platoon_assault",
+		--"rus_platoon_big_assault",
+		"rus_platoon_mg",
+		"rus_platoon_scout",
+		"rus_platoon_atlight",
+		"rus_platoon_atheavy",
+		"rus_platoon_sniper",
+		"rus_platoon_mortar",
+	},
+
+	rusengineer =
+	{
+		"rusvehicleyard",
+		"rusgunyard",
+--		"rusflag",
+		--"rusresource",
+		"ruszis5",
+		"apminesign",
+		"atminesign",
+		"tankobstacle",
+		"russtorage",
+		--"sandbags",
+		"ruspg117",
+		"pontoonraft",
+	},
+
+	ruscommissar =
+	{
+--		"rusflag",
+		--"rusresource",
+		"ruscommissar",
+		"ruspshack",
+		"rusbarracks",
+		"rusgunyard",
+		"ruszis5",
+		"apminesign",
+		"atminesign",
+		"tankobstacle",
+		--"sandbags",
+		"russtorage",
+		"ruspg117",
+		"pontoonraft",
+	},
+
+	rusk31 =
+	{
+		"rusbarracks",
+		"rusgunyard",
+		"rusvehicleyard",
+		--"russpyard",
+		"rustankyard",
+		--"rusairfield",
+		"russtorage",
+		"russupplydepot",
+		"tankobstacle",
+		"rusradar",
+	},
+
+	rusvehicleyard =
+	{
+		"rusk31",
+		--"russupplytruck",
+		"rusba64",
+		"rusm5halftrack",
+		"rusgazaaa",
+		"rust60",
+		"russu76",
+	},
+
+	rusgunyard =
+	{
+		"ruszis5",
+		"ruszis2_truck",
+		"ruszis3_truck",
+		"rusm30_truck",
+		"rus61k_truck",
+	},
+
+	rustankyard =
+	{
+		"rust70",
+		"rust3476",
+		"rusisu152",
+	},
+	
+	rustankyard1 =
+	{
+		"rust3485",
+		"russu100",
+		"rusis2",
+		"rusbm13n",
+	},
+
+	russpyard =
+	{
+		"russu76",
+		"russu85",
+		"russu100",
+		--"russu122", 
+		"rusbm13n",
+		"rusisu152",
+	},
+
+	russupplydepot =
+	{
+		"rusm5halftrack",
+	},
+	
+	rusairfield =
+	{
+		"ruspo2",
+		"rusyak3",
+		"rusil2",
+	},
+}
+
+return buildoptions
--- gamedata/sidedata.lua
+++ gamedata/sidedata.lua
@@ -4,32 +4,16 @@ local sidedata = {
 	startUnit = "GERHQBunker",
 	},
 	{
-	name = "AI GER",
-	startUnit = "AI_GERHQBunker",
-	},
-	{
 	name = "GBR",
 	startUnit = "GBRHQ",
-	},
-	{
-	name = "AI GBR",
-	startUnit = "AI_GBRHQ",
 	},
 	{
 	name = "RUS",
 	startUnit = "RUSCommander",
 	},
 	{
-	name = "AI RUS",
-	startUnit = "AI_RUSCommander",
-	},
-	{
 	name = "US",
 	startUnit = "USHQ",
-	},
-	{
-	name = "AI US",
-	startUnit = "AI_USHQ",
 	},
 }
 return sidedata
