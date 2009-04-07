52ae2bc733d4f705b80d1fc5b1694a12aa015c7e
tobi_v <tobi_v@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-04-07 18:54:08 +0000

    Disallow reclaim of units with footprintx < 4 and footprintz < 4, this includes towed guns, mine signs, all mobile units (AFAICS)
    This fix exploit where you could make for example a commando platoon
    and then reclaim the commandos for more command in total then the
    platoon costs. Besides that reclaiming infantry is just unrealistic :P

--- LuaRules/Gadgets/game_noTreeReclaim.lua
+++ LuaRules/Gadgets/game_noTreeReclaim.lua
@@ -4,7 +4,7 @@ function gadget:GetInfo()
     desc      = "Prevents default trees from being reclaimed",
     author    = "Tobi and Nemo, based on a bit by lurker",
     date      = "December, 2008",
-    license   = "public domain, except for line 17 and 18, because those were GPL'd by lurker. o_O",
+    license   = "public domain, except for line 20 and 21, because those were GPL'd by lurker. o_O",
     layer     = 0,
     enabled   = true  --  loaded by default?
   }
@@ -13,14 +13,31 @@ end
 if gadgetHandler:IsSyncedCode() then
 -- SYNCED
 	local GetFeatureDefID = Spring.GetFeatureDefID
+	local GetUnitDefID = Spring.GetUnitDefID
+	local CMD_RECLAIM = CMD.RECLAIM
 
 	function gadget:Initialize()
-	    for _,feature in ipairs(Spring.GetAllFeatures()) do
-	        local name = FeatureDefs[GetFeatureDefID(feature)].name
-	        if name and string.find(name, "treetype") ~= nil then
-	            Spring.SetFeatureNoSelect(feature, true)
-	        end
-	    end
+		for _,feature in ipairs(Spring.GetAllFeatures()) do
+			local name = FeatureDefs[GetFeatureDefID(feature)].name
+			if name and string.find(name, "treetype") ~= nil then
+				Spring.SetFeatureNoSelect(feature, true)
+			end
+		end
+	end
+
+	function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions)
+		--Only even consider blocking the command if it's a reclaim command on a (single) unit.
+		if (cmdID == CMD_RECLAIM and #cmdParams == 1 and cmdParams[1] < Game.maxUnits) then
+			local udid = GetUnitDefID(cmdParams[1])
+			if (udid ~= nil) then
+				--Allow reclaiming big buildings, block reclaim for mobile units and deployed guns etc.
+				--Now using heuristic based on footprintx >= 4 or footprintz >= 4 for this.
+				--Note that xsize/zsize value in unitDef is twice the footprint value!
+				local ud = UnitDefs[udid]
+				return ud.xsize >= 8 or ud.zsize >= 8
+			end
+		end
+		return true
 	end
 
 	-- The code below mimics Spring's internal reclaiming code, with the major
