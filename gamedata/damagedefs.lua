c3a3dd944598a27ef29068f3a7fbf276d9f0393c
Evil4Zerggin <Evil4Zerggin@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-06-25 20:41:47 +0000

    Preliminary damagedefs (not active yet).
    AP widget enabled by default.
    
    MG42 is now smallarm.

--- gamedata/damagedefs.lua
+++ gamedata/damagedefs.lua
@@ -1,7 +1,35 @@
 local damagedefs = {
-  default = {
+  default = {},
+  none = {},
+  smallarm = {
+    lightbuildings = 0.25,
+    bunkers = 0,
+    tanks = 0,
+    infantry = 10,
+    guns = 10,
+    planes = 10,
     flag = 0,
     mines = 0,
+  },
+  explosive = {
+    bunkers = 0.25,
+    unarmouredvehicles = 2,
+    tanks = 0.25,
+    flag = 0,
+  },
+  kinetic = {
+    lightbuildings = 0.1,
+    bunkers = 0.1,
+    flag = 0,
+    mines = 0,
+  },
+  shapedcharge = {
+    bunkers = 2,
+    flag = 0,
+  },
+  flame = {
+    bunkers = 5,
+    flag = 0,
   },
 }
 
