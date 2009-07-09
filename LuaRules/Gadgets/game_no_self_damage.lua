a86a008f1b5325a4810d5f7a05ec6b5fd9006e42
Evil4Zerggin <Evil4Zerggin@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-07-09 01:34:42 +0000

    Units can no longer damage themselves. While not technically realistic, the alternative is frustrating AI slipups.

---
+++ LuaRules/Gadgets/game_no_self_damage.lua
@@ -0,0 +1,19 @@
+function gadget:GetInfo()
+  return {
+    name      = "No Self-Damage",
+    desc      = "Prevents units from dealing damage to themselves.",
+    author    = "Evil4Zerggin",
+    date      = "8 July 2008",
+    license   = "GNU LGPL, v2.1 or later",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+if (not gadgetHandler:IsSyncedCode()) then
+  return false
+end
+
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, attackerID, attackerDefID, attackerTeam)
+  if unitID == attackerID then return 0 else return damage end
+end
