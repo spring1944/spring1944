18be1a6161e4977f56ddeee86f45f298eec64f16
Evil4Zerggin <Evil4Zerggin@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-07-21 20:43:51 +0000

    Added keys to modoption descriptions.

--- ModOptions.lua
+++ ModOptions.lua
@@ -16,31 +16,31 @@
 --  step:     quantization step, aligned to the def value
 --  maxlen:   the maximum string length for string options
 --  items:    array of item strings for list options
---  scope:    'all', 'player', 'team', 'allyteam'      <<< not supported yet >>>
+--  scope:    "all", "player", "team", "allyteam"      <<< not supported yet >>>
 
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 local options = {
 	--[[{
-		key = 'simple_tanks',
-		name = 'Simplified Tank Buildtree',
-		desc = 'Different german tank buildtree',
-		type = 'bool',
+		key = "simple_tanks",
+		name = "Simplified Tank Buildtree",
+		desc = "Different german tank buildtree",
+		type = "bool",
 		def = true,
 	},]]--
 	--[[{
-		key = 'always_visible_flags',
-		name = 'Always Visible Flags',
-		desc = 'Flags and their capping status can be seen without LOS',
-		type = 'bool',
+		key = "always_visible_flags",
+		name = "Always Visible Flags",
+		desc = "Flags and their capping status can be seen without LOS",
+		type = "bool",
 		def = true,
 	},]]--
 	--[[{
-    key    = 'maxammo_mult',
-    name   = 'Vehicle maxammmo multiplier',
-    desc   = 'Applies a multiplier to all the vehicle maxammo values',
-    type   = 'number',
+    key    = "maxammo_mult",
+    name   = "Vehicle maxammmo multiplier",
+    desc   = "Applies a multiplier to all the vehicle maxammo values",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
@@ -50,7 +50,7 @@ local options = {
     {
 	key    = "gametype",
 	name   = "Game Modes",
-	desc   = "Change the game mode",
+	desc   = "Change the game mode (key = 'gametype')",
 	type   = "list",
 	def    = "0",
 	items  = {
@@ -68,70 +68,70 @@ local options = {
   },
 
   {
-    key    = 'command_mult',
-    name   = 'Command Point Income/Battle Significance',
-    desc   = 'Sets level of Command Point income - use to adjust maps that provide too much or too little command points',
-    type   = 'list',
-    def    = '2',
+    key    = "command_mult",
+    name   = "Command Point Income/Battle Significance",
+    desc   = "Sets level of Command Point income - use to adjust maps that provide too much or too little command points (key = 'command_mult')",
+    type   = "list",
+    def    = "2",
     items  =
     {
 	  {
-        key  = '0',
-        name = 'Very Low',
-        desc = 'Very limited resources. Nothing but a minor skirmish, you must make the most of what resources you have.',
+        key  = "0",
+        name = "Very Low",
+        desc = "Very limited resources. Nothing but a minor skirmish, you must make the most of what resources you have.",
       },
       {
-        key  = '1',
-        name = 'Low',
-        desc = 'Limited Command Points. This battle is insignificant, and you will be struggling to maintain infantry battalions',
+        key  = "1",
+        name = "Low",
+        desc = "Limited Command Points. This battle is insignificant, and you will be struggling to maintain infantry battalions",
       },
       {
-        key  = '2',
-        name = 'Normal',
-        desc = 'Standard Command Points. The supreme commanders are keeping an eye on the outcome of this engagement. Expect medium numbers of infantry with considerable vehicle support, with armor and gun batteries appearing later.',
+        key  = "2",
+        name = "Normal",
+        desc = "Standard Command Points. The supreme commanders are keeping an eye on the outcome of this engagement. Expect medium numbers of infantry with considerable vehicle support, with armor and gun batteries appearing later.",
       },
       {
-        key  = '3',
-        name = 'High',
-        desc = 'Abundant Command Points. The command has deemed this battle vital. You must win at all costs, and your available resources reflect that urgency.',
+        key  = "3",
+        name = "High",
+        desc = "Abundant Command Points. The command has deemed this battle vital. You must win at all costs, and your available resources reflect that urgency.",
       },
 	  {
-        key  = '4',
-        name = 'Very High',
-        desc = 'Excessive Command Points. High command has an emotional attachment to your skirmish, and they want it won.',
+        key  = "4",
+        name = "Very High",
+        desc = "Excessive Command Points. High command has an emotional attachment to your skirmish, and they want it won.",
       },
     },
   },
 
     {
-    key    = 'logistics_mult',
-    name   = 'Logistics Resupply Frequency',
-    desc   = 'Sets the gap between Logistics Resupply',
-    type   = 'list',
-    def    = '1',
+    key    = "logistics_mult",
+    name   = "Logistics Resupply Frequency",
+    desc   = "Sets the gap between Logistics Resupply (key = 'logistics_mult')",
+    type   = "list",
+    def    = "1",
     items  =
     {
       {
-        key  = '0',
-        name = 'Low - 11.25 minute gap',
-        desc = 'Limited logistics supply. Conservative play - storage buildings and well supplied infantry are the order of the day.',
+        key  = "0",
+        name = "Low - 11.25 minute gap",
+        desc = "Limited logistics supply. Conservative play - storage buildings and well supplied infantry are the order of the day.",
       },
       {
-        key  = '1',
-        name = 'Normal - 7.5 minute gap',
-        desc = 'Normal logistics supply. Supplies come on a frequent enough basis to keep the warmachine rumbling, but beware of large artillery batteries or armored thrusts.',
+        key  = "1",
+        name = "Normal - 7.5 minute gap",
+        desc = "Normal logistics supply. Supplies come on a frequent enough basis to keep the warmachine rumbling, but beware of large artillery batteries or armored thrusts.",
       },
       {
-        key  = '2',
-        name = 'High - 3.75 minute gap',
-        desc = 'Abundant logistics supply. Supply deliveries arrive early and often, allowing for much more aggressive play.',
+        key  = "2",
+        name = "High - 3.75 minute gap",
+        desc = "Abundant logistics supply. Supply deliveries arrive early and often, allowing for much more aggressive play.",
       },
 	 },
     },
    {
       key="scoremode",
       name="Score Mode",
-      desc="How are control points scored?",
+      desc="How are control points scored? (key = 'scoremode')",
       type="list",
       def="disabled",
       items = {
@@ -160,7 +160,7 @@ local options = {
   {
       key="starttime",
       name="Start Time",
-      desc="When the capturing of points can begin.",
+      desc="When the capturing of points can begin. (key = 'starttime')",
       type="list",
       def="5",
       items = {
@@ -187,10 +187,10 @@ local options = {
    },
 
    {
-    key    = 'command_storage',
-    name   = 'Fixed Command Storage',
-    desc   = 'Fixes the command storage of all players.',
-    type   = 'number',
+    key    = "command_storage",
+    name   = "Fixed Command Storage",
+    desc   = "Fixes the command storage of all players. (key = 'command_storage')",
+    type   = "number",
     def    = 10000,
     min    = 1000,
     max    = 1944000,
@@ -198,10 +198,10 @@ local options = {
   },
   
   {
-    key    = 'map_command_per_player',
-    name   = 'Map Command Per Player',
-    desc   = 'Sets the total command on the map to some number per player (negative to disable).',
-    type   = 'number',
+    key    = "map_command_per_player",
+    name   = "Map Command Per Player",
+    desc   = "Sets the total command on the map to some number per player (negative to disable). (key = 'map_command_per_player')",
+    type   = "number",
     def    = -10,
     min    = -10,
     max    = 1000,
@@ -210,18 +210,18 @@ local options = {
 
 
 	{
-		key = 'gm_team_enable',
-		name = 'Enable Sandbox/GM tools faction',
-		desc = 'Allows the sandbox/game master tools faction to spawn, rather than changing to a random team',
-		type = 'bool',
+		key = "gm_team_enable",
+		name = "Enable Sandbox/GM tools faction",
+		desc = "Allows the sandbox/game master tools faction to spawn, rather than changing to a random team (key = 'gm_team_enable')",
+		type = "bool",
 		def = false,
 	},
 	
 	{
-		key = 'prisoner_income',
-		name = 'Command Income per tick per prisoner',
-		desc = 'Changes the amount you recieve for holding prisoners',
-	    type   = 'number',
+		key = "prisoner_income",
+		name = "Command Income per tick per prisoner",
+		desc = "Changes the amount you recieve for holding prisoners (key = 'prisoner_income')",
+	    type   = "number",
 		def    = 0,
 		min    = 0,
 		max    = 5,
@@ -229,60 +229,60 @@ local options = {
 	},
 	
 	--[[{
-    key    = 'weapon_range_mult',
-    name   = 'Weapon range multiplier',
-    desc   = 'Applies a multiplier to all the weapon ranges ingame',
-    type   = 'number',
+    key    = "weapon_range_mult",
+    name   = "Weapon range multiplier",
+    desc   = "Applies a multiplier to all the weapon ranges ingame",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
     step   = 0.1,
   },
   {
-    key    = 'weapon_reload_mult',
-    name   = 'Weapon reload multiplier',
-    desc   = 'Applies a multiplier to all the weapon reloadtimes ingame',
-    type   = 'number',
+    key    = "weapon_reload_mult",
+    name   = "Weapon reload multiplier",
+    desc   = "Applies a multiplier to all the weapon reloadtimes ingame",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
     step   = 0.1,
   },
   {
-    key    = 'unit_los_mult',
-    name   = 'Unit sight (los/airLoS) multiplier',
-    desc   = 'Applies a multiplier to all the LoS ranges ingame',
-    type   = 'number',
+    key    = "unit_los_mult",
+    name   = "Unit sight (los/airLoS) multiplier",
+    desc   = "Applies a multiplier to all the LoS ranges ingame",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
     step   = 0.1,
   },
   {
-    key    = 'unit_speed_mult',
-    name   = 'Unit speed multiplier',
-    desc   = 'Applies a multiplier to all the unit speeds and acceleration values ingame',
-    type   = 'number',
+    key    = "unit_speed_mult",
+    name   = "Unit speed multiplier",
+    desc   = "Applies a multiplier to all the unit speeds and acceleration values ingame",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
     step   = 0.1,
   },
   {
-    key    = 'unit_metal_mult',
-    name   = 'Metal extraction multiplier',
-    desc   = 'Applies a multiplier to all the metal extraction values',
-    type   = 'number',
+    key    = "unit_metal_mult",
+    name   = "Metal extraction multiplier",
+    desc   = "Applies a multiplier to all the metal extraction values",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
     step   = 0.05,
   },
     {
-    key    = 'weapon_aoe_mult',
-    name   = 'AoE multiplier',
-    desc   = 'Applies a multiplier to all the weapon AoE values',
-    type   = 'number',
+    key    = "weapon_aoe_mult",
+    name   = "AoE multiplier",
+    desc   = "Applies a multiplier to all the weapon AoE values",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
@@ -290,20 +290,20 @@ local options = {
   },
 
    {
-    key    = 'weapon_hedamage_mult',
-    name   = 'HE damage multiplier',
-    desc   = 'Applies a multiplier to all the HE damage values',
-    type   = 'number',
+    key    = "weapon_hedamage_mult",
+    name   = "HE damage multiplier",
+    desc   = "Applies a multiplier to all the HE damage values",
+    type   = "number",
     def    = 1.0,
     min	   = 0.1,
     max    = 10,
     step   = 0.05,
   },
   {
-    key    = 'weapon_edgeeffectiveness_mult',
-    name   = 'Weapon edgeeffectiveness multiplier',
-    desc   = 'Applies a multiplier to all the weapon edgeeffectiveness ingame',
-    type   = 'number',
+    key    = "weapon_edgeeffectiveness_mult",
+    name   = "Weapon edgeeffectiveness multiplier",
+    desc   = "Applies a multiplier to all the weapon edgeeffectiveness ingame",
+    type   = "number",
     def    = 1.0,
     min	   = 0.01,
     max    = 10,
@@ -312,20 +312,20 @@ local options = {
 
   --[[
   {
-    key    = 'unit_buildable_airfields',
-    name   = 'Airfield enabler',
-    desc   = 'Adds/removes airfields from build trees',
-    type   = 'number',
+    key    = "unit_buildable_airfields",
+    name   = "Airfield enabler",
+    desc   = "Adds/removes airfields from build trees",
+    type   = "number",
     def    = 0,
     min	   = 0,
     max    = 1,
     step   = 1,
   },
     {
-    key    = 'unit_hq_platoon',
-    name   = 'HQ-centric infantry game',
-    desc   = 'Removes rifle/assault squads from barracks, puts them in HQ',
-    type   = 'number',
+    key    = "unit_hq_platoon",
+    name   = "HQ-centric infantry game",
+    desc   = "Removes rifle/assault squads from barracks, puts them in HQ",
+    type   = "number",
     def    = 0,
     min	   = 0,
     max    = 1,
@@ -338,26 +338,26 @@ local options = {
 --  C.R.A.I.G. specific option(s)
 --
 	{
-		key    = 'craig_difficulty',
-		name   = 'C.R.A.I.G. difficulty level',
-		desc   = 'Sets the difficulty level of the C.R.A.I.G. bot.',
-		type   = 'list',
-		def    = '2',
+		key    = "craig_difficulty",
+		name   = "C.R.A.I.G. difficulty level",
+		desc   = "Sets the difficulty level of the C.R.A.I.G. bot. (key = 'craig_difficulty')",
+		type   = "list",
+		def    = "2",
 		items = {
 			{
-				key = '1',
-				name = 'Easy',
-				desc = 'No resource cheating.'
+				key = "1",
+				name = "Easy",
+				desc = "No resource cheating."
 			},
 			{
-				key = '2',
-				name = 'Medium',
-				desc = 'Little bit of resource cheating.'
+				key = "2",
+				name = "Medium",
+				desc = "Little bit of resource cheating."
 			},
 			{
-				key = '3',
-				name = 'Hard',
-				desc = 'Infinite resources.'
+				key = "3",
+				name = "Hard",
+				desc = "Infinite resources."
 			},
 		}
 	},
