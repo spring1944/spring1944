decafa42a67bf2dc81a3f30872f815ee8e2331d6
Evil4Zerggin <Evil4Zerggin@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-07-28 22:47:16 +0000

    Selection info gets a tab.

--- LuaUI/Widgets/gui_s44_gui/components/selectioninfo.lua
+++ LuaUI/Widgets/gui_s44_gui/components/selectioninfo.lua
@@ -1,8 +1,9 @@
 local component = {}
 
-local mainSizeX = 256
 local mainSizeY = 256
-local fontSize = 16
+local fontSize = 1/16
+
+local tabWidth = 1/16
 
 local GetSelectedUnits = Spring.GetSelectedUnits
 
@@ -11,9 +12,78 @@ local GetUnitHealth = Spring.GetUnitHealth
 local GetUnitExperience = Spring.GetUnitExperience
 local GetUnitRulesParam  = Spring.GetUnitRulesParam
 
+local glPushMatrix = gl.PushMatrix
+local glPopMatrix = gl.PopMatrix
+
+local glTranslate = gl.Translate
+local glScale = gl.Scale
+local glRotate = gl.Rotate
+
 local strFormat = string.format
 local glColor = gl.Color
 local glRect = gl.Rect
+
+local function DrawTab()
+  glPushMatrix()
+    glScale(tabWidth, tabWidth, 1)
+    glTranslate(1, 0, 0)
+    glRotate(90, 0, 0, 1)
+    
+    glColor(0, 0, 0, guiOpacity)
+    glRect(0, 0, 1 / tabWidth, 1)
+    glColor(1, 1, 1, 1)
+    
+    font16:Print("Selection", 0.5 / tabWidth, 0, 1, "co")
+  glPopMatrix()
+end
+
+local function DrawMain()
+  glPushMatrix()
+  glTranslate(tabWidth, 0, 0)
+    local selectedUnits = GetSelectedUnits()
+    
+    if #selectedUnits == 0 then return end
+    
+    glColor(0, 0, 0, guiOpacity)
+    glRect(0, 0, 1, 1)
+    glColor(1, 1, 1, 1)
+    
+    if #selectedUnits == 1 then
+      local unitID = selectedUnits[1]
+      local unitDef = UnitDefs[GetUnitDefID(unitID)]
+      local humanName = unitDef.humanName
+      local tooltip = unitDef.tooltip
+      
+      local health, maxHealth = GetUnitHealth(unitID)
+      local healthProportion = health / maxHealth
+      local healthColorString = GetColorString(GetHealthColor(healthProportion))
+      
+      local text = humanName .. ": " .. tooltip .. "\n"
+        .. healthColorString .. "Health: " .. strFormat("%u", health) .. "/" .. strFormat("%u", maxHealth) .. "\n"
+        
+      local ammo = GetUnitRulesParam(unitID, "ammo")
+      local maxAmmo = unitDef.customParams.maxammo
+      
+      if ammo and maxAmmo then
+        local ammoProportion = ammo / maxAmmo
+        local ammoColorString = GetColorString(GetHealthColor(ammoProportion))
+        text = text .. ammoColorString .. "Ammunition: " .. strFormat("%u", ammo) .. "/" .. strFormat("%u", maxAmmo) .. "\n"
+      end
+      
+      if #unitDef.weapons > 0 then
+        local xp = GetUnitExperience(unitID)
+        text = text .. "\255\255\255\255Experience: " .. strFormat("%.2f", xp)
+      end
+        
+      text = font16:WrapText(text, 1, 1, fontSize)
+      
+      font16:Print(text, 0, 1, fontSize, "to")
+    else
+      local text = #selectedUnits .. " selected units"
+      font16:Print(text, 0, 1, fontSize, "to")
+    end
+  glPopMatrix()
+end
 
 function component:Initialize()
   Spring.SendCommands("tooltip 0")
@@ -30,53 +100,17 @@ function component:CommandsChanged()
 end
 
 function component:DrawScreen()
-  glColor(0, 0, 0, guiOpacity)
-  glRect(0, 0, mainSizeX, mainSizeY)
-  glColor(1, 1, 1, 1)
-
-  local selectedUnits = GetSelectedUnits()
-  
-  if #selectedUnits == 0 then
-    -- ?
-  elseif #selectedUnits == 1 then
-    local unitID = selectedUnits[1]
-    local unitDef = UnitDefs[GetUnitDefID(unitID)]
-    local humanName = unitDef.humanName
-    local tooltip = unitDef.tooltip
+  glPushMatrix()
+    --transform to component coords: (0, 0) to (1, 1)
+    glScale(mainSizeY, mainSizeY, 1)
     
-    local health, maxHealth = GetUnitHealth(unitID)
-    local healthProportion = health / maxHealth
-    local healthColorString = GetColorString(GetHealthColor(healthProportion))
+    DrawTab()
+    DrawMain()
     
-    local text = humanName .. ": " .. tooltip .. "\n"
-      .. healthColorString .. "Health: " .. strFormat("%u", health) .. "/" .. strFormat("%u", maxHealth) .. "\n"
-      
-    local ammo = GetUnitRulesParam(unitID, "ammo")
-    local maxAmmo = unitDef.customParams.maxammo
-    
-    if ammo and maxAmmo then
-      local ammoProportion = ammo / maxAmmo
-      local ammoColorString = GetColorString(GetHealthColor(ammoProportion))
-      text = text .. ammoColorString .. "Ammunition: " .. strFormat("%u", ammo) .. "/" .. strFormat("%u", maxAmmo) .. "\n"
-    end
-    
-    if #unitDef.weapons > 0 then
-      local xp = GetUnitExperience(unitID)
-      text = text .. "\255\255\255\255Experience: " .. strFormat("%.2f", xp)
-    end
-      
-    text = font16:WrapText(text, mainSizeX - 8, mainSizeY, fontSize)
-    
-    font16:Print(text, 0, mainSizeY, fontSize, "to")
-  else
-    local text = #selectedUnits .. " selected units"
-    font16:Print(text, 0, mainSizeY, fontSize, "to")
-  end
-  
+  glPopMatrix()
 end
 
 function component:ViewResize()
-  mainSizeX = vsx * 0.2
   mainSizeY = vsy * 0.25
 end
 
