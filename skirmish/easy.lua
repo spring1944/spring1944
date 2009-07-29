78dc993d01aa24c325b6208b3f135507bcf77419
koshi0 <koshi0@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2009-07-29 19:03:09 +0000

    added all files necessary for SpringLobby's simple interface customization (prototype)

---
+++ skirmish/easy.lua
@@ -0,0 +1,91 @@
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+local options = {
+
+	--this is mandatory, min. 1 listitem
+    {
+	key    = "ai_team_ids",
+	name   = "AI Teams",
+	desc   = "used to map ai num <-> team id and define number of added AIs",
+	type   = "list",
+	def    = "0",
+	items  = {
+		{
+		key  = "0",
+		name = "2",
+		desc = "put first AI into team 2",
+		},
+		{
+		key  = "1",
+		name = "3",
+		desc = "put second AI into team 3",
+		},
+	  },
+  },
+	--optional, if no listitems or left out completely, all avail maps are offered to the user
+    {
+	key    = "suggested_maps",
+	name   = "Suggested Maps",
+	desc   = "add XX to this list to limit the choice of maps the player gets presented with",
+	type   = "list",
+	def    = "0",
+	items  = {
+		{
+		key  = "0",
+		name = "1944_Road_To_Rome_V3.smf",
+		desc = "",
+		},
+		{
+		key  = "1",
+		name = "1944_BocageSmall.smf",
+		desc = "",
+		},
+	  },
+  },
+
+  --all optional below, copy-paste any modoption you like and set a different default value to override
+  {
+    key    = "command_storage",
+    name   = "Fixed Command Storage",
+    desc   = "Fixes the command storage of all players. (key = 'command_storage')",
+    type   = "number",
+    def    = 20000,
+    min    = 1000,
+    max    = 1944000,
+    step   = 1000,
+  },
+	
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--  C.R.A.I.G. specific option(s)
+--
+	{
+		key    = "craig_difficulty",
+		name   = "C.R.A.I.G. difficulty level",
+		desc   = "Sets the difficulty level of the C.R.A.I.G. bot. (key = 'craig_difficulty')",
+		type   = "list",
+		def    = "1",
+		items = {
+			{
+				key = "1",
+				name = "Easy",
+				desc = "No resource cheating."
+			},
+			{
+				key = "2",
+				name = "Medium",
+				desc = "Little bit of resource cheating."
+			},
+			{
+				key = "3",
+				name = "Hard",
+				desc = "Infinite resources."
+			},
+		}
+	},
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+}
+return options
