459abdf8053a731f37e0b08bae56cb3bf29a92cc
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-01-18 06:15:28 +0000

    Bumping armor 'hardness' back up slightly (from 3.7 to 4.5 mult).

--- LuaRules/Gadgets/game_armor.lua
+++ LuaRules/Gadgets/game_armor.lua
@@ -46,7 +46,7 @@ end
 --higher = more quickly
 --along with cost, controls how hard counters are; higher = harder counters
 --recommend somewhere around 4-8?
-local ARMOR_POWER = 3.7
+local ARMOR_POWER = 4.5 --3.7
 
 --effective penetration = HE_MULT * sqrt(damage)
 
