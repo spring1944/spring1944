01ac0b2735aa5c4b6ef3ccff59e851067ad99f9e
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-02-07 08:17:52 +0000

    * Cleanup commented out code

--- LuaRules/Configs/cob_buttons.lua
+++ LuaRules/Configs/cob_buttons.lua
@@ -22,28 +22,6 @@ local tmpReturn = {
 for _, tmpUnitDef in pairs(UnitDefs) do
 	if (tmpUnitDef.customParams) then
 		local tmpParams = tmpUnitDef.customParams
-		-- Turn button
-		--[[if (tmpParams.hasturnbutton) then
-			-- determine unit's turn speed
-			local turnSpeed = tmpUnitDef.turnRate or 5*182
-			local tmpCmd = {
-				cob = "RotateHere",
-				name = "Turn",
-				tooltip = "Turn to face a given point",
-				requiresdirection = "1",
-				type = CMDTYPE.ICON_MAP,
-				cursor = "Patrol",
-				precob = "SetTurnSpeed",
-				precobparam = turnSpeed,
-			}
-			if (tmpReturn[tmpUnitDef.name]) then
-				table.insert(tmpReturn[tmpUnitDef.name], tmpCmd)
-			else
-				tmpReturn[tmpUnitDef.name] ={
-					tmpCmd,
-				}
-			end
-		end]]
 		if (tmpParams.canclearmines) then
 			local tmpCmd = {
 				cob = "LookForMines",
@@ -58,30 +36,6 @@ for _, tmpUnitDef in pairs(UnitDefs) do
 				}
 			end
 		end
-		--[[if (tmpParams.canfiresmoke) then
-			-- add Smoke button
-			local tmpCmd = {
-				cob = "SwitchToSmoke",
-				name = "Smoke",
-				tooltip = "Switch to smoke shells"
-			}
-			if (tmpReturn[tmpUnitDef.name]) then
-				table.insert(tmpReturn[tmpUnitDef.name], tmpCmd)
-			else
-				tmpReturn[tmpUnitDef.name] ={
-					tmpCmd,
-				}
-			end
-			-- add HE button
-			tmpCmd = {
-				cob = "SwitchToHE",
-				name = "HE",
-				tooltip = "Switch to HE shells"
-			}
-			-- no need to check for existing cmddescs since we just added one
-			table.insert(tmpReturn[tmpUnitDef.name], tmpCmd)
-		end]]
-		-- other things to come later
 	end
 end
 
