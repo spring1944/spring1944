526c5c59f79ec5ae690fceeaca20466fa69f8a95
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-02-19 17:19:25 +0000

    Removing some leftover Spring.Echo commands

--- LuaRules/Gadgets/unit_indirectFireAccuracy.lua
+++ LuaRules/Gadgets/unit_indirectFireAccuracy.lua
@@ -1,12 +1,3 @@
---[[
-TODO: 
-add the settle radius stuff (check if target is more than settleRadius away from previous spotID for that unit...which means adding a new table layer for units)
-make sure the logic of aiming at a spot, settling for acuisitionTime, and then firing with improved accuracy really works.
-fix any bugs
-
-
-]]--
-
 function gadget:GetInfo()
     return {
         name      = "Indirect Fire Accuracy Manager",
@@ -36,31 +27,26 @@ local GetGameSeconds		= Spring.GetGameSeconds
 local SetUnitWeaponState	= Spring.SetUnitWeaponState
 
 --constants
-local losMult 				= 0.25 --how much more accurate the weapon gets
-local accuracyDelay			= 20 --# of seconds before the accuracy improvement kicks in
+local losMult 				= 0.25 --how much more accurate the weapon gets (lower accuracy number = more accurate, this is multiplied by the regular accuracy)
+local accuracyDelay			= 20 --# of seconds after LoS is established on attack location before the accuracy improvement kicks in
 --vars
 local visibleAreas			= {}
 
 
 local function updateUnit(allyTeam, unitID)
-	--Spring.Echo("update function called")
 	local unitDefID = Spring.GetUnitDefID(unitID)
 	local weapons = UnitDefs[unitDefID].weapons
 	local newAccuracy
 	if visibleAreas[allyTeam][unitID] ~= nil then
 		if (visibleAreas[allyTeam][unitID].zeroed == true) then
 			newAccuracy = WeaponDefs[weapons[1].weaponDef].accuracy * losMult
-			Spring.Echo("zero'd in on target! you've attacked the same place long enough")
 		else
-			Spring.Echo("Not yet zeroed in on target - not in LoS or not zeroed")
 			newAccuracy = WeaponDefs[weapons[1].weaponDef].accuracy
 		end
 	end
 	
 	for i=0, #weapons do
-		Spring.Echo("old accuracy:", WeaponDefs[weapons[1].weaponDef].accuracy, "new accuracy", newAccuracy)
 		SetUnitWeaponState(unitID, i, {accuracy = newAccuracy})
-		Spring.Echo("change the weapon accuracy for weapon", i)
 	end
 end
 
@@ -71,12 +57,10 @@ function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams)
 				local allyTeam = GetUnitAllyTeam(unitID)
 				local targetX, targetY, targetZ
 				
-				if IsValidUnitID(cmdParams[1]) == true then
+				if IsValidUnitID(cmdParams[1]) == true then --shooting at a unit
 					targetX, targetY, targetZ = GetUnitPosition(cmdParams[1])
-					Spring.Echo("shooting at a unit!, ID", cmdParams[1])
-				else
+				else --shooting at the ground
 					targetX, targetY, targetZ = cmdParams[1], cmdParams[2], cmdParams[3]
-					Spring.Echo("shooting at the ground!, coords:", cmdParams[1], cmdParams[2], cmdParams[3])
 				end
 				
 				if visibleAreas[allyTeam] == nil then
@@ -103,7 +87,7 @@ function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams)
 end
 
 function gadget:GameFrame(n)
-	if (n % (2*30)) < 0.1 then
+	if (n % (2*30)) < 0.1 then --update every two seconds
 		for allyID, someThing in pairs(visibleAreas) do
 			for unitID, someThing in pairs(visibleAreas[allyID]) do
 				local unitArea = visibleAreas[allyID][unitID]
