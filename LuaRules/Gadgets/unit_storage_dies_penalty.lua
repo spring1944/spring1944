0193388cff8e0cef46eba63ba8a33e4f46e3ee6d
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-03-22 17:29:15 +0000

    * Fix 'unfinished storages removing 1k of log when killed' -- Also remove some unused crap in the problem gadget, unit_storage_dies_penalty.lua

--- LuaRules/Gadgets/unit_storage_dies_penalty.lua
+++ LuaRules/Gadgets/unit_storage_dies_penalty.lua
@@ -1,22 +1,8 @@
--- $Id: unit_storage_dies_penalty.lua 3171 2008-11-06 09:06:29Z det $
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---
---  file: unit_storage_dies_penalty.lua
---  brief: Gives realisitic penalties for destruction of economy buildings and storage
---  author: TheFatController
---
---  Copyright (C) 2007.
---  Licensed under the terms of the GNU GPL, v2 or later.
---
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 function gadget:GetInfo()
   return {
     name      = "Storage Dies Penalty",
     desc      = "Gives penalties for destruction of economy buildings and storage",
-    author    = "TheFatController, tweaked by Nemo for S44",
+    author    = "TheFatController, Nemo, FLOZi",
     date      = "June 23, 2007. October 12, 2009",
     license   = "GNU GPL, v2 or later",
     layer     = -5,
@@ -24,60 +10,25 @@ function gadget:GetInfo()
   }
 end
 
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-local GetUnitResources = Spring.GetUnitResources
 local UseTeamResource = Spring.UseTeamResource
 local GetTeamResources = Spring.GetTeamResources
-local GetUnitDefID = Spring.GetUnitDefID
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+local GetUnitHealth = Spring.GetUnitHealth
 
 if (gadgetHandler:IsSyncedCode()) then
 --------------------------------------------------------------------------------
 --  SYNCED
 --------------------------------------------------------------------------------
 
-local function deadUnit(team, penalty)
-  return { team = team, penalty = penalty}
-end
-
-local deadList = {}
-
-function gadget:GameFrame(n)
-  for i,v in pairs(deadList) do
-    if (GetUnitDefID(i) == nil) then
-      local eCur = GetTeamResources(v.team, "energy")
-      UseTeamResource(v.team, "e", math.min(v.penalty, eCur))
-      deadList[i] = nil
+function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+  local _, _, _, _, buildProgress = GetUnitHealth(unitID)
+  if buildProgress == 1 then
+    local eStore = UnitDefs[unitDefID].energyStorage
+    if (eStore > 0) then
+      local eCur, eMax = GetTeamResources(unitTeam, "energy")
+      local stored = (eStore / eMax)
+	  UseTeamResource(unitTeam, "e", (eCur * stored))
     end
   end
-end
-
-function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
-
-  local _, _, em = GetUnitResources(unitID)
-  local eStore = UnitDefs[unitDefID].energyStorage
-  
-  if (em > 0) then
-    
-    deadList[unitID] = deadUnit(unitTeam, em)
-    
-  end
-  
-  if (eStore > 0) then
-    
-     local eCur, eMax = GetTeamResources(unitTeam, "energy")
-     
-     local stored = (eStore / eMax)
-     
-     UseTeamResource(unitTeam, "e", (eCur * stored))
-     
-  end
-  
 end
 
 --------------------------------------------------------------------------------
