8016831d236cfe46d1bb402dd403871c2d8db8d9
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-04-19 00:34:58 +0000

    * Make vehicles pushresistant in unitdefs_post.lua -- Doesn't remove pushing altogether, but at least infantry don't push vehicles when they load

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -134,37 +134,7 @@ end
 		end
 	end
 
---[[
-	if (modOptions and (modOptions.unit_buildable_airfields == 0)) then
-		disableunits({usairfield", "gbrairfield", "gerairfield", "RUSAirfield"})
-	end
 
-	if (modOptions and (modOptions.unit_hq_platoon == 1)) then
-		disableunits({"us_platoon_hq", "us_platoon_rifle", "us_platoon_assault", "gbr_platoon_hq", "gbr_platoon_rifle", "gbr_platoon_assault", "ger_platoon_hq", "ger_platoon_rifle", "ger_platoon_assault", "rus_platoon_rifle", "rus_platoon_assault"})
-	end
-
-	if (modOptions and (modOptions.unit_hq_platoon == 0)) then
-		disableunits({"us_platoon_hq_rifle", "us_platoon_hq_assault", "gbr_platoon_hq_rifle", "gbr_platoon_hq_assault", "ger_platoon_hq_rifle", "ger_platoon_hq_assault", "rus_platoon_big_rifle", "rus_platoon_big_assault"})
-	end
-
-
-if (modOptions and (unit_buildable_airfields == 1)) then
-	        for name, ud in pairs(UnitDefs) do
-	            local unitname = ud.unitname
-	                if unitname == "USgmcengvehicle" then
-	                    table.insert(ud.buildoptions, 1, "usairfield")
-	                end
-					if unitname == "rusk31" then
-	                    table.insert(ud.buildoptions, 1, "rusairfield")
-	                end
-					if unitname == "gersdkfz9" then
-	                    table.insert(ud.buildoptions, 1, "gerairfield")
-	                end
-					if unitname == "GBRMatadorEngVehicle" then
-	                    table.insert(ud.buildoptions, 1, "GBRAirfield")
-	                end
-            end
-end]]--
 
 -- adjust descriptions
 for name, ud in pairs(UnitDefs) do
@@ -188,5 +158,10 @@ for name, ud in pairs(UnitDefs) do
 			ud.description = ud.description.." ("..newDescrLine..")"
 			
 		end
+	end
+	-- Make all vehicles push resistant
+	if tonumber(ud.maxvelocity or 0) > 0 and (ud.xsize or 1) > 1 then
+		Spring.Echo(name)
+		ud.pushresistant = true
 	end
 end
