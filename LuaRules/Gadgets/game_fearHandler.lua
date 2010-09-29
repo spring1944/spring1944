717f12121d67bb7cbd20c73c742bff4de6262442
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-09-29 18:43:08 +0000

    * Get rid of table.insert in game_fearHandler.lua

--- LuaRules/Gadgets/game_fearHandler.lua
+++ LuaRules/Gadgets/game_fearHandler.lua
@@ -4,7 +4,7 @@ function gadget:GetInfo()
 		desc      = "Lua Implementation of fear weapons causing suppression",
 		author    = "FLOZi",
 		date      = "13 October 2008", -- Happy Birthday Charlie!
-		license   = "CC by-nc, version 3.0",
+		license   = "GNU CPL v2",
 		layer     = 0,
 		enabled   = true
 	}
@@ -15,6 +15,7 @@ end
 local GetUnitsInCylinder		= Spring.GetUnitsInCylinder
 local GetUnitDefID       		= Spring.GetUnitDefID
 local GetUnitAllyTeam			= Spring.GetUnitAllyTeam
+local ValidUnitID				= Spring.ValidUnitID
 -- Synced Ctrl
 local CallCOBScript				= Spring.CallCOBScript
 local SetUnitExperience			= Spring.SetUnitExperience
@@ -23,43 +24,49 @@ local SetUnitExperience			= Spring.SetUnitExperience
 -- variables
 
 local targets = {}
+local tLength = 0
+
 local blockAllyTeams = {}
 
 if (gadgetHandler:IsSyncedCode()) then
 -- SYNCED
 
-function gadget:Explosion(weaponId, px, py, pz, ownerID)
-	local weapDef = WeaponDefs[weaponId]
+function gadget:Explosion(weaponID, px, py, pz, ownerID)
+	local weapDef = WeaponDefs[weaponID]
+	local cp = weapDef.customParams
+	local fearID = cp.fearid
 	
-	if not weapDef.customParams.fearid then return false end
+	if not fearID then return false end
   
-	local unitsAtSpot = GetUnitsInCylinder(px, pz, weapDef.customParams.fearaoe)
+	local unitsAtSpot = GetUnitsInCylinder(px, pz, cp.fearaoe)
 	
 	-- if the weapon is a howitzer shell reset the gun's experience to 0
-	if weapDef.customParams.howitzer and Spring.ValidUnitID(ownerID) then
+	if cp.howitzer and ValidUnitID(ownerID) then
 		SetUnitExperience(ownerID, 0)
 	end
 	
-	--local fearBlockerFound	= false
 	for i = 1, #unitsAtSpot do
-		local unitId = unitsAtSpot[i]
-		local ud = UnitDefs[GetUnitDefID(unitId)]
+		local unitID = unitsAtSpot[i]
+		local ud = UnitDefs[GetUnitDefID(unitID)]
 		if ud.customParams.blockfear == "1" then
-			blockAllyTeams[GetUnitAllyTeam(unitId)] = true
+			blockAllyTeams[GetUnitAllyTeam(unitID)] = true
 		elseif ud.customParams.feartarget then
-			table.insert(targets, unitId)
+			tLength = tLength + 1
+			targets[tLength] = unitID
 		end
 	end
 	
-	for i = 1, #targets do
-		local unitId = targets[i]
-		if unitId ~= ownerID and not blockAllyTeams[GetUnitAllyTeam(unitId)] then
-			Spring.CallCOBScript(unitId, "HitByWeaponId", 0, 0, 0, weapDef.customParams.fearid, 0)
+	for i = 1, tLength do
+		local unitID = targets[i]
+		if unitID ~= ownerID and not blockAllyTeams[GetUnitAllyTeam(unitID)] then
+			Spring.CallCOBScript(unitID, "HitByWeaponId", 0, 0, 0, fearID, 0)
 		end
-		targets[i] = nil
 	end
-		targets = {}
-		blockAllyTeams = {}
+	-- reset tables
+	targets = {}
+	tLength = 0
+	blockAllyTeams = {}
+	
 	return false
 end
 
