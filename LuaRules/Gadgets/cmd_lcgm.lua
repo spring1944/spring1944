3bee10c49a26a089627713c882bef40c11ced8ce
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-12-30 02:49:13 +0000

    * Function localisations for Gadgets/cmd_lcgm.lua

--- LuaRules/Gadgets/cmd_lcgm.lua
+++ LuaRules/Gadgets/cmd_lcgm.lua
@@ -10,7 +10,30 @@ function gadget:GetInfo()
 	}
 end
 
+if (gadgetHandler:IsSyncedCode()) then
+--SYNCED
+
 -- Localisations
+local DelayCall 			= GG.Delay.DelayCall
+-- MoveCtrl
+local mcDisable				= Spring.MoveCtrl.Disable
+local mcEnable				= Spring.MoveCtrl.Enable
+local mcSetCollideStop		= Spring.MoveCtrl.SetCollideStop
+local mcSetTrackGround		= Spring.MoveCtrl.SetTrackGround
+local mcSetVelocity			= Spring.MoveCtrl.SetVelocity
+-- Synced Read
+local GetGroundHeight		= Spring.GetGroundHeight
+local GetUnitBasePosition	= Spring.GetUnitBasePosition
+-- Synced Ctrl
+local CallCOBScript			= Spring.CallCOBScript
+local EditUnitCmdDesc		= Spring.EditUnitCmdDesc
+local FindUnitCmdDesc		= Spring.FindUnitCmdDesc
+local InsertUnitCmdDesc		= Spring.InsertUnitCmdDesc
+local SetUnitArmored		= Spring.SetUnitArmored
+local SetUnitCOBValue		= Spring.SetUnitCOBValue
+local SetUnitWeaponState	= Spring.SetUnitWeaponState
+-- Unsynced Ctrl
+local SendMessageToTeam		= Spring.SendMessageToTeam
 
 -- Constants
 local CMD_BEACH = 1000
@@ -25,10 +48,6 @@ local BASE_ACCURACY = WeaponDefs[ UnitDefNames["gbrlcgm"].weapons[1].weaponDef ]
 -- Variables
 local activeUnits = {}
 
-if (gadgetHandler:IsSyncedCode()) then
---SYNCED
-
-local DelayCall = GG.Delay.DelayCall
 
 local beachDesc= {
 	name	= "Beach",
@@ -40,46 +59,46 @@ local beachDesc= {
 }
 
 local function EndBeach(unitID, disable)
-	Spring.CallCOBScript(unitID, "StopMoving", 0)
-	Spring.MoveCtrl.SetVelocity(unitID, 0, 0, 0)
+	CallCOBScript(unitID, "StopMoving", 0)
+	mcSetVelocity(unitID, 0, 0, 0)
 	activeUnits[unitID] = nil
 	if disable then -- unit has surfaced
-		Spring.MoveCtrl.Disable(unitID) 
-		--Spring.SetUnitArmored(unitID, false)
-		Spring.SetUnitCOBValue(unitID, COB.ARMORED, 0) -- delete me
-		Spring.SetUnitWeaponState(unitID, 0, {accuracy = BASE_ACCURACY})
-		Spring.SetUnitWeaponState(unitID, 1, {accuracy = BASE_ACCURACY})
+		mcDisable(unitID) 
+		--SetUnitArmored(unitID, false)
+		SetUnitCOBValue(unitID, COB.ARMORED, 0) -- delete me
+		SetUnitWeaponState(unitID, 0, {accuracy = BASE_ACCURACY})
+		SetUnitWeaponState(unitID, 1, {accuracy = BASE_ACCURACY})
 	else -- unit is grounded
 		-- 3rd param won't work till next Spring version, and the callin is broken till then anyway!
-		--Spring.SetUnitArmored(unitID, true, ARMOUR_MULTIPLE) 
+		--SetUnitArmored(unitID, true, ARMOUR_MULTIPLE) 
 		-- so do this for now instead
-		Spring.SetUnitCOBValue(unitID, COB.ARMORED, 1)
-		Spring.SetUnitWeaponState(unitID, 0, {accuracy = BASE_ACCURACY * ACCURACY_MULT})
-		Spring.SetUnitWeaponState(unitID, 1, {accuracy = BASE_ACCURACY * ACCURACY_MULT})
+		SetUnitCOBValue(unitID, COB.ARMORED, 1)
+		SetUnitWeaponState(unitID, 0, {accuracy = BASE_ACCURACY * ACCURACY_MULT})
+		SetUnitWeaponState(unitID, 1, {accuracy = BASE_ACCURACY * ACCURACY_MULT})
 	end
 end
 
 local function Beach(unitID, groundHeight)
-	Spring.CallCOBScript(unitID, "EmitWakes", 0)
-	Spring.MoveCtrl.Enable(unitID)
-	Spring.MoveCtrl.SetVelocity(unitID, 0, SINK_RATE, 0)
+	CallCOBScript(unitID, "EmitWakes", 0)
+	mcEnable(unitID)
+	mcSetVelocity(unitID, 0, SINK_RATE, 0)
 	if groundHeight >= ACTUAL_MIN_DEPTH then
-		Spring.MoveCtrl.SetTrackGround(unitID, true)
-		Spring.MoveCtrl.SetCollideStop(unitID, true)
+		mcSetTrackGround(unitID, true)
+		mcSetCollideStop(unitID, true)
 	else
 		DelayCall(EndBeach, {unitID, false}, SINK_TIME)
 	end
 end
 
 local function UnBeach(unitID)
-	Spring.CallCOBScript(unitID, "EmitWakes", 0)
-	Spring.MoveCtrl.SetVelocity(unitID, 0, -SINK_RATE, 0)
+	CallCOBScript(unitID, "EmitWakes", 0)
+	mcSetVelocity(unitID, 0, -SINK_RATE, 0)
 	DelayCall(EndBeach, {unitID, true}, SINK_TIME)
 end
 
 function gadget:UnitCreated(unitID, unitDefID, teamID)
 	if unitDefID == UnitDefNames["gbrlcgm"].id then -- switch to custom param?
-		Spring.InsertUnitCmdDesc(unitID, beachDesc)
+		InsertUnitCmdDesc(unitID, beachDesc)
 	end
 end
 
@@ -90,15 +109,15 @@ end
 function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
 	if cmdID == CMD_BEACH then
 		-- check that this unit has this command
-		local cmdDescID = Spring.FindUnitCmdDesc(unitID, CMD_BEACH)
+		local cmdDescID = FindUnitCmdDesc(unitID, CMD_BEACH)
 		if not cmdDescID then return false end
 		-- check unit is not already un/beaching
 		if activeUnits[unitID] then return false end
 		-- check unit is in shallow enough water
-		local x, _, z = Spring.GetUnitBasePosition(unitID)
-		local groundHeight = Spring.GetGroundHeight(x,z)
+		local x, _, z = GetUnitBasePosition(unitID)
+		local groundHeight = GetGroundHeight(x,z)
 		if groundHeight < MIN_DEPTH then 
-			Spring.SendMessageToTeam(teamID, "Water is too deep to beach here!")
+			SendMessageToTeam(teamID, "Water is too deep to beach here!")
 			return false
 		end
 		if cmdParams[1] == 1 then
@@ -110,15 +129,15 @@ function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpt
 		end
 		--Spring.MoveCtrl.SetLimits(unitID, x, -15, z, x, 15, z)
 		beachDesc.params[1] = cmdParams[1]
-		Spring.EditUnitCmdDesc(unitID, cmdDescID, { params = beachDesc.params})
+		EditUnitCmdDesc(unitID, cmdDescID, { params = beachDesc.params})
 	end
 	return true
 end
 
 function gadget:MoveCtrlNotify(unitID, unitDefID, unitTeam, data)
 	if activeUnits[unitID] then
-		Spring.MoveCtrl.SetTrackGround(unitID, false)
-		Spring.MoveCtrl.SetCollideStop(unitID, false)
+		mcSetTrackGround(unitID, false)
+		mcSetCollideStop(unitID, false)
 		EndBeach(unitID)
 	end
 	return false
