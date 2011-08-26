60f49f2fbe93652a14303b1e6a8c558cdb0268f0
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2011-08-26 16:52:44 +0000

    * Remove missing units from builddefs.lua

--- gamedata/builddefs.lua
+++ gamedata/builddefs.lua
@@ -880,23 +880,7 @@ buildoptions =
   },
 }
 if (modOptions) then
-	if (modOptions.navies) then
-		local tmpNavies = tonumber(modOptions.navies)
-		if tmpNavies > 0 then
-			-- add Light ships
-			table.insert(buildoptions.rusboatyardlarge, "ruspr161")
-			table.insert(buildoptions.rusboatyardlarge, "ruspr7")
-			
-			table.insert(buildoptions.gerboatyardlarge, "gerflottentorpboot")
-			table.insert(buildoptions.gerboatyardlarge, "gertype1934")	
-			
-			table.insert(buildoptions.gbrboatyardlarge, "gbrhuntii")
-			table.insert(buildoptions.gbrboatyardlarge, "gbroclass")
 
-			table.insert(buildoptions.usboatyardlarge, "ustacoma")
-			table.insert(buildoptions.usboatyardlarge, "usfletcher")
-		end
-	end
 end
 
 return buildoptions
