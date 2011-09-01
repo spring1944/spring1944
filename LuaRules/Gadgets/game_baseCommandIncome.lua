5bf3f3342ac94b7ffe8b80c036d01a2dd6fcd508
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2011-09-01 17:37:01 +0000

    * Fix ancient bug in game_baseCommandIncome.lua which caused command income to fluctuate -- thanks Szunti! * USLCSL.fbi 76mm gun no longer fires at AIR category -- 4 bofors and 4 oerlikons is enough AA dontcha think * Rejig transportMass of inf boat transports so they play more nicely with the healthbars widget

--- LuaRules/Gadgets/game_baseCommandIncome.lua
+++ LuaRules/Gadgets/game_baseCommandIncome.lua
@@ -21,7 +21,7 @@ function gadget:Initialize()
 end
 
 function gadget:GameFrame(n)
-	if (n % (1*30) < 0.1) then
+	if (n % (1*32) < 0.1) then
 		for teamID, someThing in pairs(team) do
 			Spring.AddTeamResource(teamID, "m", 20)
 		end
