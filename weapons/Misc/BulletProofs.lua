82c18a8bac5ca847603e5e5ddb5ec4c6450afc61
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2011-09-12 23:59:08 +0000

    * Make shields invisible again!

--- weapons/Misc/BulletProofs.lua
+++ weapons/Misc/BulletProofs.lua
@@ -10,7 +10,7 @@ local BulletProofClass = Weapon:New{
   shieldRepulser     = true,
   weaponType         = [[Shield]],
   
-  visibleShield = true,
+  --visibleShield = true,
 }
 
 -- Implementations
