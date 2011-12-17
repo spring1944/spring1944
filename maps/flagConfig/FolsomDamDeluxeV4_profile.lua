9e89d120156d1eb5fe69eb8eb4d330424c2e7f6a
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2011-12-17 18:32:04 +0000

    A few new flag configs, thanks to JAL.

---
+++ maps/flagConfig/FolsomDamDeluxeV4_profile.lua
@@ -0,0 +1,54 @@
+local resources = {
+	{
+		x = 1412,
+		z = 1937,
+		feature = nil
+	},
+	{
+		x = 1535,
+		z = 3667,
+		feature = nil
+	},
+	{
+		x = 807,
+		z = 5381,
+		feature = nil
+	},
+	{
+		x = 5044,
+		z = 5300,
+		feature = nil
+	},
+	{
+		x = 4983,
+		z = 3585,
+		feature = nil
+	},
+	{
+		x = 9167,
+		z = 1868,
+		feature = nil
+	},
+	{
+		x = 7414,
+		z = 5527,
+		feature = nil
+	},
+		{
+		x = 2771,
+		z = 4750,
+		feature = nil
+	},
+		{
+		x = 9138,
+		z = 6303,
+		feature = nil
+	},
+	{
+		x = 8641,
+		z = 3627,
+		feature = nil
+	},
+}
+
+return resources
