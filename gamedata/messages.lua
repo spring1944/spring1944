c8c0a800d83f71e8963de3abc9f375fb91746ab1
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2011-12-29 14:21:46 +0000

    Fixed syntax in messages.lua, fixed fire arc on ParaBren, and added map edge widget.

--- gamedata/messages.lua
+++ gamedata/messages.lua
@@ -1,9 +1,17 @@
 local messages = {
-	["Team%i(%s) is no more"] = {
-		"Team%i(%s) has been routed",
-		"Team%i(%s) has been driven from the combat area",
-		"Team%i(%s) has been eliminated",
-		"Team%i(%s) surrendered to your forces",
+	["Team %i (lead by %s) is no more"] = {
+		"%s has been routed.",
+		"%s has been driven from the combat area.",
+		"%s has been eliminated.",
+		"%s has been liquidated.",
+		"%s surrendered to your forces.",
+	},
+	
+	["Player %i (%s) resigned and is now spectating!"] = {
+		"%s resigned their command.",
+		"%s has left their forces to another commander."
+		"%s abandoned their remaining troops."
+	
 	}
 }
 
