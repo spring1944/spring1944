2f433cee98431931e02895ab653c47ce53a4ea22
FLOZi <FLOZi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2012-01-02 22:15:09 +0000

    * Force mines to be non-blocking so units no longer try to path around them! * General cleanup of gadgets/game_minefields.lua

--- LuaRules/Gadgets/game_minefields.lua
+++ LuaRules/Gadgets/game_minefields.lua
@@ -4,7 +4,7 @@ function gadget:GetInfo()
 		desc		= "Randomly spawns mines in a small area when an engineer builds one.",
 		author		= "Nemo (B. Tyler), FLOZi (C. Lawrence)",
 		date		= "December 30, 2008",
-		license		 = "Public domain",
+		license		= "GNU GPL v2",
 		layer		= 0,
 		enabled	= true	--	loaded by default?
 	}
@@ -18,17 +18,26 @@ local GetUnitsInCylinder	= Spring.GetUnitsInCylinder
 local GetGroundHeight		= Spring.GetGroundHeight
 -- Synced Ctrl
 local CreateUnit			= Spring.CreateUnit
+local SetUnitBlocking		= Spring.SetUnitBlocking
+local SetUnitCloak			= Spring.SetUnitCloak
+local TransferUnit			= Spring.TransferUnit
+
 -- Constants
 local GAIA_TEAM_ID 			= Spring.GetGaiaTeamID()
 
-local APMineNumber			= 16
-local APMineSpread			= 25
-
-local ATMineNumber			= 5
-local ATMineSpread			= 23
-
-local APMineMinDist			= 2.5
-local ATMineMinDist			= 1.5
+local mineTypes = {}
+mineTypes["apminesign"] = {
+	number = 16,
+	spread = 25,
+	minDist = 2.5,
+	fieldSize = 5,
+}
+mineTypes["atminesign"] = {
+	number = 5,
+	spread = 23,
+	minDist = 1.5,
+	fieldSize = 3,
+}
 
 if gadgetHandler:IsSyncedCode() then
 --	SYNCED
@@ -36,44 +45,32 @@ local DelayCall = GG.Delay.DelayCall
 
 function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 	local ud = UnitDefs[unitDefID]
-	if ud.name == "apminesign" then		
+	local mineData = mineTypes[ud.name]
+	if mineData then		
 		local x, y, z = GetUnitPosition(unitID)
 		local mineCount = 0
-		while mineCount < APMineNumber do
-			local xpos = (math.random(-5, 5) * APMineSpread) + x
-			local zpos = (math.random(-5, 5) * APMineSpread) + z
-			while #GetUnitsInCylinder(xpos, zpos, APMineMinDist, GAIA_TEAM_ID) > 0 do
-				xpos = (math.random(-5, 5) * APMineSpread) + x
-				zpos = (math.random(-5, 5) * APMineSpread) + z
+		while mineCount < mineData.number do
+			local xpos = (math.random(-mineData.fieldSize, mineData.fieldSize) * mineData.spread) + x
+			local zpos = (math.random(-mineData.fieldSize, mineData.fieldSize) * mineData.spread) + z
+			while #GetUnitsInCylinder(xpos, zpos, mineData.minDist, GAIA_TEAM_ID) > 0 do -- This could well be slow >_>
+				xpos = (math.random(-mineData.fieldSize, mineData.fieldSize) * mineData.spread) + x
+				zpos = (math.random(-mineData.fieldSize, mineData.fieldSize) * mineData.spread) + z
 			end
 			local ypos = GetGroundHeight(xpos, zpos)
-			CreateUnit("apmine", xpos, ypos, zpos, 0, GAIA_TEAM_ID) --unitTeam
+			local mineID = CreateUnit("apmine", xpos, ypos, zpos, 0, GAIA_TEAM_ID)
+			SetUnitBlocking(mineID, false, false, false)
 			mineCount = mineCount + 1
 		end
-		DelayCall(Spring.TransferUnit, {unitID, GAIA_TEAM_ID}, 1)
-	end
-	
-	if ud.name == "atminesign" then		
-		local x, y, z = GetUnitPosition(unitID)
-		local mineCount = 0
-		while mineCount < ATMineNumber do
-			local xpos = (math.random(-3, 3) * ATMineSpread) + x
-			local zpos = (math.random(-3, 3) * ATMineSpread) + z
-			while #GetUnitsInCylinder(xpos, zpos, ATMineMinDist, GAIA_TEAM_ID) > 0 do
-				xpos = (math.random(-3, 3) * ATMineSpread) + x
-				zpos = (math.random(-3, 3) * ATMineSpread) + z
-			end
-			CreateUnit("atmine", xpos, y, zpos, 0, GAIA_TEAM_ID)
-			mineCount = mineCount + 1
-		end
-		DelayCall(Spring.TransferUnit, {unitID, GAIA_TEAM_ID}, 1)
+		 -- DelayCall needed to fix the notify widget as unsynced can't find gaia units!
+		DelayCall(TransferUnit, {unitID, GAIA_TEAM_ID}, 1)
 	end
 end
 
 function gadget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
 	local ud = UnitDefs[unitDefID]
-	if ud.name == "apminesign" or ud.name == "atminesign" and unitTeam == GAIA_TEAM_ID then
-		Spring.SetUnitCloak(unitID, true)
+	local mineData = mineTypes[ud.name]
+	if mineData and unitTeam == GAIA_TEAM_ID then
+		SetUnitCloak(unitID, true)
 	end
 end
 
