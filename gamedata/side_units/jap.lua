b0bcc78756158d396d15a8757d271e336563ef49
s44-yuritch <s44-yuritch@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2013-04-04 20:34:11 +0000

    (AdditionalSides mod) Separated build defs per side. Need to do the same to armordefs and maybe a few more tables

---
+++ gamedata/side_units/jap.lua
@@ -0,0 +1,165 @@
+local japunits =
+{
+  --- JAPAN
+    japhqengineer =
+  {
+    "japvehicleyard",
+    "japbarracks",
+	"japtent",
+    "japgunyard",
+    "japstorage",
+    "japradar",
+    "atminesign",
+    "apminesign",
+    "tankobstacle",
+    "gersturmboot",
+    "pontoonraft",
+  },
+
+	japriki =
+	{
+		"japbarracks",
+		"japgunyard",
+		"japvehicleyard",
+		"japradar",
+		"japtankyard",
+		"japstorage",
+		"japsupplydepot",
+		"tankobstacle",    
+	},
+	japhq =
+	{
+		"jap_sortie_recon",
+		"japhqengineer",
+		"jap_platoon_hq"
+	},
+	japbarracks =
+	{
+		"japhqengineer",
+		"jap_platoon_rifle",
+		"jap_platoon_assault",
+		"jap_platoon_mg",
+		"jap_platoon_at",
+		"jap_platoon_sniper",
+		"jap_platoon_mortar",
+		"japisuzutx40",
+		"jappontoontruck",
+		"japtype4mortar_mobile",
+	},
+	japtent =
+	{
+		"japhqengineer",
+		"jap_platoon_rifle",
+	},
+	japvehicleyard =
+	{
+		"japriki",
+		"japisuzutx40",
+		"japhoha",
+		"japisuzutype94_aa",
+		"japteke",
+		"japhago",
+		"japhonii",
+	},
+	japvehyard_landing =
+	{
+		"japriki",
+		"japisuzutx40",
+		"japhoha",
+		"japisuzutype94_aa",
+		"japteke",
+		"japhago",
+		"japhonii",
+		"japkatsu",
+		"japkami",
+		"jappontoontruck",
+	},
+	japvehyard_tank =
+	{
+		"japriki",
+		"japisuzutx40",
+		"japhoha",
+		"japisuzutype94_aa",
+		"japteke",
+		"japhago",
+		"japhonii",
+		"japchiha",
+	},
+	japgunyard =
+	{
+		"japriki",
+		"japisuzutx40",
+		"japtype98_20mm_truck",
+		"japtype1_47mm_truck",
+		"japtype90_75mm_truck",
+		"japtype91_105mm_truck",
+	},
+	japatyard =
+	{
+		"japriki",
+		"japisuzutx40",
+		"japtype98_20mm_truck",
+		"japtype1_47mm_truck",
+		"japtype90_75mm_truck",
+		"japtype91_105mm_truck",
+		"japhoniiii",
+	},
+	japspyard =
+	{
+		"japriki",
+		"japisuzutx40",
+		"japtype98_20mm_truck",
+		"japtype1_47mm_truck",
+		"japtype90_75mm_truck",
+		"japtype91_105mm_truck",
+		"japhoniii",
+	},
+	japtankyard =
+	{
+		"japriki",
+		"japhago",
+		"japshinhotochiha",
+		"japchihe",
+		"japhoro",
+	},
+	japtankyard_medium =
+	{
+		"japriki",
+		"japhago",
+		"japshinhotochiha",
+		"japchihe",
+		"japhoro",
+		"japchinu",
+	},
+	japtankyard_support =
+	{
+		"japriki",
+		"japhago",
+		"japshinhotochiha",
+		"japchihe",
+		"japhoro",
+		"japchiha120mm",
+	},
+	japradar =
+	{
+		"jap_sortie_recon",
+		"jap_sortie_interceptor",
+		"jap_sortie_fighter",
+		"jap_sortie_divebomber",
+		"jap_sortie_at",
+	},
+	japboatyard =
+	{
+		"rubberdingy",
+		"pontoonraft",
+		"japdaihatsu",
+		"japabtei",
+	},
+	japsupplydepot =
+	{
+		"japisuzutx40",
+		"japhoha",
+	},
+}
+
+return japunits
