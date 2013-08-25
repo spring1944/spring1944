609792cdafb70136091243ac39a5b786b30bc04e
flozi <flozi@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2013-08-25 13:04:57 +0000

    * Fix gamedata/armordefs.lua for 95.0+

--- gamedata/armordefs.lua
+++ gamedata/armordefs.lua
@@ -523,15 +523,6 @@ local armorDefs = {
   },
 }
 
--- convert to named maps  (trepan is a noob)
-for categoryName, categoryTable in pairs(armorDefs) do
-  local t = {}
-  for _, unitName in pairs(categoryTable) do
-    t[unitName] = 1
-  end
-  armorDefs[categoryName] = t
-end
-
 local system = VFS.Include('gamedata/system.lua')  
 
 return system.lowerkeys(armorDefs)
