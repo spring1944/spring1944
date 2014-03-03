5e2d8a42e8843c08f7fd56aaebae8d42ad450db2
nemonobody <nemonobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2014-03-03 00:34:48 +0000

    Snipers are now subclasses of Rifle. Also fix ordering of tags in PTRD weapondef.

--- weapons/Infantry/Rifles.lua
+++ weapons/Infantry/Rifles.lua
@@ -15,6 +15,7 @@ local RifleClass = Weapon:New{
   fireStarter        = 0,
   id                 = 2, -- used for cob based fear from rifle/smg
   impactonly         = 1,
+  impulsefactor      = 0.1,
   intensity          = 0.9,
   interceptedByShieldType = 8,
   laserFlareSize     = 0.0001,
@@ -93,6 +94,57 @@ local PartisanMosinNagant = MosinNagant:New{
   movingAccuracy     = 1800,
 }
 
+-- Sniper Rifle Base Class
+local SniperRifleClass = RifleClass:New{
+  accuracy           = 0,
+  explosionGenerator = [[custom:Bullet]],
+  movingAccuracy     = 1777,
+  range              = 1040,
+  reloadtime         = 10,
+  soundTrigger       = false,
+  tolerance          = 2000,
+  turret             = true,
+  weaponType         = [[LaserCannon]],
+  customparams = {
+    damagetype         = [[smallarm]],
+    fearaoe            = 90,
+    fearid             = 401,
+  },
+  damage = {
+    default              = 625,
+    infantry             = 1700,
+    sandbags             = 325,
+  },
+}
+
+-- Implementations
+
+-- SMLE No. 4 Mk. I (T) (GBR)
+local Enfield_T = SniperRifleClass:New{
+  name               = [[Lee-Enfield No. 4 Mk. I Scoped]],
+  reloadtime         = 8.5,
+  soundStart         = [[GBR_Enfield]],
+}
+
+-- Karabiner 98K Scope (GER)
+local K98kScope = SniperRifleClass:New{
+  name               = [[Karabiner 98k Scoped]],
+  soundStart         = [[GER_K98K]],
+}
+
+-- M1903A4 Springfield (USA)
+local M1903Springfield = SniperRifleClass:New{
+  movingAccuracy     = 888, -- intended?
+  name               = [[M1903A4 Springfield]],
+  soundStart         = [[US_Springfield]],
+}
+
+-- Mosin Nagant M1890/30 PU (RUS)
+local MosinNagantPU = SniperRifleClass:New{
+  name               = [[Mosin-Nagant PU Scoped]],
+  soundStart         = [[RUS_MosinNagant]],
+}
+
 -- Return only the full weapons
 return lowerkeys({
   Enfield = Enfield,
@@ -101,4 +153,9 @@ return lowerkeys({
   BAR = BAR,
   MosinNagant = MosinNagant,
   PartisanMosinNagant = PartisanMosinNagant,
+  -- sniper weapons
+  Enfield_T = Enfield_T,
+  K98kScope = K98kScope,
+  M1903Springfield = M1903Springfield,
+  MosinNagantPU = MosinNagantPU,
 })
