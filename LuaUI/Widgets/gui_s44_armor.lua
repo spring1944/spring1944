0c933f4adbb08c70a8c6a68fa81a8e7e065dba4d
yuritch <s44.yuritch@gmail.com>
2014-05-26 22:59:39 +0300

    Copied gui_armor from AdditionalSides as it is better there - it properly shows HEAT+AP config

--- LuaUI/Widgets/gui_s44_armor.lua
+++ LuaUI/Widgets/gui_s44_armor.lua
@@ -39,6 +39,7 @@ local font
 local GetMouseState = Spring.GetMouseState
 local TraceScreenRay = Spring.TraceScreenRay
 local GetUnitDefID = Spring.GetUnitDefID
+local GetWeaponDefID = Spring.GetWeaponDefID
 local GetUnitPosition = Spring.GetUnitPosition
 local GetFeaturePosition = Spring.GetFeaturePosition
 local GetUnitVectors = Spring.GetUnitVectors
@@ -71,7 +72,7 @@ local GL_LINES = GL.LINES
 --local functions
 ------------------------------------------------
 local function GetArmorColor(t)
-  return {2 - 2 * t / maxArmor, 2 * t / maxArmor, 0}
+	return {2 - 2 * t / maxArmor, 2 * t / maxArmor, 0}
 end
 
 ------------------------------------------------
@@ -79,160 +80,175 @@ end
 ------------------------------------------------
 
 function widget:Initialize()
-  for unitDefID=1, #UnitDefs do
-    local unitDef = UnitDefs[unitDefID]
+	for unitDefID=1, #UnitDefs do
+		local unitDef = UnitDefs[unitDefID]
 
-    local penetration, dropoff
-    for i=1,#unitDef.weapons do
-      local weapon = unitDef.weapons[i]
-      local weaponDef = WeaponDefs[weapon.weaponDef]
-      local customParams = weaponDef.customParams
+		local penetration, dropoff, radiusrange, penetrationheat
+		for i=1,#unitDef.weapons do
+			local weapon = unitDef.weapons[i]
+			local weaponDef = WeaponDefs[weapon.weaponDef]
+			local customParams = weaponDef.customParams
+			local range = weaponDef.range
 
-	  if (customParams.damagetype ~= "grenade") then
-        if (tonumber(customParams.armor_penetration) or 0) > (penetration or 0) then
-          local armor_penetration = customParams.armor_penetration
-          local armor_penetration_1000m = customParams.armor_penetration_1000m or armor_penetration
-          penetration = tonumber(armor_penetration)
-          dropoff = log(armor_penetration_1000m / armor_penetration) / 1000
-        elseif (tonumber(customParams.armor_penetration_100m) or 0) > (penetration or 0) then
-          local armor_penetration_100m = customParams.armor_penetration_100m
-          local armor_penetration_1000m = customParams.armor_penetration_1000m or armor_penetration_100m
-          penetration = (armor_penetration_100m / armor_penetration_1000m) ^ (1/9) * armor_penetration_100m
-          dropoff = log(armor_penetration_1000m / armor_penetration_100m) / 900
+			if (customParams.damagetype ~= "grenade") then
+				if (customParams.damagetype == "shapedcharge") then 
+					local armor_penetration = customParams.armor_penetration
+					penetrationheat = tonumber(armor_penetration)
+					radiusrange = range
+				else
+					if (tonumber(customParams.armor_penetration) or 0) > (penetration or 0) then
+						local armor_penetration = customParams.armor_penetration
+						local armor_penetration_1000m = customParams.armor_penetration_1000m or armor_penetration
+						penetration = tonumber(armor_penetration)
+						dropoff = log(armor_penetration_1000m / armor_penetration) / 1000
+					elseif (tonumber(customParams.armor_penetration_100m) or 0) > (penetration or 0) then
+						local armor_penetration_100m = customParams.armor_penetration_100m
+						local armor_penetration_1000m = customParams.armor_penetration_1000m or armor_penetration_100m
+						penetration = (armor_penetration_100m / armor_penetration_1000m) ^ (1/9) * armor_penetration_100m
+						dropoff = log(armor_penetration_1000m / armor_penetration_100m) / 900
+					end
+				end
+			end
 		end
-      end
-    end
 
-    if penetration then
-      infos[unitDefID] = {penetration, dropoff}
-    end
-  end
+		if penetration or penetrationheat then
+			infos[unitDefID] = {penetration, dropoff, radiusrange, penetrationheat}
+		end
+	end
 
-  font = WG.S44Fonts.TypewriterBold32
+	font = WG.S44Fonts.TypewriterBold32
 end
 
 function widget:DrawWorld()
-  local mx, my = GetMouseState()
-  local mouseTargetType, mouseTarget = TraceScreenRay(mx, my)
-  local selectedUnit = GetSelectedUnits()[1]
+	local mx, my = GetMouseState()
+	local mouseTargetType, mouseTarget = TraceScreenRay(mx, my)
+	local selectedUnit = GetSelectedUnits()[1]
 
-  if mouseTargetType == "unit" then
-	local udid = GetUnitDefID(mouseTarget)
-	if (not udid) then return end  -- may happen when hovering over enemy radar dot
-    local unitDef = UnitDefs[udid]
-    local customParams = unitDef.customParams
+	if mouseTargetType == "unit" then
+		local udid = GetUnitDefID(mouseTarget)
+		if (not udid) then return end	-- may happen when hovering over enemy radar dot
+		local unitDef = UnitDefs[udid]
+		local customParams = unitDef.customParams
 
-    if customParams.armor_front then
-      local armor_front = customParams.armor_front
-      local armor_side = customParams.armor_side or armor_front
-      local armor_rear = customParams.armor_rear or armor_side
-      local armor_top = customParams.armor_top or armor_rear
-      local tx, ty, tz = GetUnitPosition(mouseTarget)
-      local frontdir, updir, rightdir = GetUnitVectors(mouseTarget)
+		if customParams.armor_front then
+			local armor_front = customParams.armor_front
+			local armor_side = customParams.armor_side or armor_front
+			local armor_rear = customParams.armor_rear or armor_side
+			local armor_top = customParams.armor_top or armor_rear
+			local tx, ty, tz = GetUnitPosition(mouseTarget)
+			local frontdir, updir, rightdir = GetUnitVectors(mouseTarget)
 
-      local diagdir1 = {
-        (frontdir[1] + rightdir[1]) / SQRT2,
-        (frontdir[2] + rightdir[2]) / SQRT2,
-        (frontdir[3] + rightdir[3]) / SQRT2,
-      }
+			local diagdir1 = {
+				(frontdir[1] + rightdir[1]) / SQRT2,
+				(frontdir[2] + rightdir[2]) / SQRT2,
+				(frontdir[3] + rightdir[3]) / SQRT2,
+			}
 
-      local diagdir2 = {
-        (frontdir[1] - rightdir[1]) / SQRT2,
-        (frontdir[2] - rightdir[2]) / SQRT2,
-        (frontdir[3] - rightdir[3]) / SQRT2,
-      }
+			local diagdir2 = {
+				(frontdir[1] - rightdir[1]) / SQRT2,
+				(frontdir[2] - rightdir[2]) / SQRT2,
+				(frontdir[3] - rightdir[3]) / SQRT2,
+			}
 
-      local vertices = {
-        {v = {diagdir1[1] * closeDist, diagdir1[2] * closeDist, diagdir1[3] * closeDist}},
-        {v = {diagdir1[1] * farDist, diagdir1[2] * farDist, diagdir1[3] * farDist}},
-        {v = {diagdir2[1] * closeDist, diagdir2[2] * closeDist, diagdir2[3] * closeDist}},
-        {v = {diagdir2[1] * farDist, diagdir2[2] * farDist, diagdir2[3] * farDist}},
-        {v = {-diagdir1[1] * closeDist, -diagdir1[2] * closeDist, -diagdir1[3] * closeDist}},
-        {v = {-diagdir1[1] * farDist, -diagdir1[2] * farDist, -diagdir1[3] * farDist}},
-        {v = {-diagdir2[1] * closeDist, -diagdir2[2] * closeDist, -diagdir2[3] * closeDist}},
-        {v = {-diagdir2[1] * farDist, -diagdir2[2] * farDist, -diagdir2[3] * farDist}},
-      }
+			local vertices = {
+				{v = {diagdir1[1] * closeDist, diagdir1[2] * closeDist, diagdir1[3] * closeDist}},
+				{v = {diagdir1[1] * farDist, diagdir1[2] * farDist, diagdir1[3] * farDist}},
+				{v = {diagdir2[1] * closeDist, diagdir2[2] * closeDist, diagdir2[3] * closeDist}},
+				{v = {diagdir2[1] * farDist, diagdir2[2] * farDist, diagdir2[3] * farDist}},
+				{v = {-diagdir1[1] * closeDist, -diagdir1[2] * closeDist, -diagdir1[3] * closeDist}},
+				{v = {-diagdir1[1] * farDist, -diagdir1[2] * farDist, -diagdir1[3] * farDist}},
+				{v = {-diagdir2[1] * closeDist, -diagdir2[2] * closeDist, -diagdir2[3] * closeDist}},
+				{v = {-diagdir2[1] * farDist, -diagdir2[2] * farDist, -diagdir2[3] * farDist}},
+			}
 
-      glLineWidth(lineWidth)
-      glSmoothing(false, smooth, false)
+			glLineWidth(lineWidth)
+			glSmoothing(false, smooth, false)
 
-      glPushMatrix()
-        glTranslate(tx, ty, tz)
-        glColor(1, 1, 1)
-        glShape(GL_LINES, vertices)
+			glPushMatrix()
+				glTranslate(tx, ty, tz)
+				glColor(1, 1, 1)
+				glShape(GL_LINES, vertices)
 
-        glColor(GetArmorColor(armor_front))
-        glPushMatrix()
-          glTranslate(frontdir[1] * dist, frontdir[2] * dist, frontdir[3] * dist)
-          glBillboard()
-          font:Print(armor_front .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
-        glPopMatrix()
+				glColor(GetArmorColor(armor_front))
+				glPushMatrix()
+					glTranslate(frontdir[1] * dist, frontdir[2] * dist, frontdir[3] * dist)
+					glBillboard()
+					font:Print(armor_front .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
+				glPopMatrix()
 
-        glColor(GetArmorColor(armor_side))
-        glPushMatrix()
-          glTranslate(rightdir[1] * dist, rightdir[2] * dist, rightdir[3] * dist)
-          glBillboard()
-          font:Print(armor_side .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
-        glPopMatrix()
+				glColor(GetArmorColor(armor_side))
+				glPushMatrix()
+					glTranslate(rightdir[1] * dist, rightdir[2] * dist, rightdir[3] * dist)
+					glBillboard()
+					font:Print(armor_side .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
+				glPopMatrix()
 
-        glPushMatrix()
-          glTranslate(-rightdir[1] * dist, -rightdir[2] * dist, -rightdir[3] * dist)
-          glBillboard()
-          font:Print(armor_side .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
-        glPopMatrix()
+				glPushMatrix()
+					glTranslate(-rightdir[1] * dist, -rightdir[2] * dist, -rightdir[3] * dist)
+					glBillboard()
+					font:Print(armor_side .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
+				glPopMatrix()
 
-        glColor(GetArmorColor(armor_rear))
-        glPushMatrix()
-          glTranslate(-frontdir[1] * dist, -frontdir[2] * dist, -frontdir[3] * dist)
-          glBillboard()
-          font:Print(armor_rear .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
-        glPopMatrix()
+				glColor(GetArmorColor(armor_rear))
+				glPushMatrix()
+					glTranslate(-frontdir[1] * dist, -frontdir[2] * dist, -frontdir[3] * dist)
+					glBillboard()
+					font:Print(armor_rear .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
+				glPopMatrix()
 
-        glColor(GetArmorColor(armor_top))
-        glPushMatrix()
-          glTranslate(updir[1] * dist / 2, updir[2] * dist / 2, updir[3] * dist / 2)
-          glBillboard()
-          font:Print(armor_top .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
-        glPopMatrix()
+				glColor(GetArmorColor(armor_top))
+				glPushMatrix()
+					glTranslate(updir[1] * dist / 2, updir[2] * dist / 2, updir[3] * dist / 2)
+					glBillboard()
+					font:Print(armor_top .. "mm", 0, -fontSizeWorld / 2, fontSizeWorld, "nc")
+				glPopMatrix()
 
-      glPopMatrix()
+			glPopMatrix()
 
-      glLineWidth(1)
-      glSmoothing(false, false, false)
-    end
-  end
+			glLineWidth(1)
+			glSmoothing(false, false, false)
+		end
+	end
 end
 
 function widget:DrawScreen()
-  local selectedUnit = GetSelectedUnits()[1]
+	local selectedUnit = GetSelectedUnits()[1]
 
-  if selectedUnit then
-    local _, cmd, _ = GetActiveCommand()
-    if cmd == CMD_ATTACK then
-      local unitDefID = GetUnitDefID(selectedUnit)
-      if infos[unitDefID] then
-        local mx, my = GetMouseState()
-        local mouseTargetType, mouseTarget = TraceScreenRay(mx, my)
-        local tx, ty, tz
+	if selectedUnit then
+		local _, cmd, _ = GetActiveCommand()
+		if cmd == CMD_ATTACK then
+			local unitDefID = GetUnitDefID(selectedUnit)
+			if infos[unitDefID] then
+				local mx, my = GetMouseState()
+				local mouseTargetType, mouseTarget = TraceScreenRay(mx, my)
+				local tx, ty, tz
 
-        if mouseTargetType == "unit" then
-          tx, ty, tz = GetUnitPosition(mouseTarget)
-        elseif mouseTargetType == "feature" then
-          tx, ty, tz = GetFeaturePosition(mouseTarget)
-        elseif mouseTargetType == "ground" then
-          tx, ty, tz = mouseTarget[1], mouseTarget[2], mouseTarget[3]
-        else
-          return
-        end
+				if mouseTargetType == "unit" then
+					tx, ty, tz = GetUnitPosition(mouseTarget)
+				elseif mouseTargetType == "feature" then
+					tx, ty, tz = GetFeaturePosition(mouseTarget)
+				elseif mouseTargetType == "ground" then
+					tx, ty, tz = mouseTarget[1], mouseTarget[2], mouseTarget[3]
+				else
+					return
+				end
 
-        local ux, uy, uz = GetUnitPosition(selectedUnit)
-        local penetration, dropoff = infos[unitDefID][1], infos[unitDefID][2]
-        local dist = vMagnitude(ux - tx, uy - ty, uz - tz)
-        penetration = penetration * exp(dropoff * dist)
-
-        glColor(GetArmorColor(penetration))
-        font:Print(strFormat("%.0fmm", penetration), mx + 16, my - fontSizeScreen / 2, fontSizeScreen, "n")
-      end
-    end
-  end
+				local ux, uy, uz = GetUnitPosition(selectedUnit)
+				local penetration, dropoff, radiusrange, penetrationheat = infos[unitDefID][1], infos[unitDefID][2], infos[unitDefID][3], infos[unitDefID][4]
+				local dist = vMagnitude(ux - tx, uy - ty, uz - tz)
+ 
+				if radiusrange and radiusrange > dist then 
+					glColor(GetArmorColor(penetrationheat)) 
+					font:Print(strFormat("%.0fmm", penetrationheat), mx + 16, my - fontSizeScreen / 2, fontSizeScreen, "n")
+					font:Print("HEAT", mx + 16, - 16 + my - fontSizeScreen / 2, fontSizeScreen, "n")
+				else
+					if dropoff then 
+						penetration = penetration * exp(dropoff * dist)
+						glColor(GetArmorColor(penetration)) 
+						font:Print(strFormat("%.0fmm", penetration), mx + 16, my - fontSizeScreen / 2, fontSizeScreen, "n")
+					end
+				end
+			end
+		end
+	end
 end
