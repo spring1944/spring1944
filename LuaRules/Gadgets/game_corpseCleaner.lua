148980f6550fec9f61f28681b33fec71e2c87a47
Craig Lawrence <flozi@blueyonder.co.uk>
2014-05-27 22:46:24 +0100

    * Rewrite corpse cleaner gadget -- Was the worst gadget by far in profiler

--- LuaRules/Gadgets/game_corpseCleaner.lua
+++ LuaRules/Gadgets/game_corpseCleaner.lua
@@ -2,8 +2,8 @@ function gadget:GetInfo()
 	return {
 		name      = "Corpse cleaner",
 		desc      = "Removes infantry corpses over time",
-		author    = "Gnome, tweaked by Nemo",
-		date      = "June 2008",
+		author    = "Rewritten by FLOZi (C.Lawrence)",
+		date      = "27/05/2014",
 		license   = "CC by-nc, version 3.0",
 		layer     = 0,
 		enabled   = true
@@ -12,28 +12,14 @@ end
 
 if (gadgetHandler:IsSyncedCode()) then
 
-function gadget:GameFrame(n)
-	if(n % 30 < 1) then
-		local features = Spring.GetAllFeatures()
-		for _,fid in ipairs(features) do
-			local fdid = Spring.GetFeatureDefID(fid)
-			local fname = FeatureDefs[fdid].name
-			local fmetal = FeatureDefs[fdid].metal
-			if fname then
-				if fname == "ussoldier_dead" or fname == "russoldier_dead" or fname == "ger_shoulderhit" or fname == "gersoldier_dead" or fname == "gbrsoldier_dead" then
-					local fhp, fmaxhp = Spring.GetFeatureHealth(fid)
-					local subtract = fmaxhp * 0.01 --always make it take about 90 seconds regardless of the feature
-					local fhp = fhp - subtract
-					if(fhp ~= nil) then
-						if(fhp <= 0) then
-							Spring.DestroyFeature(fid)
-						else
-							Spring.SetFeatureHealth(fid,fhp)
-						end
-					end
-				end
-			end
-		end
+local CLEAR_TIME = 90 * 30 -- 90sec
+local DelayCall = GG.Delay.DelayCall
+
+function gadget:FeatureCreated(featureID, featureAllyID)
+	local featureDefID = Spring.GetFeatureDefID(featureID)
+	local fName = FeatureDefs[featureDefID].name
+	if fName:find("soldier") then
+		DelayCall(Spring.DestroyFeature, {featureID}, CLEAR_TIME)
 	end
 end
 
