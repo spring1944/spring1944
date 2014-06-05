d4d4b3d4f0d8261c2aeaf89916f16f4c38908412
Craig Lawrence <flozi@blueyonder.co.uk>
2014-06-05 20:25:17 +0100

    * Be harsher

--- LuaRules/Gadgets/player_banRacists.lua
+++ LuaRules/Gadgets/player_banRacists.lua
@@ -10,24 +10,32 @@ function gadget:GetInfo()
 	}
 end
 
+local racists = {["zCram"] = true}
+
 if (gadgetHandler:IsSyncedCode()) then
 -- SYNCED
 
-local racists = {["zCram"] = true}
-
-function gadget:GameFrame(n)
+--[[function gadget:GameFrame(n)
 	if n == 10 then -- wait a few frames for lua setup
 		local playerList = Spring.GetPlayerList()
 		for _, playerID in pairs(playerList) do
 			local playerName, _, _, teamID = Spring.GetPlayerInfo(playerID)
 			if racists[playerName] then
 				Spring.SendMessageToTeam(teamID, "There is a racist on your team, goodbye.")
-				Spring.KillTeam(teamID)
+				--Spring.KillTeam(teamID)
 			end
 		end
 	end
-end
+end]]
 
 else
 -- UNSYNCED
+
+function gadget:Update() 
+	local playerName = Spring.GetPlayerInfo(Spring.GetMyPlayerID())
+	if racists[playerName] then
+		Spring.SendCommands("quit")
+	end
+end
+
 end
