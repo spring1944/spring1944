66dee453141a592e273c01438ede85c68c1554ba
Eshed <ashdnazg@gmail.com>
2014-06-05 22:08:30 +0300

    moved squads and sorties to auto-generation

--- LuaRules/configs/side_squad_defs/ita.lua
+++ LuaRules/configs/side_squad_defs/ita.lua
@@ -11,6 +11,12 @@ local itaDefs = {
 		"itarifle",
 		"itarifle",
 		"itarifle",
+		name = "Rifle Squad",
+		description = "5 x Carcano M91 Rifle, 1 x M38 SMG: Small Combat Squad",
+		buildCostMetal = 580,
+		buildPic = "ITARifle.png",
+		buildTime = 580,
+		side = "ITA",
 	},
 	
 	["ita_platoon_rifle"] = 
@@ -25,6 +31,12 @@ local itaDefs = {
 		"itarifle",
 		"itarifle",
 		"itabreda30",
+		name = "Carcano Rifle Platoon",
+		description = "8 x Carcano M91 Rifle, 1 x Solothurn Anti-Tank, 1 x Breda 30 Light Machinegun: Long-Range Combat Platoon",
+		buildCostMetal = 1410,
+		buildPic = "ITARifle.png",
+		buildTime = 1410,
+		side = "ITA",
 	},
 	
 	["ita_platoon_assault"] = 
@@ -39,6 +51,12 @@ local itaDefs = {
 		"itam38",
 		"itam38",
 		"itasoloat",
+		name = "Assault Platoon",
+		description = "9 x M38 SMG, 1 x Solothurn Anti-Tank: Close-Quarters Assault Infantry",
+		buildCostMetal = 1400,
+		buildPic = "ITAM38.png",
+		buildTime = 1400,
+		side = "ITA",
 	},
 	
 	["ita_platoon_mg"] = 
@@ -47,12 +65,24 @@ local itaDefs = {
 		"itamg",
 		"itabreda30",
 		"itaobserv",
+		name = "Machinegun Squad",
+		description = "2 x M37 Machinegun, 1 x Breda 30 Light Machinegun, 1 x Scout: Infantry Fire Support Squad",
+		buildCostMetal = 1200,
+		buildPic = "ITAM37.png",
+		buildTime = 1200,
+		side = "ITA",
 	},
 	
 	["ita_platoon_sniper"] = 
 	{
 		"itasniper",
 		"itaobserv",
+		name = "Sniper Team",
+		description = "1 x Carcano M91 Sniper, 1 x Spotter: Long-Range Fire Support",
+		buildCostMetal = 1140,
+		buildPic = "ITASniper.png",
+		buildTime = 1140,
+		side = "ITA",
 	},
 	
 	["ita_platoon_mortar"] = 
@@ -61,6 +91,12 @@ local itaDefs = {
 		"itaobserv",
 		"itamortar",
 		"itamortar",
+		name = "Mortar Team",
+		description = "3 x 81/14 Mortar, 1 x Spotter: Heavy Infantry Fire Support",
+		buildCostMetal = 1580,
+		buildPic = "ITAMortar.png",
+		buildTime = 1580,
+		side = "ITA",
 	},
 	
 	["ita_platoon_at"] = 
@@ -69,11 +105,23 @@ local itaDefs = {
 		"itasoloat",
 		"itasoloat",
 		"itaelitesoloat",
+		name = "Anti-Tank Squad",
+		description = "3 x Solothurn + 1 x Elitesolothurn  : Anti-Tank Infantry",
+		buildCostMetal = 800,
+		buildPic = "ITASolothurn.png",
+		buildTime = 900,
+		side = "ITA",
 	},
 	["ita_platoon_infgun"] =
 	{
 		"itacannone65",
 		"itaobserv",
+		name = "Infantry Gun Team",
+		description = "1 x Cannone da 65/17, 1 x Spotter: Long-Range Fire Support",
+		buildCostMetal = 1150,
+		buildPic = "ITACannone65.png",
+		buildTime = 1150,
+		side = "ITA",
 	},
 	["ita_platoon_bersaglieri"] =
 	{
@@ -90,6 +138,12 @@ local itaDefs = {
 		"itaelitesoloat",
 		"itabreda30",
 		"itaobserv",
+		name = "Bersaglieri Combat Platoon",
+		description = "8 x Bersaglieri Rifles, 2 x Bersaglieri SMG Troopers, 1 x Elitesolothurn, 1 x Bersaglieri Machinegunner, 1 x Bersaglieri Scout: Bersaglieri Assault Marksmen",
+		buildCostMetal = 2000,
+		buildPic = "ITAbersaglieri.png",
+		buildTime = 2000,
+		side = "ITA",
 	},
   	["ita_platoon_carabinieri"] =
 	{
@@ -103,6 +157,12 @@ local itaDefs = {
 		"itacarabinieri",
 		"itacarabinieri",
 		"itacarabinieri",
+		name = "Black Brigade Anti-Partisan Platoon",
+		description = "10 x Carabinieri: Anti-Partisan Platoon",
+		buildCostMetal = 1500,
+		buildPic = "ITARifle.png",
+		buildTime = 800,
+		side = "ITA",
 	},
 	["ita_platoon_alpini"] =
 	{
@@ -120,6 +180,12 @@ local itaDefs = {
 		"itaalpinirifle",
 		"itaalpinirifle",
 		"itaobserv",
+		name = "Alpini Mountain Division",
+		description = "4 x Alpini Rifles, 8 x Alpini SMG Troopers, 1 x Alpini Mortar , 1 x Alpini Scout: Alpini Mountain Squad",
+		buildCostMetal = 2300,
+		buildPic = "itaalpini.png",
+		buildTime = 2300,
+		side = "ITA",
 	},
 }
 
--- LuaRules/configs/side_squad_defs/jap.lua
+++ LuaRules/configs/side_squad_defs/jap.lua
@@ -11,6 +11,12 @@ local japDefs = {
 		"japrifle",
 		"japtype100smg",
 		"japkneemortar",
+		name = "Arisaka Rifle Platoon",
+		description = "8 x Arisaka Type 99 Rifle, 1 x Type 100 SMG, 1 x Knee Mortar: Long-Range Combat Platoon",
+		buildCostMetal = 1500,
+		buildPic = "JAPRifle.png",
+		buildTime = 1500,
+		side = "JAP",
 	},
 	["jap_platoon_hq"] =
 	{
@@ -20,6 +26,12 @@ local japDefs = {
 		"japrifle",
 		"japrifle",
 		"japtype100smg",
+		name = "Rifle Squad",
+		description = "5 x Arisaka Type 99, 1 x Type 100 SMG: Small Combat Squad",
+		buildCostMetal = 600,
+		buildPic = "JAPRifle.png",
+		buildTime = 600,
+		side = "JAP",
 	},
 	["jap_platoon_assault"] =
 	{
@@ -33,6 +45,12 @@ local japDefs = {
 		"japtype100smg",
 		"japtype100smg",
 		"japtype99lmg",
+		name = "Assault Platoon",
+		description = "9 x Type 99 SMG, 1 x Type 99 LMG: Close-Quarters Assault Infantry",
+		buildCostMetal = 1400,
+		buildPic = "JAPType100SMG.png",
+		buildTime = 1400,
+		side = "JAP",
 	},
 	["jap_platoon_mg"] =
 	{
@@ -40,11 +58,23 @@ local japDefs = {
 		"japtype99lmg",
 		"japtype92hmg",
 		"japobserv",
+		name = "Machinegun Squad",
+		description = "2 x Type 99 Machinegun, 1 x Type 98 Heavy Machinegun, 1 x Scout: Infantry Fire Support Squad",
+		buildCostMetal = 1200,
+		buildPic = "JAPType99LMG.png",
+		buildTime = 1200,
+		side = "JAP",
 	},
 	["jap_platoon_sniper"] =
 	{
 		"japsniper",
 		"japobserv",
+		name = "Sniper Team",
+		description = "1 x Arisaka Type 99 Sniper, 1 x Spotter: Long-Range Fire Support",
+		buildCostMetal = 1140,
+		buildPic = "JAPSniper.png",
+		buildTime = 1140,
+		side = "JAP",
 	},
 	["jap_platoon_mortar"] =
 	{
@@ -52,12 +82,24 @@ local japDefs = {
 		"japmortar",
 		"japmortar",
 		"japobserv",
+		name = "Mortar Team",
+		description = "3 x Type 97 81mm Mortar, 1 x Spotter: Heavy Infantry Fire Support",
+		buildCostMetal = 1580,
+		buildPic = "JAPMortar.png",
+		buildTime = 1580,
+		side = "JAP",
 	},
 	["jap_platoon_at"] =
 	{
 		"japtype4at",
 		"japtype3at",
 		"japtype3at",
+		name = "Anti-Tank Squad",
+		description = "2 x Type 3 AT Grenade, 1 x Type 4 Rocket Launcher: Anti-Tank Infantry",
+		buildCostMetal = 800,
+		buildPic = "JAPType3AT.png",
+		buildTime = 900,
+		side = "JAP",
 	},
 }
 
