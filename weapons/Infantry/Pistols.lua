0f0f21cd6b74878bf6d51f2277c4762553b6c18e
Eshed <ashdnazg@gmail.com>
2014-09-02 18:51:46 +0300

    Got rid of pistols

--- weapons/Infantry/Pistols.lua
+++ weapons/Infantry/Pistols.lua
@@ -70,10 +70,27 @@ local TT33 = PistolClass:New{
   },
 }
 
+-- Beretta M1934 (ITA)
+local BerettaM1934 = PistolClass:New{
+  name               = [[Beretta M1934]],
+  reloadtime         = 1, -- intended?
+  soundStart         = [[ITA_BerettaM34]],
+  sprayAngle         = 50, -- intended?
+}
+
+local NambuType14 = PistolClass:New{
+  name               = [[Nambu Type 14 8mm]],
+  reloadtime         = 1, -- intended?
+  soundStart         = [[ITA_BerettaM34]],
+  sprayAngle         = 50, -- intended?
+}
+
 -- Return only the full weapons
 return lowerkeys({
   Webley = Webley,
   WaltherP38 = WaltherP38,
   M1911A1Colt = M1911A1Colt,
   TT33 = TT33,
+  BerettaM1934 = BerettaM1934,
+  NambuType14 = NambuType14,
 })
