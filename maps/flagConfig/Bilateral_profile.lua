c1e55080b9063a5e88ddbb1fb6d03d906c5a73b0
jal76 <jal1976@googlemail.com>
2014-09-21 10:46:53 +0200

    fixed jpn pontoon truck geting stuck in vehicleyard for landing,added Bilateral mapflag config

---
+++ maps/flagConfig/Bilateral_profile.lua
@@ -0,0 +1,167 @@
+local resources = {
+	{
+		x = 2203,
+		z = 2178,
+		feature = nil
+	},
+	{
+		x = 2597,
+		z = 2182,
+		feature = nil
+	},
+	{
+		x = 2176,
+		z = 2519,
+		feature = nil
+	},
+	{
+		x = 2561,
+		z = 2538,
+		feature = nil
+	},
+	{
+		x = 4359,
+		z = 5987,
+		feature = nil
+	},
+	{
+		x = 4713,
+		z = 6355,
+		feature = nil
+	},
+	{
+		x = 979,
+		z = 8097,
+		feature = nil
+	},
+	{
+		x = 996,
+		z = 10262,
+		feature = nil
+	},
+	{
+		x = 785,
+		z = 14235,
+		feature = nil
+	},
+	{
+		x = 1236,
+		z = 14257,
+		feature = nil
+	},
+	{
+		x = 801,
+		z = 14773,
+		feature = nil
+	},
+	{
+		x = 1232,
+		z = 14781,
+		feature = nil
+	},
+	{
+		x = 2383,
+		z = 15835,
+		feature = nil
+	},
+	{
+		x = 4696,
+		z = 11152,
+		feature = nil
+	},
+	{
+		x = 5194,
+		z = 11723,
+		feature = nil
+	},
+
+	{
+		x = 7589,
+		z = 12261,
+		feature = nil
+	},
+	{
+		x = 11175,
+		z = 4662,
+		feature = nil
+	},
+	{
+		x = 11679,
+		z = 5219,
+		feature = nil
+	},
+	{
+		x = 11760,
+		z = 7656,
+		feature = nil
+	},
+
+	{
+		x = 13964,
+		z = 554,
+		feature = nil
+	},
+	{
+		x = 15117,
+		z = 1606,
+		feature = nil
+	},
+	{
+		x = 15573,
+		z = 1561,
+		feature = nil
+	},
+	{
+		x = 15140,
+		z = 2119,
+		feature = nil
+	},
+	{
+		x = 15605,
+		z = 2125,
+		feature = nil
+	},
+	{
+		x = 15339,
+		z = 6099,
+		feature = nil
+	},
+	{
+		x = 15387,
+		z = 8306,
+		feature = nil
+	},
+	{
+		x = 13808,
+		z = 13864,
+		feature = nil
+	},
+	{
+		x = 14203,
+		z = 13843,
+		feature = nil
+	},
+	{
+		x = 13785,
+		z = 14191,
+		feature = nil
+	},
+	{
+		x = 14184,
+		z = 14175,
+		feature = nil
+	},
+	{
+		x = 11670,
+		z = 10046,
+		feature = nil
+	},
+	{
+		x = 12025,
+		z = 10406,
+		feature = nil
+	},
+	
+}
+
+return resources
