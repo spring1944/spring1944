0502296a3c70bc3d8fdc94f3ae762ea07cb65eee
unknown <C:\The Bat!>
2014-10-08 14:43:54 +0300

    Made composite ships have proper flags, exhaust smoke and damage smoke. Fixed wakes on them.

--- scripts/BoatMother.lua
+++ scripts/BoatMother.lua
@@ -1,5 +1,6 @@
 local unitDefID = Spring.GetUnitDefID(unitID)
 local teamID = Spring.GetUnitTeam(unitID)
+local GetUnitHealth = Spring.GetUnitHealth
 unitDefID = Spring.GetUnitDefID(unitID)
 unitDef = UnitDefs[unitDefID]
 info = GG.lusHelper[unitDefID]
@@ -18,7 +19,8 @@ local function findPieces(input, name)
 	for pieceName, pieceNum in pairs(pieceMap) do
 		local index = pieceName:find(name)
 		if index then
-			local num = tonumber(pieceName:sub(index + string.len(name), -1))
+			-- add a condition for unnumbered child which would be the first and only
+			local num = tonumber(pieceName:sub(index + string.len(name), -1)) or 1
 			input[num] = piece(pieceName)
 		end
 	end
@@ -28,10 +30,75 @@ local childrenPieces = {} -- {[1] = unitname, etc}
 findPieces(childrenPieces, "child")
 local wakes = {}
 findPieces(wakes, "wake")
+local exhausts = {}
+findPieces(exhausts, "exhaust")
 local torps = {}
 findPieces(torps, "torp")
 local tpTurrets = {}
 findPieces(tpTurrets, "tpturret")
+
+local function FlagFlap()
+	local flags = {}
+	findPieces(flags, "flag")
+	if #flags == 0 then
+		return
+	end
+	if flags[1] then
+		Turn(flags[1], y_axis, math.rad(180));
+	end
+	local _,_,_,_,buildProgress = GetUnitHealth(unitID)
+	while (buildProgress > 0) do
+		Sleep(150)
+		_,_,_,_,buildProgress = GetUnitHealth(unitID)
+	end
+	local FLAG_FLAP_ANGLE = math.rad(30)
+	local FLAG_FLAP_SPEED = math.rad(45)
+	local FLAG_FLAP_PERIOD = 2000 * FLAG_FLAP_ANGLE / FLAG_FLAP_SPEED
+	local direction = 1
+	while true do
+		direction = -1 * direction
+		local direction2 = direction
+		local correction_past_first = 1
+		for i = 1, #flags do
+			local angle = FLAG_FLAP_ANGLE * direction2
+			Turn(flags[i], y_axis, angle * correction_past_first, FLAG_FLAP_SPEED * correction_past_first)
+			direction2 = -1 * direction2
+			if i == 1 then
+				correction_past_first = 2
+			end
+		end
+		Sleep(FLAG_FLAP_PERIOD)
+	end
+end
+
+local function DamageSmoke()
+	-- emit some smoke if the unit is damaged
+	-- check if the unit has finished building
+	_,_,_,_,buildProgress = GetUnitHealth(unitID)
+	while (buildProgress > 0) do
+		Sleep(150)
+		_,_,_,_,buildProgress = GetUnitHealth(unitID)
+	end
+	-- random delay between smoke start
+	timeDelay = math.random(1, 5)*30
+	Sleep(timeDelay)
+	while (1 == 1) do
+		curHealth, maxHealth = GetUnitHealth(unitID)
+		healthState = curHealth / maxHealth
+		if healthState < 0.66 then
+			EmitSfx(base, SFX.BLACK_SMOKE)
+			-- the less HP we have left, the more often the smoke
+			timeDelay = 500 * healthState
+			-- no sence to make a delay shorter than a game frame
+			if timeDelay < 30 then
+				timeDelay = 30
+			end
+		else
+			timeDelay = 500
+		end
+		Sleep(timeDelay)
+	end
+end
 
 function script.Create()
 	local x,y,z = Spring.GetUnitPosition(unitID) -- strictly needed?
@@ -40,14 +107,18 @@ function script.Create()
 		Spring.UnitScript.AttachUnit(childrenPieces[i], childID)
 		Hide(childrenPieces[i])
 	end
-	
+	StartThread(DamageSmoke)
+	StartThread(FlagFlap)
 end
 
 local function EmitWakes()
 	SetSignalMask(SIG_MOVE)
 	while true do
-		for wake in pairs(wakes) do
-			EmitSfx(wake, 2)
+		for _, wake in pairs(wakes) do
+			EmitSfx(wake, SFX.WAKE)
+		end
+		for _, exhaust in pairs(exhausts) do
+			EmitSfx(exhaust, SFX.BLACK_SMOKE)
 		end
 		Sleep(300)
 	end
