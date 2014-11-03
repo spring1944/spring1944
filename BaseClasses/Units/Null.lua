c90e524415db30ed24a6ee5ced86ea09cae2d521
Craig Lawrence <flozi@blueyonder.co.uk>
2014-11-03 17:48:46 +0000

    * Fix 'squads don't respect factory orders'

--- BaseClasses/Units/Null.lua
+++ BaseClasses/Units/Null.lua
@@ -1,6 +1,7 @@
 -- Null Unit --
 -- Used as base for sorties, squads, etc
 local Null = Unit:New{
+	canMove = true, -- required to pass orders
 	category = "FLAG",
 	explodeAs = "noweapon",
 	footprintX = 1,
@@ -8,7 +9,7 @@ local Null = Unit:New{
 	idleAutoHeal = 0,
 	maxDamage = 1e+06,
 	maxVelocity = 0.01,
-	--movementClass = "KBOT_Infantry",
+	movementClass = "KBOT_Infantry", -- as is this
 	objectName = "MortarShell.S3O",
 	script = "null.cob",
 	selfDestructAs = "noweapon",
