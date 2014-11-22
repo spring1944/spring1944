a46ea20ce896b60eac4c5e9d8f3b4659226aa9dc
Eshed <ashdnazg@gmail.com>
2014-11-22 17:56:03 +0200

    transported units now aren't autotargeted and also can't see. fixes #56

--- LuaRules/Gadgets/unit_hideTransported.lua
+++ LuaRules/Gadgets/unit_hideTransported.lua
@@ -10,26 +10,56 @@ function gadget:GetInfo()
   }
 end
 
+
+if (gadgetHandler:IsSyncedCode()) then
+
 -- Unsynced Ctrl
-local SetUnitNoDraw			= Spring.SetUnitNoDraw
+local SetUnitNoDraw         = Spring.SetUnitNoDraw
 -- Synced Read
-local GetUnitDefID			= Spring.GetUnitDefID
-local GetUnitPosition 		= Spring.GetUnitPosition
-local GetUnitTransporter 	= Spring.GetUnitTransporter
-local GetUnitsInCylinder 	= Spring.GetUnitsInCylinder
+local GetUnitDefID          = Spring.GetUnitDefID
+local GetUnitTeam           = Spring.GetUnitTeam
+local GetUnitPosition       = Spring.GetUnitPosition
+local GetUnitTransporter    = Spring.GetUnitTransporter
+local GetUnitsInCylinder    = Spring.GetUnitsInCylinder
+local GetUnitSensorRadius   = Spring.GetUnitSensorRadius
 -- Synced Ctrl
-local GiveOrderToUnit		= Spring.GiveOrderToUnit
+local GiveOrderToUnit       = Spring.GiveOrderToUnit
+local SetUnitNeutral        = Spring.SetUnitNeutral
+local SetUnitSensorRadius   = Spring.SetUnitSensorRadius
 
 -- Constants
 local CMD_LOAD_ONTO = CMD.LOAD_ONTO
 local CMD_STOP = CMD.STOP
+local LOS_TYPES = {"los", "airLos", "radar", "sonar", "seismic", "radarJammer", "sonarJammer"}
 -- Variables
 local massLeft = {}
 local toBeLoaded = {}
+local savedRadius = {}
 
-if (gadgetHandler:IsSyncedCode()) then
 
 local DelayCall = GG.Delay.DelayCall
+
+local function StoreLOSRadius(unitID, unitDefID)
+	if not savedRadius[unitDefID] then
+		radiusArray = {}
+		for i, losType in pairs(LOS_TYPES) do
+			radiusArray[i] = GetUnitSensorRadius(unitID, losType)
+			SetUnitSensorRadius(unitID, losType, 0)
+		end
+		savedRadius[unitDefID] = radiusArray
+	else
+		for i, losType in pairs(LOS_TYPES) do
+			SetUnitSensorRadius(unitID, losType, 0)
+		end
+	end
+end
+
+local function RestoreLOSRadius(unitID, unitDefID)
+	radiusArray = savedRadius[unitDefID]
+	for i, losType in pairs(LOS_TYPES) do
+		SetUnitSensorRadius(unitID, losType, radiusArray[i])
+	end
+end
 
 function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions)
 	if cmdID == CMD_LOAD_ONTO then
@@ -76,6 +106,8 @@ function gadget:UnitLoaded(unitID, unitDefID, unitTeam, transportID, transportTe
 		if unitDef.xsize == 2 and not (transportDef.minWaterDepth > 0) and not unitDef.customParams.hasturnbutton then 
 			-- transportee is Footprint of 1 (doubled by engine) and transporter is not a boat and transportee is not an infantry gun
 			SetUnitNoDraw(unitID, true)
+			SetUnitNeutral(unitID, true)
+			StoreLOSRadius(unitID, unitDefID)
 		end
 	end
 	Spring.SetUnitNoMinimap(unitID, true)
@@ -88,9 +120,19 @@ function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID)
 	massLeft[transportID] = massLeft[transportID] + unitDef.mass
 	if unitDef.xsize == 2 and not (transportDef.minWaterDepth > 0) and not unitDef.customParams.hasturnbutton then 
 		SetUnitNoDraw(unitID, false)
+		SetUnitNeutral(unitID, false)
+		RestoreLOSRadius(unitID, unitDefID)
 	end
 	DelayCall(Spring.SetUnitVelocity, {unitID, 0, 0, 0}, 16)
 	Spring.SetUnitNoMinimap(unitID, false)
+end
+
+function gadget:Initialize()
+	for _, unitID in ipairs(Spring.GetAllUnits()) do
+		local unitTeam = GetUnitTeam(unitID)
+		local unitDefID = GetUnitDefID(unitID)
+		gadget:UnitCreated(unitID, unitDefID, unitTeam)
+	end
 end
 
 else
