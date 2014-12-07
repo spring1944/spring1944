d2ff538388cc29eb8dfcd67f0402049e2be4dc0c
Craig Lawrence <flozi@blueyonder.co.uk>
2014-12-07 20:23:55 +0000

    * OO binocSpot, flag & Buoy (lol) as GameMechanics.lua

---
+++ units/GameMechanics.lua
@@ -0,0 +1,38 @@
+local BinocSpot = Null:New{
+	sightDistance				= 200,
+	yardmap						= "y",
+}
+
+local Flag = Null:New{
+	name				= "Flag",
+	description			= "Command Flag",
+	activateWhenBuilt	= true,
+	extractsMetal		= 0.000292,
+	hideDamage			= true,
+	iconType			= "flag",
+	levelGround			= false,
+	objectName			= "GEN/flag.S3O",
+	script				= "Flag.lua", -- atm defaults to .cob
+	sightDistance		= 0,
+	windGenerator		= 0.00001, -- needed for WindChanged callin
+	yardmap				= "y",
+	customParams = {
+		dontCount			= true,
+		flag				= true,
+	},
+}
+
+local Buoy = Flag:New{ -- One day...
+	name				= "Buoy",
+	description			= "Naval Spawn Point",
+	floater				= true,
+	objectName			= "Gen/buoy.S3O",
+	script				= "buoy.cob", --overwrite flag
+	canMove = false, -- for some reason cannot be true or it won't float?
+}
+
+return lowerkeys({
+	["BinocSpot"] = BinocSpot,
+	["Flag"] = Flag,
+	["Buoy"] = Buoy,
+})
