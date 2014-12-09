92073191b4515d9db8f2c6d435758ac840d6f75f
Craig Lawrence <flozi@blueyonder.co.uk>
2014-12-09 22:52:15 +0000

    * DUKW OO * Fix a couple of typos re: CEGs in unitdef sfx tables

--- units/us/vehicles/USGMCTruck.lua
+++ units/us/vehicles/USGMCTruck.lua
@@ -7,7 +7,21 @@ local US_GMCTruckBase = Truck:New{
 local US_GMCTruck = US_GMCTruckBase:New(TransportTruck)
 local US_PontoonTruck = US_GMCTruckBase:New(PontoonTruck, true)
 
+local US_DUKW = US_GMCTruck:New{
+	name					= "DUKW",
+	description				= "Amphibious Transport Truck",
+	buildCostMetal			= 500,
+	canHover				= true, --TODO: required?
+	collisionVolumeScales	= [[16 15 57]],
+	collisionVolumeOffsets	= [[0 -3 0]],
+	corpse					= "usgmctruck_destroyed", -- TODO: DUKW corpse
+	maxDamage				= 650,
+	movementClass			= "HOVER_AmphibTruck",
+	script					= "<NAME>.cob", -- overwrite transporttruck.cob
+}
+
 return lowerkeys({
 	["USGMCTruck"] = US_GMCTruck,
 	["USPontoonTruck"] = US_PontoonTruck,
+	["USDUKW"] = US_DUKW,
 })
