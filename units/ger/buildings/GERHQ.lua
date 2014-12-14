0a5aeebd1614edd5c22c980f63d58d1c32682aab
Eshed <ashdnazg@gmail.com>
2014-12-14 02:03:22 +0200

    Updating GERHQ here too. We will need to solve the warning though.

--- units/ger/buildings/GERHQ.lua
+++ units/ger/buildings/GERHQ.lua
@@ -1,12 +1,13 @@
 local GER_HQ = HQ:New{
 	name					= "Wehrmacht HQ Bunker",
+	buildTime				= 40000,
 	collisionVolumeScales	= [[150 30 110]],
 	collisionVolumeOffsets	= [[0 -13 0]],
 	footprintX				= 10,
 	footprintZ				= 10,
 	maxDamage				= 10625,
 	yardmap					= [[oooooooooo 
-							    oooooooooo 
+								oooooooooo 
 								ooyyyyyyoo 
 								ooyyyyyyoo 
 								ooyyyyyyoo 
