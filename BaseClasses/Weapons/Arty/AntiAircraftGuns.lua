992889015edbb8ba83899c7bc2d45ed68e166293
Craig Lawrence <flozi@blueyonder.co.uk>
2015-01-18 19:03:10 +0000

    * Rip out remaining weapon base classes -- next step; ensure all have relevant cegflare for LUS and implement targetting categories here

---
+++ BaseClasses/Weapons/Arty/AntiAircraftGuns.lua
@@ -0,0 +1,60 @@
+-- Artillery - Anti Aircraft Guns
+
+-- AA Gun Base Class
+local AAGunClass = Weapon:New{
+  burnblow           = true,
+  collisionSize      = 2,
+  explosionSpeed     = 30,
+  impulseFactor      = 0,
+  intensity          = 0.9,
+  predictBoost       = 0, -- this seems very strange for an AA weapon!
+  size               = 1e-5,
+  soundHitDry        = [[GEN_Explo_Flak1]],
+  soundStart         = [[GEN_37mmAA]],
+  sprayAngle         = 400,
+  turret             = true,
+  weaponType         = [[Cannon]],
+  customparams = {
+    damagetype         = [[explosive]],
+	cegflare           = "XSMALL_MUZZLEFLASH",
+	flareonshot        = true,
+  },
+  
+}
+
+-- AA Round Class
+local AAGunAAClass = Weapon:New{
+  areaOfEffect       = 60,
+  canattackground    = false,
+  collisionSize      = 5,
+  cylinderTargeting  = 2.5,
+  edgeEffectiveness  = 0.001,
+  explosionGenerator = [[custom:HE_Medium]],
+  name               = [[AA Shell]],
+  tolerance          = 1400,
+  customparams = {
+    no_range_adjust    = true,
+    fearaoe            = 450,
+    fearid             = 701,
+  }
+}
+
+-- HE Round Class
+local AAGunHEClass = Weapon:New{
+  areaOfEffect       = 24,
+  edgeEffectiveness  = 0.25,
+  explosionGenerator = [[custom:HE_XSmall]],
+  name               = [[HE Shell]],
+  sprayAngle         = 0,
+  tolerance          = 700,
+  customparams = {
+    fearaoe            = 45,
+    fearid             = 301,
+  },
+}
+
+return {
+  AAGunClass = AAGunClass,
+  AAGunHEClass = AAGunHEClass,
+  AAGunAAClass = AAGunAAClass,
+}
---
+++ BaseClasses/Weapons/Arty/Mortars.lua
@@ -0,0 +1,54 @@
+-- Artillery - Mortars
+
+-- Mortar Base Class
+local MortarClass = Weapon:New{
+  accuracy           = 485,
+  avoidFeature		 = false,
+  collideFriendly    = false,
+  edgeEffectiveness  = 0.2,
+  explosionGenerator = [[custom:HE_Medium]],
+  explosionSpeed     = 30,
+  impulseFactor      = 1e-07,
+  leadLimit          = 500,
+  model              = [[MortarShell.S3O]],
+  reloadtime         = 12,
+  size               = 1.5,
+  soundHitDry        = [[GEN_Explo_3]],
+  soundStart         = [[GEN_Mortar]],
+  turret             = true,
+  weaponType         = [[Cannon]],
+  weaponVelocity     = 550,
+  customparams = {
+    armor_hit_side     = [[top]],
+    damagetype         = [[explosive]],
+  },
+}
+
+-- HE Round Class
+local MortarHEClass = Weapon:New{
+  name               = [[HE Shell]],
+  customparams = {
+    fearaoe            = 105,
+    fearid             = 301,
+  },
+}
+
+-- Smoke Round Class
+local MortarSmokeClass = Weapon:New{
+  areaOfEffect       = 20,
+  name               = [[Smoke Shell]],
+  customparams = {
+	smokeradius        = 160,
+	smokeduration      = 25,
+	smokeceg           = [[SMOKESHELL_Small]],
+  },
+  damage = {
+    default = 100,
+  } ,
+}
+
+return {
+  MortarClass = MortarClass,
+  MortarHEClass = MortarHEClass,
+  MortarSmokeClass = MortarSmokeClass,
+}
