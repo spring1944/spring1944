50f17d8f0e7551553fcaa184474a70aad7e2b962
Craig Lawrence <flozi@blueyonder.co.uk>
2015-03-14 00:47:46 +0000

    * Cleanup T-14 for swe

--- units/jpn/ships/JPNT14.lua
+++ units/jpn/ships/JPNT14.lua
@@ -4,32 +4,26 @@ local JPNT14 = BoatMother:New{
 	acceleration			= 0.3,
 	brakeRate				= 0.15,
 	buildCostMetal			= 2200,
-	buildTime				= 2200,
 	collisionVolumeOffsets	= [[0.0 -10.0 -5.0]],
 	collisionVolumeScales	= [[22.0 20.0 85.0]],
 	corpse					= "GERSBoot_dead", -- TODO: needs a corpse (model exists)
-	mass					= 1450,
 	maxDamage				= 1450,
 	maxReverseVelocity		= 3.005,
 	maxVelocity				= 6.01,
 	movementClass			= "BOAT_LightPatrol",
-	objectName				= "JPNT14.s3o",
 	transportCapacity		= 1, -- 1 x 1fpu turrets
 	turnRate				= 205,	
 	weapons = {	
 		[1] = {
 			name				= "RUS533mmTorpedo", -- TODO: a japanese torpedo
-			onlyTargetCategory	= "LARGESHIP", -- TODO: make sure (only) LCT and Gabi (LCGM? LCSL3? MAL?) are LARGESHIP
 			maxAngleDif			= 40,
 		},
 		[2] = {
 			name				= "RUS533mmTorpedo",
-			onlyTargetCategory	= "LARGESHIP",
 			maxAngleDif			= 40,
 		},
 	},
 	customparams = {
-		soundcategory = "JPN/Boat",
 	    maxammo				= 2,
 		weaponcost			= 40,
 		weaponswithammo		= 2,
@@ -45,15 +39,12 @@ local JPNT14 = BoatMother:New{
 local JPNT14_Turret_25mm = BoatChild:New{
 	name					= "25mm Turret",
 	description				= "25mm AA Turret",
-	objectName				= "JPNT14_Turret_25mm.s3o",
   	weapons = {	
 		[1] = {
 			name				= "Type9625mmAA",
-			onlyTargetCategory	= "AIR",
 		},
 		[2] = {
 			name				= "Type9625mmHE",
-			onlyTargetCategory	= "BUILDING INFANTRY SOFTVEH OPENVEH HARDVEH SHIP LARGESHIP DEPLOYED",
 		},
 	},
 	customparams = {
