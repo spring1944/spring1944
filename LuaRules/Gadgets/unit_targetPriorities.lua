4c67713bf3774636c8b54cb86cbb49ddf6c4e64e
Eshed <ashdnazg@gmail.com>
2015-05-18 01:43:47 +0300

    breaking priorities for now

--- LuaRules/Gadgets/unit_targetPriorities.lua
+++ LuaRules/Gadgets/unit_targetPriorities.lua
@@ -18,13 +18,13 @@ if (gadgetHandler:IsSyncedCode()) then
 local lusPriorityCache = {}
 local unitDefIDsPassed = {}
 
-function gadget:AllowWeaponTargetCheck(attackerID, attackerWeaponNum, attackerWeaponDefID)
-	if lusPriorityCache[attackerID] and Spring.GetUnitStates(attackerID).firestate == 2 and 
-			not WeaponDefs[attackerWeaponDefID].noAutoTarget then -- verify we're on fire at will
-		return true
-	end
-	return false
-end
+-- function gadget:AllowWeaponTargetCheck(attackerID, attackerWeaponNum, attackerWeaponDefID)
+	-- if lusPriorityCache[attackerID] and Spring.GetUnitStates(attackerID).firestate == 2 and 
+			-- not WeaponDefs[attackerWeaponDefID].noAutoTarget then -- verify we're on fire at will
+		-- return true
+	-- end
+	-- return false
+-- end
 
 function gadget:AllowWeaponTarget(unitID, targetID, attackerWeaponNum, attackerWeaponDefID, defPriority)
 	if lusPriorityCache[unitID] then
