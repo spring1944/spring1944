3c10c795549a9f0603060d22341e087ff05d9cae
Eshed <ashdnazg@gmail.com>
2015-05-19 04:36:25 +0300

    Various UI changes: - When spectating, selecting a unit will cause you to spectate its team - Added a widget to enable los view by default - Disabled lockCamers by default since it's giving loads of errors

---
+++ LuaUI/Widgets/gui_spectate_selected_team.lua
@@ -0,0 +1,31 @@
+function widget:GetInfo()
+  return {
+    name      = "Spectate selected team",
+    desc      = "Switch to viewing a team based on selected units on spectator mode",
+    author    = "ashdnazg",
+    date      = "19 May 2015",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+local currentTeam
+
+function widget:Update(dt)
+	local units = Spring.GetSelectedUnits()
+	if units and units[1] then
+		local unitTeam = Spring.GetUnitTeam(units[1])
+		if unitTeam ~= currentTeam then
+			Spring.SendCommands("specteam ".. unitTeam)
+			currentTeam = unitTeam
+		end
+	end
+end
+
+function widget:Initialize()
+	local spec = Spring.GetSpectatingState()
+	if not spec then
+		widgetHandler:RemoveWidget()
+	end
+end
