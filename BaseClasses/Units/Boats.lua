8ef115776a7b88c6eeb2a482756adfb6267e4db3
Ben Tyler <benjamin.tyler@gmail.com>
2015-06-03 22:49:08 +0200

    Ships: make turrets even weaker
    Turrets lost some HP, and are now lumped in with unarmouredvehicles for damage
    purposes. This might be too weak for the fully enclosed tank turrets that some
    ships have, but I want them to be really quite weak in general.

--- BaseClasses/Units/Boats.lua
+++ BaseClasses/Units/Boats.lua
@@ -12,7 +12,7 @@ local Boat = Unit:New{ -- used for transports as is
 	selfDestructAs		= "Vehicle_Explosion_Sm",
 	sightDistance		= 840,
 	turninplace			= false,
-	
+
 	customparams = {
 		damageGroup			= 'ships',
 		dontCount			= 1,
@@ -24,7 +24,7 @@ local BoatMother = Boat:New{ -- used for combat boats with multiple turrets
 	iconType			= "gunboat",
 	script				= "BoatMother.lua",
 	usePieceCollisionVolumes	= true,
-		
+
 	-- Transport tags
 	transportSize		= 1, -- assumes footprint of BoatChild == 1
 	isFirePlatform 		= true,
@@ -37,43 +37,44 @@ local BoatMother = Boat:New{ -- used for combat boats with multiple turrets
 local ArmedBoat = BoatMother:New{ }
 
 local BoatChild = Boat:New{ -- a boat turret
-	buildCostMetal		= 1500, -- only used for exp
-	canMove				= true,
-	cantBeTransported	= false,
-	canSelfDestruct 	= false,
-	category 			= "SHIP MINETRIGGER TURRET DEPLOYED",
-	footprintX			= 1,
-	footprintZ 			= 1,
-	iconType			= "turret",
-	idleAutoHeal		= 1,
-	mass				= 10,
-	maxDamage			= 1000,
-	maxVelocity			= 1,
-	movementClass		= "KBOT_Infantry", -- needed!
-	power		        = 20,
-	script				= "BoatChild.lua",
-	
+	buildCostMetal				= 1500, -- only used for exp
+	canMove						= true,
+	cantBeTransported			= false,
+	canSelfDestruct				= false,
+	category					= "SHIP MINETRIGGER TURRET DEPLOYED",
+	footprintX					= 1,
+	footprintZ					= 1,
+	iconType					= "turret",
+	idleAutoHeal				= 1,
+	mass						= 10,
+	maxDamage					= 1000,
+	maxVelocity					= 1,
+	movementClass				= "KBOT_Infantry", -- needed!
+	power						= 20,
+	script						= "BoatChild.lua",
+	useFootPrintCollisionVolume	= true,
+
 	customparams = {
 		child				= true,
-		damageGroup			= 'guns',
+		damageGroup			= 'unarmouredvehicles',
 	}
 }
 
 local EnclosedBoatTurret = BoatChild:New{
-	maxDamage			= 2500,
+	maxDamage			= 1250,
 }
 
 local OpenBoatTurret = BoatChild:New{
-	maxDamage			= 1250,
+	maxDamage			= 625,
 	customparams = {
 		feartarget		= true,
-		fearlimit		= 25, -- default to double inf, open mounts should be 25
+		fearlimit		= 25,
 	}
 }
 
 -- as durable as a fully enclosed, but still suppressible
 local PartiallyEnclosedBoatTurret = OpenBoatTurret:New{
-	maxDamage			= 2500,
+	maxDamage			= 1250,
 }
 
 return {
