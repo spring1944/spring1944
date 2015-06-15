3ed3b6dd27306cdbfda820041c524d3a692483e8
Lowdive <elisantiago.4e@gmail.com>
2015-06-14 20:28:19 -0500

    Revert "Revert "Added new AI exclusive engineers at game start, so that AI can reach a late-game buildorder""
    This reverts commit 925c1056ad334f3f5253fdb2c9a26f16ca5c16bf.

--- units/us/infantry/USInfantry.lua
+++ units/us/infantry/USInfantry.lua
@@ -8,6 +8,9 @@ local USPara = {
 
 local US_HQEngineer = EngineerInf:New(USInf):New{
 	name				= "Field Engineer",
+	canAssist			= true,
+	canReclaim			= true,
+	canRepair			= true,
 }
 
 local US_Rifle = RifleInf:New(USInf):New{
@@ -120,6 +123,7 @@ local US_Paratrooper = Infantry:New{
 return lowerkeys({
 	-- Regular Inf
 	["USHQEngineer"] = US_HQEngineer,
+	["USHQAIEngineer"] = US_HQEngineer:Clone("USHQEngineer"),
 	["USRifle"] = US_Rifle,
 	["USThompson"] = US_Thompson,
 	["USBAR"] = US_BAR,
