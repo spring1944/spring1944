69b00085178b07416d3f37b584fb8945ecd5adc5
yuritch <s44.yuritch@gmail.com>
2015-06-27 00:27:51 +0300

    Changed unit files and scripts (and corpses) to use the new boatyard model

--- scripts/BoatYard.lua
+++ scripts/BoatYard.lua
@@ -1,8 +1,4 @@
 include "Yard.lua"
-local crane = piece "crane"
-local crane2 = piece "crane2"
-local turret = piece "turret"
-local turret2 = piece "turret2"
 local smoke = piece "smoke"
 local smoke2 = piece "smoke2"
 local barge1 = piece "barge1"
@@ -10,18 +6,11 @@ local barge2 = piece "barge2"
 local barge3 = piece "barge3"
 local barge4 = piece "barge4"
 
-local CRANE_TURN_SPEED  = math.rad(5)
-local CRANE_RAISE_ANGLE	= math.rad(-30)
-local CRANE_MIN_POS     = math.rad(80)
-local CRANE_MAX_STEPS   = 20
-local CRANE_STEP_VALUE  = CRANE_MIN_POS / CRANE_MAX_STEPS
 local BARGE_MOVE_DIST   = 6
 local BARGE_MOVE_SPEED  = 3
 local SMOKE_DELAY       = 500
 
-
 local active = false
-
 
 local _Activate = script.Activate
 local _Deactivate = script.Deactivate
@@ -53,7 +42,6 @@ local function SafeEmitSfx(p, ...)
 	end
 end
 
-
 local function Funnels()
 	while true do
 		if active then
@@ -65,38 +53,6 @@ local function Funnels()
 			end
 		end
 		Sleep(SMOKE_DELAY)
-	end
-end
-
-local function CraneMovement1()
-	if not turret then
-		return
-	end
-	local nextCranePos
-	while true do
-		while active do
-			nextCranePos = math.random(0, CRANE_MAX_STEPS)
-			nextCranePos = nextCranePos * CRANE_STEP_VALUE + CRANE_MIN_POS
-			Turn(turret, y_axis,-nextCranePos, CRANE_TURN_SPEED)
-			Sleep(math.random(500, 5000))
-		end
-		Sleep(250)
-	end
-end
-
-local function CraneMovement2()
-	if not turret2 then
-		return
-	end
-	local nextCranePos
-	while true do
-		while active do
-			nextCranePos = math.random(0, CRANE_MAX_STEPS)
-			nextCranePos = nextCranePos * CRANE_STEP_VALUE + CRANE_MIN_POS
-			Turn(turret2, y_axis, math.pi + nextCranePos, CRANE_TURN_SPEED)
-			Sleep(math.random(500, 5000))
-		end
-		Sleep(250)
 	end
 end
 
@@ -130,8 +86,6 @@ end
 function script.Create()
 	_Create()
 	StartThread(Funnels)
-	StartThread(CraneMovement1)
-	StartThread(CraneMovement2)
 end
 
 function script.Activate()
@@ -148,16 +102,10 @@ end
 
 function script.StartBuilding()
 	_StartBuilding()
-	SafeTurn(crane   ,x_axis, CRANE_RAISE_ANGLE, CRANE_TURN_SPEED)
-	SafeTurn(crane2  ,x_axis, -CRANE_RAISE_ANGLE, CRANE_TURN_SPEED)
 	active = true
 end
 
 function script.StopBuilding()
 	_StopBuilding()
-	SafeTurn(crane   ,x_axis, 0, CRANE_TURN_SPEED)
-	SafeTurn(crane2  ,x_axis, 0, CRANE_TURN_SPEED)
-	SafeTurn(turret  ,y_axis, 0, CRANE_TURN_SPEED)
-	SafeTurn(turret2 ,y_axis, 0, CRANE_TURN_SPEED)
 	active = false
 end
