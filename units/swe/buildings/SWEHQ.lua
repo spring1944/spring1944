bd52c24d21fea2fb41819b66913942c719d3533d
yuritch <s44.yuritch@gmail.com>
2016-03-03 20:03:31 +0200

    Corpse setup for SWE HQ building (which is actually an armored car)

--- units/swe/buildings/SWEHQ.lua
+++ units/swe/buildings/SWEHQ.lua
@@ -1,5 +1,6 @@
 local SWE_HQ = HQ:New{
 	name					= "Swedish Army HQ",
+	corpse					= "swepbilm31f_destroyed",
 	footprintX				= 7,
 	footprintZ				= 7,
 	maxDamage				= 10625, -- TODO: Same as GER Bunker! Change me!
