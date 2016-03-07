c78c86057d1b5486740ee471882f5678947cf877
yuritch <s44.yuritch@gmail.com>
2016-03-07 09:54:12 +0200

    Swedish sortie list: only contains recon for now

---
+++ LuaRules/Configs/side_sortie_defs/swe.lua
@@ -0,0 +1,14 @@
+local sweSorties = {
+	swe_sortie_recon = {
+		members = {
+			"swes14b",
+		},
+		delay = 15,
+		name = "Recon Sortie",
+		description = "1 x S 14B Storch Reconnaissance Plane",
+		buildCostMetal = 1000,
+		buildPic = "SWES14B.png",
+	},
+}
+
+return sweSorties
