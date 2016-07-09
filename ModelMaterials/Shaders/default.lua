046b162285db1eaf67ad0ce9f4b8e1beeda10278
yuritch <s44.yuritch@gmail.com>
2016-07-09 20:52:06 +0300

    LuaMaterials file updated for 102, thank you ZK

--- ModelMaterials/Shaders/default.lua
+++ ModelMaterials/Shaders/default.lua
@@ -1,161 +1,211 @@
 return {
-  vertex = [[
-//#define use_normalmapping
-//#define flip_normalmap
-//#define use_shadows
+vertex = [[
+	//#define use_normalmapping
+	//#define flip_normalmap
+	//#define use_shadows
+	%%VERTEX_GLOBAL_NAMESPACE%%
+	
+	uniform mat4 camera;   //ViewMatrix (gl_ModelViewMatrix is ModelMatrix!)
+	uniform vec3 cameraPos;
+	uniform vec3 sunPos;
+	uniform vec3 sunDiffuse;
+	uniform vec3 sunAmbient;
+	uniform vec3 etcLoc;
+	uniform int simFrame;
+	#ifdef flashlights
+		varying float selfIllumMod;
+	#endif
+	//uniform float frameLoc;
+	
+	#ifdef use_treadoffset
+		uniform float treadOffset;
+	#endif
+	
+	//The api_custom_unit_shaders supplies this definition:
+	#ifdef use_shadows  
+		uniform mat4 shadowMatrix;
+		uniform vec4 shadowParams;
+	#endif
+	
+	#ifdef use_vertex_ao
+		varying float aoTerm;
+	#endif
+	varying vec3 cameraDir;
+	
+	#ifdef use_normalmapping
+		varying mat3 tbnMatrix;
+	#else
+		varying vec3 normalv;
+	#endif
 
-    %%VERTEX_GLOBAL_NAMESPACE%%
+	void main(void)
+	{
+		vec4 vertex = gl_Vertex;
+		vec3 normal = gl_Normal;
+		
+		%%VERTEX_PRE_TRANSFORM%%
 
-    uniform mat4 camera;   //ViewMatrix (gl_ModelViewMatrix is ModelMatrix!)
-    //uniform mat4 cameraInv;
-    uniform vec3 cameraPos;
-    uniform vec3 sunPos;
-    uniform vec3 sunDiffuse;
-    uniform vec3 sunAmbient;
-  #ifdef use_shadows
-    uniform mat4 shadowMatrix;
-    varying vec4 shadowpos;
-    #ifndef use_perspective_correct_shadows
-      uniform vec4 shadowParams;
-    #endif
-  #endif
+		#ifdef use_normalmapping
+			vec3 tangent   = gl_MultiTexCoord5.xyz;
+			vec3 bitangent = gl_MultiTexCoord6.xyz;
+			tbnMatrix = gl_NormalMatrix * mat3(tangent, bitangent, normal);
+		#else
+			normalv = gl_NormalMatrix * normal;
+		#endif
 
-    varying vec3 cameraDir;
-    varying vec3 teamColor;
-    //varying float fogFactor;
+		vec4 worldPos = gl_ModelViewMatrix * vertex;
+		gl_Position   = gl_ProjectionMatrix * (camera * worldPos);
+		cameraDir     = worldPos.xyz - cameraPos;
 
-  #ifdef use_normalmapping
-    varying vec3 t;
-    varying vec3 b;
-    varying vec3 n;
-  #else
-    varying vec3 normalv;
-  #endif
+		#ifdef use_shadows
+			gl_TexCoord[1] =shadowMatrix *gl_ModelViewMatrix*gl_Vertex;
+			gl_TexCoord[1].st = gl_TexCoord[1].st * (inversesqrt( abs(gl_TexCoord[1].st) + shadowParams.z) + shadowParams.w) + shadowParams.xy;
+		#endif
+		#ifdef use_treadoffset
+			gl_TexCoord[0].st = gl_MultiTexCoord0.st;
+			if (gl_MultiTexCoord0.s < 0.74951171875 && gl_MultiTexCoord0.s > 0.6279296875 && gl_MultiTexCoord0.t > 0.5702890625 && gl_MultiTexCoord0.t <0.6220703125){
+				gl_TexCoord[0].s = gl_MultiTexCoord0.s + etcLoc.z;
+			}
+		#endif
+		#ifdef use_vertex_ao
+			aoTerm= max(0.4,fract(gl_MultiTexCoord0.s*16384.0)*1.3); // great
+		#endif
 
-    void main(void)
-    {
-      vec4 vertex = gl_Vertex;
-      vec3 normal = gl_Normal;
+		#ifndef use_treadoffset
+			gl_TexCoord[0].st = gl_MultiTexCoord0.st;
+		#endif
+		
+		#ifdef flashlights
+			//float unique_value = sin((gl_ModelViewMatrix[3][0]+gl_ModelViewMatrix[3][2])));
+			selfIllumMod = max(-0.2,sin(simFrame *0.063 + (gl_ModelViewMatrix[3][0]+gl_ModelViewMatrix[3][2])*0.1))+0.2;
+		#endif
+		//float fogCoord = length(gl_Position.xyz); // maybe fog should be readded?
+		//fogFactor = (gl_Fog.end - fogCoord) * gl_Fog.scale; //gl_Fog.scale := 1.0 / (gl_Fog.end - gl_Fog.start)
+		//fogFactor = clamp(fogFactor, 0.0, 1.0);
 
-      %%VERTEX_PRE_TRANSFORM%%
+		%%VERTEX_POST_TRANSFORM%%
+	}
+]],
 
-    #ifdef use_normalmapping
-      vec3 tangent   = gl_MultiTexCoord5.xyz;
-      vec3 bitangent = gl_MultiTexCoord6.xyz;
-      t = gl_NormalMatrix * tangent;
-      b = gl_NormalMatrix * bitangent;
-      n = gl_NormalMatrix * normal;
-    #else
-      normalv = gl_NormalMatrix * normal;
-    #endif
-
-      vec4 worldPos = gl_ModelViewMatrix * vertex;
-      gl_Position   = gl_ProjectionMatrix * (camera * worldPos);
-      cameraDir     = worldPos.xyz - cameraPos;
-
-    #ifdef use_shadows
-      shadowpos = shadowMatrix * worldPos;
-      #ifndef use_perspective_correct_shadows
-        shadowpos.st = shadowpos.st * (inversesqrt(abs(shadowpos.st) + shadowParams.z) + shadowParams.w) + shadowParams.xy;
-      #endif
-    #endif
-
-      gl_TexCoord[0].st = gl_MultiTexCoord0.st;
-      teamColor = gl_TextureEnvColor[0].rgb;
-
-      //float fogCoord = length(gl_Position.xyz);
-      //fogFactor = (gl_Fog.end - fogCoord) * gl_Fog.scale; //gl_Fog.scale := 1.0 / (gl_Fog.end - gl_Fog.start)
-      //fogFactor = clamp(fogFactor, 0.0, 1.0);
-
-      %%VERTEX_POST_TRANSFORM%%
-    }
-  ]],
   fragment = [[
 //#define use_normalmapping
 //#define flip_normalmap
 //#define use_shadows
 
-    %%FRAGMENT_GLOBAL_NAMESPACE%%
+	%%FRAGMENT_GLOBAL_NAMESPACE%%
 
-    uniform sampler2D textureS3o1;
-    uniform sampler2D textureS3o2;
-    uniform samplerCube specularTex;
-    uniform samplerCube reflectTex;
+	uniform sampler2D textureS3o1;
+	uniform sampler2D textureS3o2;
+	uniform samplerCube specularTex;
+	uniform samplerCube reflectTex;
 
-    uniform vec3 sunPos; // is sunDir!
-    uniform vec3 sunDiffuse;
-    uniform vec3 sunAmbient;
+	uniform vec3 sunPos; // is sunDir!
+	uniform vec3 sunDiffuse;
+	uniform vec3 sunAmbient;
+	uniform vec3 etcLoc;
+	#ifndef SPECULARMULT
+		#define SPECULARMULT 2.0
+	#endif
+	
+	#ifdef use_shadows
+		uniform sampler2DShadow shadowTex;
+		uniform float shadowDensity;
+	#endif
+	#ifdef use_vertex_ao
+		varying float aoTerm;
+	#endif
+	uniform vec4 teamColor;
+	varying vec3 cameraDir;
+	//varying float fogFactor;
+	
+	#ifdef flashlights
+		varying float selfIllumMod;
+	#endif
+	
+	#ifdef use_normalmapping
+		varying mat3 tbnMatrix;
+		uniform sampler2D normalMap;
+	#else
+		varying vec3 normalv;
+	#endif
+	float GetShadowCoeff(vec4 shadowCoors){
+		#ifdef use_shadows
+			float coeff = shadow2DProj(shadowTex, shadowCoors+vec4(0.0, 0.0, -0.00005, 0.0)).r;
+			coeff  = (1.0 - coeff);
+			coeff *= shadowDensity;
+			return (1.0 - coeff);
+		#else
+			return 1.0;
+		#endif
+	}
+	
+	void main(void){
+		%%FRAGMENT_PRE_SHADING%%
 
-  #ifdef use_shadows
-    #ifdef use_perspective_correct_shadows
-      uniform vec4 shadowParams;
-    #endif
-    varying vec4 shadowpos;
-    uniform sampler2DShadow shadowTex;
-    uniform float shadowDensity;
-  #endif
+		#ifdef use_normalmapping
+			vec2 tc = gl_TexCoord[0].st;
+			#ifdef flip_normalmap
+				tc.t = 1.0 - tc.t;
+			#endif
+			vec4 normaltex=texture2D(normalMap, tc);
+			vec3 nvTS   = normalize((normaltex.xyz - 0.5) * 2.0);
+			vec3 normal = tbnMatrix * nvTS;
+		#else
+			vec3 normal = normalize(normalv);
+		#endif
+		vec3 light = max(dot(normal, sunPos), 0.0) * sunDiffuse + sunAmbient;
 
-    varying vec3 teamColor;
-    varying vec3 cameraDir;
-    //varying float fogFactor;
+		vec4 diffuseIn  = texture2D(textureS3o1, gl_TexCoord[0].st);
+		vec4 outColor   = diffuseIn;
+		vec4 extraColor = texture2D(textureS3o2, gl_TexCoord[0].st);
+		vec3 reflectDir = reflect(cameraDir, normal);
+		
+		#if (deferred_mode == 0)
+			vec3 specular   = textureCube(specularTex, reflectDir).rgb * extraColor.g * SPECULARMULT;
+			vec3 reflection = textureCube(reflectTex,  reflectDir).rgb;
+		#endif
+		#if (deferred_mode == 1) 
+			vec3 specular   = vec3(1.0,1.0,1.0) * extraColor.g * SPECULARMULT;
+			vec3 reflection = vec3(0.0,0.0,0.0);
+		#endif
+		float shadow = GetShadowCoeff(gl_TexCoord[1] + vec4(0.0, 0.0, -0.00005, 0.0));
+		light     = mix(sunAmbient, light, shadow);
+		specular *= shadow;
 
-  #ifdef use_normalmapping
-    varying vec3 t;
-    varying vec3 b;
-    varying vec3 n;
-    uniform sampler2D normalMap;
-  #else
-    varying vec3 normalv;
-  #endif
+		reflection  = mix(light, reflection, extraColor.g); // reflection
+		#ifdef flashlights
+			extraColor.r =extraColor.r * selfIllumMod;
+		#endif
+		reflection += (extraColor.rrr); // self-illum
 
-    void main(void)
-    {
-       %%FRAGMENT_PRE_SHADING%%
+		outColor.rgb = mix(outColor.rgb, teamColor.rgb, outColor.a);
 
-    #ifdef use_normalmapping
-       vec2 tc = gl_TexCoord[0].st;
-      #ifdef flip_normalmap
-         tc.t = 1.0 - tc.t;
-      #endif
-       vec3 nvTS   = normalize(texture2D(normalMap, tc).xyz - 0.5);
-       vec3 normal = normalize(mat3(t,b,n)  * nvTS);
-    #else
-       vec3 normal = normalize(normalv);
-    #endif
-       float a    = max( dot(normal, sunPos), 0.0);
-       vec3 light = a * sunDiffuse + sunAmbient;
+		//#if (deferred_mode == 0)
+			// diffuse + specular + envcube lighting
+			// (reflection contains the NdotL term!)
+			outColor.rgb = outColor.rgb * reflection + specular;
+		//#endif
 
-       vec4 extraColor  = texture2D(textureS3o2, gl_TexCoord[0].st);
+		outColor.a   = extraColor.a;
+		//outColor.rgb = outColor.rgb + outColor.rgb * (normaltex.a - 0.5) * etcLoc.g; // no more wreck color blending
 
-       vec3 reflectDir = reflect(cameraDir, normal);
-       vec3 specular   = textureCube(specularTex, reflectDir).rgb * extraColor.g * 4.0;
-       vec3 reflection = textureCube(reflectTex,  reflectDir).rgb;
+		#ifdef use_vertex_ao
+			outColor.rgb=outColor.rgb*aoTerm;
+		#endif
 
-    #ifdef use_shadows
-       vec4 shadowTC = shadowpos;
-      #ifdef use_perspective_correct_shadows
-       shadowTC.st = shadowTC.st * (inversesqrt( abs(shadowTC.st) + shadowParams.z) + shadowParams.w) + shadowParams.xy;
-      #endif
-       float shadow = shadow2DProj(shadowTex, shadowTC).r;
-       shadow    = 1.0 - (1.0 - shadow) * shadowDensity;
-       light     = mix(sunAmbient, light, shadow);
-       specular *= shadow;
-    #endif
+		#if (deferred_mode == 0)
+			gl_FragColor = outColor;
+		#else
+			gl_FragData[0] = vec4((normal + 1.0) * 0.5, 1.0);
+			gl_FragData[1] = outColor;
+			gl_FragData[2] = vec4(specular, extraColor.a);
+			gl_FragData[3] = vec4(extraColor.rrr, 1.0);
+		#endif
 
-       reflection  = mix(light, reflection, extraColor.g); // reflection
-       reflection += extraColor.rrr; // self-illum
+		%%FRAGMENT_POST_SHADING%%
+	}
+]],
 
-       gl_FragColor     = texture2D(textureS3o1, gl_TexCoord[0].st);
-       gl_FragColor.rgb = mix(gl_FragColor.rgb, teamColor.rgb, gl_FragColor.a); // teamcolor
-       gl_FragColor.rgb = gl_FragColor.rgb * reflection + specular;
-       gl_FragColor.a   = extraColor.a;
-       //gl_FragColor.rgb = mix(gl_Fog.color.rgb, gl_FragColor.rgb, fogFactor); // fog
-       //gl_FragColor.a = teamColor.a; // far fading
-       //gl_FragColor.rgb = normal;
-
-       %%FRAGMENT_POST_SHADING%%
-    }
-  ]],
   uniformInt = {
     textureS3o1 = 0,
     textureS3o2 = 1,
@@ -166,13 +216,13 @@ return {
     --detailMap   = 6,
   },
   uniform = {
-    sunPos = {gl.GetSun("pos")},
+    -- sunPos = {gl.GetSun("pos")}, -- material has sunPosLoc
     sunAmbient = {gl.GetSun("ambient" ,"unit")},
     sunDiffuse = {gl.GetSun("diffuse" ,"unit")},
     shadowDensity = {gl.GetSun("shadowDensity" ,"unit")},
-    shadowParams  = {gl.GetShadowMapParams()},
+    -- shadowParams  = {gl.GetShadowMapParams()}, -- material has shadowParamsLoc
   },
   uniformMatrix = {
-    shadowMatrix = {gl.GetMatrixData("shadow")},
+    -- shadowMatrix = {gl.GetMatrixData("shadow")}, -- material has shadow{Matrix}Loc
   },
 }
