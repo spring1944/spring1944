cb9fb17f03f186164620a71e496e97eb64fbe384
yuritch <s44.yuritch@gmail.com>
2016-07-20 11:50:13 +0300

    Removed cob script from Po-2 partisan

--- units/rus/aircraft/RUSPo2.lua
+++ units/rus/aircraft/RUSPo2.lua
@@ -16,7 +16,7 @@ local RUS_Po2 = Recon:New{
 
 local RUS_Po2Partisan = RUS_Po2:Clone("RUSPo2"):New{
 	buildpic		= "RUSPo2Partisan.png", -- override clone
-	script			= "RUSPo2Partisan.cob", -- more override clone
+--	script			= "RUSPo2Partisan.cob", -- more override clone
 	description		= "Partisan Supply Plane",
 	customParams = {
 		troopdropper	= 1,
