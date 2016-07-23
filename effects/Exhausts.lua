d2a55f9f2011703c3532ded88967abf33f94fae0
yuritch <s44.yuritch@gmail.com>
2016-07-23 10:40:12 +0300

    Better looking exhaust FX

--- effects/Exhausts.lua
+++ effects/Exhausts.lua
@@ -2,65 +2,69 @@
 --diesel_exhaust
 
 return {
-  ["petrol_exhaust"] = {
-    muzzlesmoke = {
-      air                = true,
-      class              = [[CSimpleParticleSystem]],
-      count              = 4, -- 10
-      ground             = true,
-      water              = true,
-      properties = {
-        airdrag            = 0.4, --0.8
-        alwaysvisible      = false,
-        colormap           = [[1.0 0.7 0.5 1.0     0.4 0.4 0.4 1.0    0.3 0.3 0.3 1.0    0.2 0.2 0.2 1.0    0 0 0 0.01]],
-        directional        = true,
-        emitrot            = 0,
-        emitrotspread      = 10,
-        emitvector         = [[dir]],
-        gravity            = [[0, -0.1, 0]],
-        numparticles       = 10,
-        particlelife       = 50, -- 20
-        particlelifespread = 25,
-        particlesize       = [[7 i-0.4]],
-        particlesizespread = 1,
-        particlespeed      = [[10 i-1]], --[10 i-1]],
-        particlespeedspread = 1,
-        pos                = [[0, 0, 0]],
-        sizegrowth         = 0.5,
-        sizemod            = 1.0,
-        texture            = [[smokesmall]],
-      },
-    },
-  },
-  ["diesel_exhaust"] = {
-    muzzlesmoke = {
-      air                = true,
-      class              = [[CSimpleParticleSystem]],
-      count              = 4, -- 10
-      ground             = true,
-      water              = true,
-      properties = {
-        airdrag            = 0.4, --0.8
-        alwaysvisible      = false,
-        colormap           = [[0.72 0.61 0.41 1      0 0 0 0.01]],
-        directional        = true,
-        emitrot            = 0,
-        emitrotspread      = 10,
-        emitvector         = [[dir]],
-        gravity            = [[0, -0.1, 0]],
-        numparticles       = 10,
-        particlelife       = 50, -- 20
-        particlelifespread = 25,
-        particlesize       = [[7 i-0.4]],
-        particlesizespread = 1,
-        particlespeed      = [[10 i-1]], --[10 i-1]],
-        particlespeedspread = 1,
-        pos                = [[0, 0, 0]],
-        sizegrowth         = 0.5,
-        sizemod            = 1.0,
-        texture            = [[smokesmall]],
-      },
-    },
-  },
+	["petrol_exhaust"] = {
+		muzzlesmoke =
+		{
+			air                = true,
+			ground				= true,
+			water				= true,
+			class              = [[CSimpleParticleSystem]],
+			count              = 10,
+			properties =
+			{
+				airdrag            = 0.8,
+				alwaysvisible      = false,
+				colormap           = [[0 0 0 0.01  0.5 0.5 0.5 0.5     0 0 0 0.01]],
+				directional        = false,
+				emitrot            = 0,
+				emitrotspread      = 30,
+				emitvector         = [[dir]],
+				gravity            = [[0, 0.1, 0]],
+				numparticles       = 1,
+				particlelife       = 50,
+				particlelifespread = 25,
+				particlesize       = [[7 i-0.4]],
+				particlesizespread = 1,
+				particlespeed      = [[8 i-1]],
+				particlespeedspread = 1,
+				pos                = [[0, 0, 0]],
+				sizegrowth         = 0.1,
+				sizemod            = 1.0,
+				texture            = [[smokesmall]],
+			},
+		},
+	},
+	["diesel_exhaust"] = {
+		muzzlesmoke =
+		{
+			air                = true,
+			ground				= true,
+			water				= true,
+			class              = [[CSimpleParticleSystem]],
+			count              = 10,
+			properties =
+			{
+				airdrag            = 0.8,
+				alwaysvisible      = false,
+				colormap           = [[0.72 0.61 0.41 1      0 0 0 0.01]],
+				directional        = false,
+				emitrot            = 0,
+				emitrotspread      = 30,
+				emitvector         = [[dir]],
+				gravity            = [[0, 0.1, 0]],
+				numparticles       = 1,
+				particlelife       = 50,
+				particlelifespread = 25,
+				particlesize       = [[7 i-0.4]],
+				particlesizespread = 1,
+				particlespeed      = [[8 i-1]],
+				particlespeedspread = 1,
+				pos                = [[0, 0, 0]],
+				sizegrowth         = 0.1,
+				sizemod            = 1.0,
+				texture            = [[smokesmall]],
+			},
+		},
+	},
 }
 
