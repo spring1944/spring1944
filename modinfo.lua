5fe374173b803b87ceb12cfdc458e5abffcf640d
PepeAmpere <machys@gmail.com>
2016-10-31 03:20:04 +0100

    Spring 1944 beta-161031

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 local modinfo = {
 	name			= 'Spring 1944',
 	shortName		= 's44',
-	version			= 'beta-161030',
+	version			= 'beta-161031',
 	game			= 'Spring 1944',
 	shortGame		= 's44',
 	mutator			= 'Official',
