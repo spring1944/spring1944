23aa491849e6e24aa81ee420c90dfa93ce436259
yuritch <s44.yuritch@gmail.com>
2017-05-24 12:58:19 +0300

    Lorraine 38L APC

---
+++ units/fra/vehicles/FRALorraine38L.lua
@@ -0,0 +1,24 @@
+local Lorraine38L = HalfTrack:New{
+	name					= "Voiture blindée de chasseurs portés 38L",
+	buildCostMetal			= 1100,
+	corpse					= "FRALorraine38L_Burning",
+	maxDamage				= 605,
+	trackOffset				= 10,
+	trackWidth				= 14,
+	
+	objectName				= "FRA/FRALorraine38L.s3o",
+	
+	customParams = {
+		armor_front			= 16,
+		armor_rear			= 16,
+		armor_side			= 9,
+		armor_top			= 0,
+		maxvelocitykmh		= 37,
+		customanims			= "lorraine38l",
+	},
+	
+}
+
+return lowerkeys({
+	["FRALorraine38L"] = Lorraine38L,
+})
