21ebcc8f6cc1ab4bd4e58d5d74f01b27ff39789f
yuritch <s44.yuritch@gmail.com>
2017-05-27 13:49:20 +0300

    Added towed guns to Spoils of War spawn list

--- LuaRules/Configs/spoils_of_war/fra.lua
+++ LuaRules/Configs/spoils_of_war/fra.lua
@@ -8,27 +8,31 @@ local unitDefs = {
 		{	-- tier 1 - truck
 			"fracitroentype45",
 		},
-		{	-- tier 2 - APC and scout
+		{	-- tier 2 - APC, scout, light AT
 			"fralorraine38l",
 			"fralafflyv15r",
+			"fra25mmmle1934_truck",
 		},
-		{	-- tier 3 - bad light tanks
+		{	-- tier 3 - bad light tanks, light AA
 			"frar35",
 			"frah35",
+			"fra25mmaa_truck",
 		},
-		{	-- tier 4 - better light tanks, armored car
+		{	-- tier 4 - better light tanks, armored car, good AT
 			"frar39",
 			"frah39",
 			"fraamd35",
+			"fra47mmat_truck",
 		},
 		{	-- tier 5 - medium tank
 			"fras35",
+			"fra75mmmle1897_truck",
 		},
-		--[[ NOOOOO, composite unit spawning doesn't work correctly here!
+		-- NOOOOO, composite unit spawning doesn't work correctly here!
 		{	-- tier 6 - heavy
-			"fracharb1bis",
+			--"fracharb1bis",
+			"fra105mmmle1935b_truck",
 		},
-		]]--
 	},
 }
 
