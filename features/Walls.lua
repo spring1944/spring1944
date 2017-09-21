da8a1e46c849855d8a6acb60907369e4c716fffa
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2017-09-21 21:28:56 +0200

    Adde more wall components

--- features/Walls.lua
+++ features/Walls.lua
@@ -28,4 +28,56 @@ wallDefs["WallRedBricks"] = {
     },
 }
 
+wallDefs["WallRedBricks_Large"] = {
+    description    = "WallRedBricks_Large",
+    object         = "Features/WallRedBricks_Large.dae",
+    blocking       = true,
+    burnable       = false,
+    reclaimable    = false,
+    noSelect       = false,
+    indestructible = false,
+    energy          = 0,
+    damage          = 1000,
+    metal           = 0,
+    mass            = 40,
+    crushResistance = 14,
+    footprintX  = 1,
+    footprintZ  = 1,
+    upright =  true,
+    floating = false,
+    collisionVolumeTest = 1,
+    collisionVolumeType = "box",
+    collisionVolumeScales = {4, 24, 72},
+    collisionVolumeOffsets = {0, 12, 0},
+    customParams = {
+        mod = true,
+    },
+}
+
+wallDefs["WallRedBricks_Corner"] = {
+    description    = "WallRedBricks_Corner",
+    object         = "Features/WallRedBricks_Corner.dae",
+    blocking       = true,
+    burnable       = false,
+    reclaimable    = false,
+    noSelect       = false,
+    indestructible = false,
+    energy          = 0,
+    damage          = 1000,
+    metal           = 0,
+    mass            = 40,
+    crushResistance = 14,
+    footprintX  = 1,
+    footprintZ  = 1,
+    upright =  true,
+    floating = false,
+    collisionVolumeTest = 1,
+    collisionVolumeType = "box",
+    collisionVolumeScales = {6, 24, 6},
+    collisionVolumeOffsets = {0, 12, 0},
+    customParams = {
+        mod = true,
+    },
+}
+
 return lowerkeys( wallDefs )
