c3ca4e3dfa45abee8e260836d7ae8b0d51ce2d98
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2017-10-02 08:31:00 +0200

    Added an small train station

---
+++ features/RailRoad.lua
@@ -0,0 +1,31 @@
+-- Default Spring Treedef
+
+local defs = {}
+
+defs["RailStation_small"] = {
+    description    = "RailStation_small",
+    object         = "Features/TrainStation_small.dae",
+    blocking       = true,
+    burnable       = false,
+    reclaimable    = false,
+    noSelect       = false,
+    indestructible = true,
+    energy          = 0,
+    damage          = 100000,
+    metal           = 0,
+    mass            = 10000,
+    crushResistance = 1000,
+    footprintX  = 144 / 16,  -- 1 footprint unit = 16 elmo
+    footprintZ  = 48 / 16,  -- 1 footprint unit = 16 elmo
+    upright =  true,
+    floating = false,
+    collisionVolumeTest = 1,
+    collisionVolumeType = "box",
+    collisionVolumeScales = {144, 64, 48},
+    collisionVolumeOffsets = {0, 0, 0},
+    customParams = {
+        mod = true,
+    },
+}
+
+return lowerkeys( defs )
