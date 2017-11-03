41bdae4aee9dc39844794818ab511ccd4634b4a0
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2017-11-03 11:52:15 +0100

    Unit trees generation

--- LuaUI/Widgets/cmd_generatewiki.lua
+++ LuaUI/Widgets/cmd_generatewiki.lua
@@ -41,11 +41,66 @@ local function __to_string(data, indent)
         end
     elseif(type(data) == "function") then
         str = str .. (indenter):rep(indent) .. 'function' .. "\n"
+    elseif(type(data) == "nil") then
+        str = str .. "*nil"
     else
         print(1, "Error: unknown data type: %s", type(data))
     end
 
     return str
+end
+
+
+UNITS = {}  -- Global list of units to document
+
+local function _to_wikilist(data, url_base, prefix)
+    local str = ""
+
+    if(prefix == nil) then
+        prefix = ""
+    end
+    local i, v
+    local count = 1
+    for i, v in pairs(data) do
+        if UNITS[i] ~= "" then
+            -- We are not interested on adding morphs to this list, which are
+            -- featured by the abscence of a human readable name
+            str = str .. prefix .. tostring(count) .. ".- "
+            str = str .. "[" .. UNITS[i] .. "]"
+            str = str .. "(" .. url_base .. i .. ")\n\n"
+            str = str .. _to_wikilist(v, url_base, prefix .. tostring(count) .. ".")
+            count = count + 1
+        end
+    end
+
+    return str
+end
+
+function _units_tree(startUnit)
+    -- Departs from the starting unit, and traverse all the tech tree derived
+    -- from him, simply following the building capabilities of each unit.
+    local name = startUnit
+    if type(name) == "number" then
+        name = UnitDefs[startUnit].name
+    end
+    local tree = {}
+    local unitDef = UnitDefNames[name]
+    if UNITS[name] == nil then
+        UNITS[name] = unitDef.humanName
+    else
+        -- The unit has been already digested. Parsing that again will result
+        -- in an infinite loop
+        return {}
+    end
+    local children = unitDef.buildOptions
+    for i = 1,#children do
+        local name = children[i]
+        if type(name) == "number" then
+            name = UnitDefs[name].name
+        end
+        tree[name] = _units_tree(name)
+    end
+    return tree
 end
 
 FACTIONS_PICS_URL = "https://gitlab.com/Spring1944/spring1944/raw/master/LuaUI/Widgets/faction_change/"
@@ -64,6 +119,14 @@ function _gen_faction(folder, faction)
     handle.write(handle, "(" .. FACTIONS_PICS_URL .. side .. ".png) ")
     handle.write(handle, faction.title .. "\n\n")
     handle.write(handle, faction.wiki .. "\n\n")
+
+    -- Faction units tree
+    local tree = {}
+    tree[faction.startUnit] = _units_tree(faction.startUnit)
+    handle.write(handle, "## Units tree\n\n")
+    handle.write(handle, _to_wikilist(tree, "factions/" .. side .. "/"))
+    handle.write(handle, "\n")    
+
     handle.close(handle)
 end
 
