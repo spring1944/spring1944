9c5761d6f3212f717669e57967a15bc7e8a46e0f
PepeAmpere <machys@gmail.com>
2018-01-24 04:00:21 +0100

    Steam Spring 1944 beta-180125

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 local modinfo = {
 	name			= 'Steam Spring 1944',
 	shortName		= 'ss44',
-	version			= 'beta-180124',
+	version			= 'beta-180125',
 	game			= 'Steam Spring 1944',
 	shortGame		= 'ss44',
 	mutator			= 'notAlab distribution',
