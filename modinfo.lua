e1f5aeb6ec53350152ac929a71ab6d75f12cf838
PepeAmpere <machys@gmail.com>
2018-02-25 20:04:43 +0100

    reverting changes in alpha branch done by github

--- modinfo.lua
+++ modinfo.lua
@@ -1,13 +1,12 @@
 local modinfo = {
-	name			= 'Spring: 1944',
-	shortName		= 'S44',
-	version			= '$VERSION',
-	game			= 'Spring: 1944',
-	shortGame		= 'S44',
-	mutator			= 'Official',
-	description		= 'Epic World War II RTS!',
-	url				= 'http://www.spring1944.net/',
-	modtype			= '1',
+    name = 'Steam Spring 1944',
+    shortName = 'ss44',
+    version = 'alpha-$VERSION',
+    game = 'Steam Spring 1944',
+    shortGame = 'ss44',
+    mutator = 'notAlab distribution',
+    description = 'Epic World War II RTS',
+    url = 'http://notalab.com/products/ss44/',
 }
 
 return modinfo
