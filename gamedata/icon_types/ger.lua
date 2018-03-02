c4ea65be35c8ea385bd7df37cff9045a7b52b20a
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2018-03-02 18:12:17 +0100

    Made icons extensible

---
+++ gamedata/icon_types/ger.lua
@@ -0,0 +1,14 @@
+local iconTypes = {
+    gerhq = {
+        bitmap   = "icons/gerhq.tga",
+        size     = 4,
+        distance = 0.8,
+    },
+    gerflag = {
+        bitmap   = "icons/gerhq.tga",
+        size     = 2.25,
+        distance = 0.3,
+    },
+}
+
+return iconTypes
---
+++ gamedata/icon_types/jpn.lua
@@ -0,0 +1,14 @@
+local iconTypes = {
+    jpnhq = {
+        bitmap   = "icons/jpnhq.tga",
+        size        =    4,
+        distance = 0.8,
+    },
+    jpnflag = {
+        bitmap   = "icons/jpnhq.tga",
+        size     = 2.25,
+        distance = 0.3,
+    },
+}
+
+return iconTypes
