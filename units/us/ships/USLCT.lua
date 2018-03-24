d8711c06da4594e92ea54474bfe0b902eaa9e828
yuritch <s44.yuritch@gmail.com>
2018-03-24 20:51:18 +0200

    Converted USLCT to the new code

--- units/us/ships/USLCT.lua
+++ units/us/ships/USLCT.lua
@@ -1,4 +1,4 @@
-local US_LCT = TankLandingCraft:New{
+local US_LCT = TankLandingCraftComposite:New{
 	name					= "LCT Mk. 5",
 	acceleration			= 0.075,
 	brakeRate				= 0.05,
@@ -13,46 +13,65 @@ local US_LCT = TankLandingCraft:New{
 	weapons = {	
 		[1] = {
 			name				= "Oerlikon20mmaa",
-			maxAngleDif			= 240,
-			onlyTargetCategory	= "AIR",
-			mainDir				= [[1 0 0]],
-		},
-		[2] = {
-			name				= "Oerlikon20mmaa",
-			maxAngleDif			= 240,
-			onlyTargetCategory	= "AIR",
-			mainDir				= [[-1 0 0]],
-		},
-		[3] = {
-			name				= "Oerlikon20mmhe",
-			maxAngleDif			= 240,
-			onlyTargetCategory	= "BUILDING INFANTRY SOFTVEH OPENVEH HARDVEH SHIP LARGESHIP DEPLOYED",
-			mainDir				= [[1 0 0]],
-		},
-		[4] = {
-			name				= "Oerlikon20mmhe",
-			maxAngleDif			= 240,
-			onlyTargetCategory	= "BUILDING INFANTRY SOFTVEH OPENVEH HARDVEH SHIP LARGESHIP DEPLOYED",
-			mainDir				= [[-1 0 0]],
 		},
 	},
 	customparams = {
-		--[[ enable me later when using LUS
+		children = {
+			"us_lct_turret_20mm_left",
+			"us_lct_turret_20mm_right",
+		},
+		customanims = "gbr_lct",
 		deathanim = {
-			["z"] = {angle = -30, speed = 10},
-		},]]
-
-	},
-	sfxtypes = { -- remove once using LUS
-		explosionGenerators = {
-			[1] = "custom:SMOKEPUFF_GPL_FX",
-			[8] = "custom:XSMALL_MUZZLEFLASH",
-			[9] = "custom:XSMALL_MUZZLEDUST",
+			["x"] = {angle = -5, speed = 5},
 		},
 	},
 }
 
+local US_LCT_Turret_20mm_Left = OpenBoatTurret:New{
+	name					= "Oerlikon 20mm Turret",
+	description				= "20mm AA Turret",
+	objectName				= "<SIDE>/us_lct_turret_20mm.s3o",
+	weapons = {	
+		[1] = {
+			name				= "Oerlikon20mmaa",
+			maxAngleDif			= 180,
+			mainDir				= [[1 0 0]],
+		},
+		[2] = {
+			name				= "Oerlikon20mmhe",
+			maxAngleDif			= 150,
+			mainDir				= [[1 0 0]],
+		},
+	},
+	customparams = {
+		maxammo					= 14,
+
+		barrelrecoildist		= 2,
+		barrelrecoilspeed		= 10,
+		turretturnspeed			= 45,
+		elevationspeed			= 45,
+		aaweapon				= 1,
+		facing 					= 3,
+		defaultheading1			= math.rad(-90),
+	},
+}
+local US_LCT_Turret_20mm_Right = US_LCT_Turret_20mm_Left:New{
+	weapons = {	
+		[1] = {
+			mainDir				= [[-1 0 0]],
+		},
+		[2] = {
+			mainDir				= [[-1 0 0]],
+		},
+	},
+	customparams = {
+		facing 					= 1,
+		defaultheading1				= math.rad(90),
+	},
+}
 
 return lowerkeys({
 	["USLCT"] = US_LCT,
+	["us_lct_turret_20mm_left"] = US_LCT_Turret_20mm_Left,
+	["us_lct_turret_20mm_right"] = US_LCT_Turret_20mm_Right,
 })
