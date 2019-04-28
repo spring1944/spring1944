de41717bc402a05e13fbe7aaf218df96753ab2fd
Fedja Beader <fedja@protonmail.ch>
2019-04-28 16:48:59 +0200

    Add a beautifully unpleasant window with red text in it.

--- LuaUI/Widgets/engine_compatibility.lua
+++ LuaUI/Widgets/engine_compatibility.lua
@@ -2,10 +2,10 @@ function widget:GetInfo()
 	return {
 		name = "Engine compatibility",
 		desc = "Exits if Spring engine version used is not supported",
-		author = "raaar, adapted for s44 by ThinkSome",
+		author = "raaar, ThinkSome adapted for s44 and added Chili",
 		version   = "1.0",
 		date = "20190428",
-		license = "PD",
+		license = "GNU AGPLv3",
 		layer = 999999,
 		enabled = true,
 	}
@@ -19,6 +19,9 @@ local countdown = 10
 
 local currentEngineVersion = "???"
 local isAllowedEngineVersion = false
+local Chili, Screen0
+local errorWindow, errorLabel
+local errorMessage
 
 
 function widget:Initialize()
@@ -31,21 +34,57 @@ function widget:Initialize()
 	if currentEngineVersion == allowedEngineVersion then
 		isAllowedEngineVersion = true
 	end
+
+	if isAllowedEngineVersion then
+		widgetHandler:RemoveWidget()
+		return
+	end
+
+	errorMessage = "Error: unsupported Spring Engine version detected (" .. currentEngineVersion .. ").\n"
+	             .. "Please use " .. allowedEngineVersionDesc .. " instead."
+
+	if not WG.Chili then
+		Spring.Echo("Error: Chili not found!")
+		return
+	end
+
+	Chili = WG.Chili
+	Screen0 = Chili.Screen0
+
+	errorWindow = Chili.Window:New{
+	  parent = Screen0,
+	  x = '30%',
+	  y = '20%',
+	  width = '40%',
+	  height = '10%',
+	}
+
+	errorLabel = Chili.Label:New{
+	  parent = errorWindow,
+	  width = '100%',
+	  height = '100%',
+	  caption = errorMessage,
+	  font = {
+	    size = 16,
+	    color = {1,0.2,0.2,1},
+	  }
+	}
 end
 
 
 function widget:GameFrame(n)
 	if n % 60 == 8 then
-		if isAllowedEngineVersion then
-			widgetHandler:RemoveWidget()
-		else
-			Spring.Echo("Error: unsupported Spring Engine version detected (" .. currentEngineVersion
-			  .. "). Please use " .. allowedEngineVersionDesc .. " instead.")
-			countdown = countdown - 1
-			Spring.Echo("Exiting in " .. countdown .. "...")
-			if countdown == 0 then
-				Spring.SendCommands("QuitForce")
-			end
+		countdown = countdown - 1
+		local exitMessage = "Exiting in " .. countdown .. "..."
+
+		Spring.Echo(errorMessage)
+		Spring.Echo(exitMessage)
+		if errorLabel then
+			errorLabel:SetCaption(errorMessage .. "\n" .. exitMessage)
+		end
+
+		if countdown == 0 then
+			Spring.SendCommands("QuitForce")
 		end
 	end
 end
