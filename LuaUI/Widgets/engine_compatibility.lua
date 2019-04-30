f657a5734b67f1f40342938cbd9ba2d96a030338
Fedja Beader <fedja@protonmail.ch>
2019-04-30 15:53:40 +0200

    Switch engine checker to 104 maint from commit 1218 to 105 release.

--- LuaUI/Widgets/engine_compatibility.lua
+++ LuaUI/Widgets/engine_compatibility.lua
@@ -3,8 +3,8 @@ function widget:GetInfo()
 		name = "Engine compatibility",
 		desc = "Exits if Spring engine version used is not supported",
 		author = "raaar, ThinkSome adapted for s44 and added Chili",
-		version   = "1.0",
-		date = "20190428",
+		version   = "1.1",
+		date = "20190430",
 		license = "GNU AGPLv3",
 		layer = 999999,
 		enabled = true,
@@ -12,8 +12,8 @@ function widget:GetInfo()
 end
 
 
-local allowedEngineVersion = "103"
-local allowedEngineVersionDesc = "Spring 103 (103.0)"
+local allowedEngineVersionDesc = "104.0.1-1218-ga0cd6b1 maintenance (Spring 104 maintenance commit 1218 or later)"
+local allowedEngineVersionDl = "To download, type 1218 into Springlobby Downloads->Search file and select appropriate one."
 local countdown = 10
 
 
@@ -31,8 +31,14 @@ function widget:Initialize()
 		currentEngineVersion = Game.version
 	end
 
-	if currentEngineVersion == allowedEngineVersion then
-		isAllowedEngineVersion = true
+	if Script ~= nil and Script.IsEngineMinVersion ~= nil then
+		if Script.IsEngineMinVersion(104, 0, 1218) and nil ~= currentEngineVersion:find("maintenance") then
+			isAllowedEngineVersion = true
+		end
+		-- Futureproofing
+		if Script.IsEngineMinVersion(105) then
+			isAllowedEngineVersion = false
+		end
 	end
 
 	if isAllowedEngineVersion then
@@ -41,7 +47,8 @@ function widget:Initialize()
 	end
 
 	errorMessage = "Error: unsupported Spring Engine version detected (" .. currentEngineVersion .. ").\n"
-	             .. "Please use " .. allowedEngineVersionDesc .. " instead."
+	             .. "Please use " .. allowedEngineVersionDesc .. " instead.\n"
+	             .. allowedEngineVersionDl
 
 	if not WG.Chili then
 		Spring.Echo("Error: Chili not found!")
@@ -56,7 +63,7 @@ function widget:Initialize()
 	  x = '30%',
 	  y = '20%',
 	  width = '40%',
-	  height = '10%',
+	  height = '15%',
 	}
 
 	errorLabel = Chili.Label:New{
