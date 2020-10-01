75295926d70ee6db0d9e7c6728a86502477dbe25
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2020-10-01 16:55:48 +0200

    Simplified the heatmaps, which now are not based on a diffusion process

--- LuaRules/Gadgets/ai_heatmaps.lua
+++ LuaRules/Gadgets/ai_heatmaps.lua
@@ -22,17 +22,43 @@ else
 --  UNSYNCED -------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local MY_PLAYER_ID = Spring.GetMyPlayerID()
+local heatmaps_manager = nil
 
--- locals
+local MY_PLAYER_ID = Spring.GetMyPlayerID()
 local HEATMAPS_DEBUG = nil
 local HEATMAPS_DEBUG_SIZE = 0.5
-local DT = 0.0
-local heatmaps = {}
-local shader
-local uni_dx, uni_dy, uni_dt, uni_diffusion, uni_heating
 
---------------------------------------------------------------------------------
+local GL_COLOR_BUFFER_BIT = GL.COLOR_BUFFER_BIT
+local GL_POINTS = GL.POINTS
+local GL_TRIANGLE_FAN = GL.TRIANGLE_FAN
+
+local glCreateTexture = gl.CreateTexture
+local glCreateFBO = gl.CreateFBO
+local glDeleteTexture = gl.DeleteTexture
+local glDeleteFBO = gl.DeleteFBO
+local glBlending = gl.Blending
+local glColor = gl.Color
+local glPushMatrix = gl.PushMatrix
+local glLoadIdentity = gl.LoadIdentity
+local glActiveFBO = gl.ActiveFBO
+local glPopMatrix = gl.PopMatrix
+local glClear = gl.Clear
+local glVertex = gl.Vertex
+local glBeginEnd = gl.BeginEnd
+local glTexture = gl.Texture
+local glTexRect = gl.TexRect
+
+local spGetAllUnits = Spring.GetAllUnits
+local spValidUnitID = Spring.ValidUnitID
+local spGetUnitPosition = Spring.GetUnitPosition
+local spGetWindowGeometry = Spring.GetWindowGeometry
+
+local min, max = math.min, math.max
+local function clamp(v, vmin, vmax)
+    return min(max(v, vmin), vmax)
+end
+
+--- DEBUG ----------------------------------------------------------------------
 
 function gadget.Log(...)
     Spring.Log(gadget:GetInfo().name, LOG.INFO, table.concat{...})
@@ -57,12 +83,19 @@ local function SetDebug(cmd, line, words, player)
     elseif words[1] == nil then
         HEATMAPS_DEBUG = nil
         return true
-    elseif heatmaps[words[1]] == nil then
+    elseif heatmaps_manager == nil then
+        Error("No heatmaps manager has been built")
+    elseif heatmaps_manager:GetHeatmap(words[1]) == nil then
         Error("Cannot find a heatmap named '" .. words[1] .. "'")
         return true
     end
-    
+
+    if HEATMAPS_DEBUG ~= nil and heatmaps_manager:GetHeatmap(HEATMAPS_DEBUG) ~= nil then
+        heatmaps_manager:GetHeatmap(HEATMAPS_DEBUG).debug = false
+    end
+
     HEATMAPS_DEBUG = words[1]
+    heatmaps_manager:GetHeatmap(words[1]).debug = true
     Log("debug set to " .. HEATMAPS_DEBUG)
 
     return true
@@ -76,154 +109,25 @@ local function SetupCmdChangeAIDebug()
     gadgetHandler:AddChatAction(cmd, func, help)
 end
 
-local function world2tex(x, z)
-    return x / Game.mapSizeX, (Game.mapSizeZ - z) / Game.mapSizeZ
-end
+--- Utilities ------------------------------------------------------------------
 
-local function __new_heatmap(name, tsize, diffusion, heating)
-    if name == "nil" then
-        Error("Cannot create a heatmap with the reserved name '" .. name .. "'")
-        return false
-    end
-    if heatmaps[name] ~= nil then
-        Warning("Already exists a heatmap named '" .. name .. "'")
-        return false
-    end
-    local tile_size = math.floor(tsize)
-    if tile_size <= 0 then
-        Error("Invalid tile size, '" .. tostring(tsize) .. "'")
-        return false
-    end
-    if diffusion <= 0 then
-        Error("Invalid diffusion factor, '" .. tostring(diffusion) .. "'")
-        return false
-    end
-    if heating <= 0 then
-        Error("Invalid heating factor, '" .. tostring(heating) .. "'")
-        return false
-    end
-
-    heatmaps[name] = {tile_size = tile_size,
-                      diffusion = diffusion,
-                      heating = heating,
-                      sx = math.floor(Game.mapSizeX / tile_size),
-                      sy = math.floor(Game.mapSizeZ / tile_size),
-                      textures = {},
-                      current_texture = 1,
-                      pumps = {},
-                      pump_tex = nil,
-                      pumps_fbo = nil}
-
-    for i = 1,3  do 
-        local t = gl.CreateTexture(heatmaps[name].sx, heatmaps[name].sy, {
-            fbo = true, min_filter = GL.NEAREST, mag_filter = GL.NEAREST,
-            wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
-        })
-        if not t then
-            Error("Failure creating heatmap texture")
-            heatmaps[name] = nil
-            return false
+local function DrawPoints(heat_objs)
+    for _, data in ipairs(heat_objs) do
+        if data.radius <= 1.42 then
+            local c = data.color
+            if (type(c) == "number") then
+                glColor(c, c, c, 1.0)
+            else
+                glColor(c.r, c.g, c.b, c.a)
+            end
+            glVertex(2.0 * data.x_norm - 1.0, 2.0 * data.y_norm - 1.0, 0.0)
         end
-        heatmaps[name].textures[i] = t
-    end
-    heatmaps[name].pump_tex = heatmaps[name].textures[3]
-    heatmaps[name].pump_fbo = gl.CreateFBO({color0 = heatmaps[name].pump_tex})
-
-    Log("New heatmap : '" .. name .. "'")
-    SetDebug("ai_heatmaps", "ai_heatmaps " .. name, {name}, nil)
-
-    return true
-end
-
-local function __set_pump(name, pump_name, x, z, q, r)
-    if heatmaps[name] == nil then
-        Error("Cannot find a heatmap named '" .. name .. "'")
-        return false
-    end
-    local lx, ly = world2tex(x, z)
-    local lr = (r or heatmaps[name].tile_size) / heatmaps[name].tile_size
-    if lr < 1.0 then
-        lr = 1.0
-    end
-    heatmaps[name].pumps[pump_name] = {x = lx, y = ly, q = q, r = lr}
-
-    return true
-end
-
-local function __unset_pump(name, pump_name)
-    if heatmaps[name] == nil then
-        Error("Cannot find a heatmap named '" .. name .. "'")
-        return false
-    end
-    if heatmaps[name].pumps[pump_name] == nil then
-        return false
-    end
-    heatmaps[name].pumps[pump_name] = nil
-
-    return true
-end
-
-function gadget:Initialize()
-    SetupCmdChangeAIDebug()
-
-    shader = shader or gl.CreateShader({
-        fragment = VFS.LoadFile("LuaRules\\Gadgets\\ai_heatmaps\\rsc\\diffusion.fs", VFS.ZIP),
-        uniformInt = {T = 0, Q = 1},
-    })
-    if not shader then
-        Error("Failure creating heatmap shader")
-        Spring.Echo(gl.GetShaderLog())
-        return
-    end
-    uni_dx = gl.GetUniformLocation(shader, "dx")
-    uni_dy = gl.GetUniformLocation(shader, "dy")
-    uni_dt = gl.GetUniformLocation(shader, "dt")
-    uni_diffusion = gl.GetUniformLocation(shader, "diffusion")
-    uni_heating = gl.GetUniformLocation(shader, "heating")
-
-    GG.CreateAIHeatmap = __new_heatmap
-    GG.SetAIHeatmapPump = __set_pump
-    GG.UnsetAIHeatmapPump = __unset_pump
-
-    -- Testing
-    --[[
-    __new_heatmap("testing", 64, 30.0, 10.0)
-    __set_pump("testing", "pump1", Game.mapSizeX / 4.0, Game.mapSizeZ / 4.0, {r=1.0, g=0.8, b=0.6, a=1.0}, 8.0 * 64)
-    __set_pump("testing", "pump2", Game.mapSizeX / 1.25, Game.mapSizeZ / 1.25, {r=0.0, g=0.4, b=0.2, a=1.0}, 64)
-    __set_pump("testing", "pump3", Game.mapSizeX / 1.2499, Game.mapSizeZ / 1.24995, {r=1.0, g=0.0, b=0.0, a=1.0}, 64)
-    __set_pump("testing", "pump4", Game.mapSizeX / 1.24, Game.mapSizeZ / 1.26, {r=0.0, g=0.0, b=0.5, a=1.0}, 8.0 * 64)
-    SetDebug("ai_heatmaps", "ai_heatmaps testing", {"testing"}, nil)
-    --]]
-end
-
-function gadget:Shutdown()
-    gl.DeleteShader(shader)
-    shader, uni_dx, uni_dy, uni_dt, uni_diffusion, uni_heating = nil, nil, nil, nil, nil, nil
-
-    for name, heatmap in pairs(heatmaps) do
-        for i = 1,2  do 
-            gl.DeleteTexture(heatmap.textures[i] or "")
-            gl.DeleteFBO(heatmap.pump_fbo);
-        end
-        heatmaps[name] = nil
     end
 end
 
-local t = nil
-function gadget:Update()
-    local _, speedFactor, paused = Spring.GetGameSpeed()
-    if paused or t == nil then
-        t = Spring.GetTimer()
-        DT = 0.0
-        return
-    end
-    DT = speedFactor * Spring.DiffTimers(Spring.GetTimer(), t)
-    t = Spring.GetTimer()
-end
-
-function DrawPumpCircle(x, y, rx, ry, n)
+local function DrawCircle(x, y, rx, ry, n)
     n = n or 8
-    gl.Vertex(x, y)
+    glVertex(x, y)
 
     for i=0,n do
         local a = i * 2.0 * math.pi / n
@@ -231,110 +135,226 @@ function DrawPumpCircle(x, y, rx, ry, n)
     end
 end
 
-local function DrawPumpPoints(heatmap)
-    for name, pump in pairs(heatmap.pumps) do
-        if pump.r <= 1.42 then
-            local q = pump.q
-            if (type(q) == "number") then
-                gl.Color(q, q, q, 1.0)
-            else
-                gl.Color(q.r, q.g, q.b, q.a)
-            end
-            gl.Vertex(2.0 * pump.x - 1.0, 2.0 * pump.y - 1.0, 0.0)
-        end
-    end
-end
+local function DrawObjs(heat_objs)
+    gl.BeginEnd(GL_POINTS, DrawPoints, heat_objs)
 
-local function DrawPumps(heatmap)
-    gl.BeginEnd(GL.POINTS, DrawPumpPoints, heatmap)
-
-    for name, pump in pairs(heatmap.pumps) do
-        if pump.r > 1.42 then
-            local q = pump.q
-            if (type(q) == "number") then
-                gl.Color(q, q, q, 1.0)
+    for _, data in ipairs(heat_objs) do
+        if data.radius > 1.42 then
+            local c = data.color
+            if (type(c) == "number") then
+                c = clamp(c, 0, 1)
+                glColor(c, c, c, 1.0)
             else
-                gl.Color(q.r, q.g, q.b, q.a)
+                glColor(clamp(c.r, 0, 1),
+                        clamp(c.g, 0, 1),
+                        clamp(c.b, 0, 1),
+                        clamp(c.a, 0, 1))
             end
-            local rx, ry = pump.r / heatmap.sx, pump.r / heatmap.sy
-            gl.BeginEnd(GL.TRIANGLE_FAN,
-                        DrawPumpCircle,
-                        2.0 * pump.x - 1.0, 2.0 * pump.y - 1.0,
+            local rx, ry = data.rx_norm, data.ry_norm
+            glBeginEnd(GL_TRIANGLE_FAN,
+                        DrawCircle,
+                        2.0 * data.x_norm - 1.0, 2.0 * data.y_norm - 1.0,
                         rx, ry)
         end
     end
 end
 
-function gadget:DrawGenesis()
-    if shader == nil then
-        return
+local function world2tex(x, z)
+    return x / Game.mapSizeX, (Game.mapSizeZ - z) / Game.mapSizeZ
+end
+
+--- Heatmaps manager -----------------------------------------------------------
+
+HeatMap = {}
+HeatMap.__index = HeatMap
+
+function HeatMap:Create(tilesize)
+    local heatmap = {}
+    setmetatable(heatmap, HeatMap)
+    heatmap.tilesize = (tilesize ~= nil) and tilesize or 64
+    heatmap.sx = math.floor(Game.mapSizeX / heatmap.tilesize)
+    heatmap.sy = math.floor(Game.mapSizeZ / heatmap.tilesize)
+    heatmap.textures = {}
+    heatmap.fbos = {}
+    for i = 1,2 do
+        heatmap.textures[i] = glCreateTexture(heatmap.sx, heatmap.sy, {
+            fbo = true, min_filter = GL.NEAREST, mag_filter = GL.NEAREST,
+            wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
+        })
+        heatmap.fbos[i] = glCreateFBO({color0 = heatmap.textures[i]})
     end
+    heatmap.active_texture = 1   -- The one becoming drawn
+    heatmap.has_texture = false  -- No texture has been drawn yet
+    heatmap.debug = false
+    return heatmap
+end
 
-    for name, heatmap in pairs(heatmaps) do
-        -- Set the heat pumps texture
-        local r, g, b, a = 0.0, 0.0, 0.0, 0.0
-        if HEATMAPS_DEBUG == name then
-            a = 1.0
-        end
-
-        gl.Blending(false);
-        gl.Color(r, g, b, a)
-
-        gl.PushMatrix();
-        gl.LoadIdentity();
-
-        gl.ActiveFBO(heatmap.pump_fbo, gl.Clear, GL.COLOR_BUFFER_BIT, r, g, b, a);
-        gl.Blending("add");
-        gl.ActiveFBO(heatmap.pump_fbo, DrawPumps, heatmap);
-        gl.Blending(false);
-
-        -- Solve the diffusion equation
-        local next_texture
-        if heatmap.current_texture == 1 then
-            next_texture = 2
-        else
-            next_texture = 1
-        end
-        t_in = heatmap.textures[heatmap.current_texture]
-        t_out = heatmap.textures[next_texture]
-
-        gl.UseShader(shader)
-            gl.Texture(0, t_in)
-            gl.Texture(1, heatmap.pump_tex)
-            -- The theoretical value for dx and dy is 1/text_size. However,
-            -- using that value the offset can be so small than the method is
-            -- fetching the same texel several times, impairing diffusion.
-            -- Thus, we want to increase this value to something closer to 2
-            -- pixels, but not too close
-            gl.Uniform(uni_dx, 1.25 / heatmap.sx)
-            gl.Uniform(uni_dy, 1.25 / heatmap.sy)
-            gl.Uniform(uni_dt, DT)
-            gl.Uniform(uni_diffusion, heatmap.diffusion)
-            gl.Uniform(uni_heating, heatmap.heating)
-
-            gl.RenderToTexture(t_out, gl.TexRect, -1, 1, 1, -1)
-
-            gl.Texture(1, false)
-            gl.Texture(0, false)
-        gl.UseShader(0)
-
-        gl.PopMatrix();
-
-        heatmap.current_texture = next_texture
-
-        heatmaps[name] = heatmap
+function HeatMap:Destroy()
+    for i = 1,2  do 
+        glDeleteTexture(self.textures[i] or "")
+        glDeleteFBO(self.fbos[i])
     end
 end
 
+function HeatMap:SwapBuffer()
+    self.active_texture = (self.active_texture == 1) and 2 or 1
+    -- Clear the texture
+    local r, g, b, a = 0.0, 0.0, 0.0, 0.0
+    if self.debug then
+        a = 1.0
+    end
+
+    glBlending(false);
+    glColor(r, g, b, a)
+    glPushMatrix();
+    glLoadIdentity();
+    glActiveFBO(self.fbos[self.active_texture],
+                glClear,
+                GL_COLOR_BUFFER_BIT,
+                r, g, b, a);
+    glPopMatrix();
+end
+
+function HeatMap:Draw(heat_objs)
+    if not self.has_texture then
+        self:SwapBuffer()
+    end
+    self.has_texture = true
+
+    glPushMatrix();
+    glLoadIdentity();
+    glBlending("add")
+    glActiveFBO(self.fbos[self.active_texture],
+                DrawObjs, heat_objs)
+    glBlending(false)
+
+    glPopMatrix()
+end
+
+function HeatMap:GetTexture()
+    local finished = (self.active_texture == 1) and 2 or 1
+    return self.textures[finished]
+end
+
+HeatmapManager = {}
+HeatmapManager.__index = HeatmapManager
+
+function HeatmapManager:Create(units_per_timestep)
+    local manager = {}
+    setmetatable(manager, HeatmapManager)
+    manager.units_per_timestep = (units_per_timestep ~= nil) and units_per_timestep or 64
+    manager.current_unit = 1
+    manager.heatmaps = {}
+    manager.callbacks = {}
+    return manager
+end
+
+function HeatmapManager:Destroy()
+    for name, heatmap in pairs(self.heatmaps)  do
+        heatmap:Destroy()
+    end
+    self.heatmaps = {}
+    self.callbacks = {}
+end
+
+-- The callback should be a function to which a unitID is sent, and is returing
+-- a list of heat objects. Each heat object is featured by a color and a radius
+-- in world space units
+-- tilesize is optional (64 by default)
+function HeatmapManager:AddHeatmap(name, callback, tilesize)
+    self.heatmaps[name] = HeatMap:Create(tilesize)
+    self.callbacks[name] = callback
+
+    -- Testing
+    -- SetDebug("ai_heatmaps", "ai_heatmaps " .. name, {name}, nil)
+end
+
+function HeatmapManager:GetHeatmap(name)
+    return self.heatmaps[name]
+end
+
+function HeatmapManager:DestroyHeatmap(name)
+    self.heatmaps[name]:Destroy()
+    self.heatmaps[name] = nil
+end
+
+function HeatmapManager:__ParseUnit(unitID, out_obj)
+    local x, y, z = spGetUnitPosition(unitID)
+    x_norm, y_norm = world2tex(x, z)
+    for name, f in pairs(self.callbacks) do
+        local new_objs = f(unitID)
+        for _, new_obj in ipairs(new_objs) do
+            new_obj.x, new_obj.y, new_obj.z = x, y, z
+            new_obj.x_norm, new_obj.y_norm = x_norm, y_norm
+            local rx, ry = new_obj.radius / Game.mapSizeX, new_obj.radius / Game.mapSizeZ
+            new_obj.rx_norm, new_obj.ry_norm = rx, ry
+            out_obj[name][#out_obj[name] + 1] = new_obj
+        end
+    end
+end
+
+function HeatmapManager:Update()
+    -- Parse the queried units
+    local units = spGetAllUnits()
+
+    local first_unit = self.current_unit
+    local last_unit = math.min(first_unit + self.units_per_timestep - 1, #units)
+    local heat_objs = {}
+    for name, _ in pairs(self.heatmaps) do
+        heat_objs[name] = {}
+    end
+    for i = first_unit, last_unit do
+        local unitID = units[i]
+        if spValidUnitID(unitID) then
+            self:__ParseUnit(unitID, heat_objs)
+        end
+    end
+
+    -- Apply the collected heat footprints
+    for name, heatmap in pairs(self.heatmaps) do
+        heatmap:Draw(heat_objs[name])
+    end
+
+    -- Check if the job is done
+    self.current_unit = last_unit + 1
+    if self.current_unit > #units then
+        self.current_unit = 1
+        for _, heatmap in pairs(self.heatmaps) do
+            heatmap:SwapBuffer()
+        end
+    end
+end
+
+--- API ------------------------------------------------------------------------
+
+function gadget:Initialize()
+    SetupCmdChangeAIDebug()
+
+    heatmaps_manager = HeatmapManager:Create()
+    GG.HeatmapManager = heatmaps_manager
+end
+
+function gadget:Shutdown()
+    heatmaps_manager:Destroy()
+    GG.HeatmapManager = nil
+end
+
+function gadget:Update()
+    -- heatmaps_manager:Update()
+end
+
+function gadget:DrawGenesis()
+    heatmaps_manager:Update()
+end
+
 function gadget:DrawScreen()
-    if HEATMAPS_DEBUG == nil or heatmaps[HEATMAPS_DEBUG] == nil then
-        HEATMAPS_DEBUG = nil
+    if HEATMAPS_DEBUG == nil then
         return
     end
-    local heatmap = heatmaps[HEATMAPS_DEBUG]
+    local heatmap = heatmaps_manager:GetHeatmap(HEATMAPS_DEBUG)
 
     local aspect = heatmap.sy / heatmap.sx
-    local ww, wh = Spring.GetWindowGeometry()
+    local ww, wh = spGetWindowGeometry()
     local sx = HEATMAPS_DEBUG_SIZE * ww
     local sy = sx * aspect
     if sy > HEATMAPS_DEBUG_SIZE * wh then
@@ -342,10 +362,10 @@ function gadget:DrawScreen()
         sx = sy / aspect
     end
 
-    gl.Texture(heatmap.textures[heatmap.current_texture])
+    glTexture(heatmap:GetTexture())
     -- gl.Texture(heatmap.pump_tex)
-    gl.TexRect(0, 0, sx, sy, false, true)
-    gl.Texture(false)
+    glTexRect(0, 0, sx, sy, false, true)
+    glTexture(false)
 end
 
 end
--- LuaRules/Gadgets/craig/heatmap.lua
+++ LuaRules/Gadgets/craig/heatmap.lua
@@ -12,10 +12,15 @@ function HeatmapMgr.GameFrame(f)
 function HeatmapMgr.UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
 ]]--
 
+local spGetUnitDefID = Spring.GetUnitDefID
+local spGetUnitIsDead = Spring.GetUnitIsDead
+local spGetUnitAllyTeam = Spring.GetUnitAllyTeam
+
 function CreateHeatmapMgr(myTeamID, myAllyTeamID, Log)
 
 local UNITS_PER_FRAME = 5
-local F_D, F_P, F_A = 1e-8, 1.0 / 50.0, 1e-8
+-- local F_D, F_P, F_A = 1e-8, 1.0 / 50.0, 1e-8
+local F_D, F_P, F_A = 1e-7, 0.2, 1e-7
 local F_H = 2e-9
 local HeatmapMgr = {}
 
@@ -28,22 +33,26 @@ end
 local units = {}
 local intelligence
 
-local function parse_unit_heat(unitID, unitDefID)
-    local unitDef = UnitDefs[unitDefID]
-    local x, y, z = Spring.GetUnitPosition(unitID)
+local function parse_unit(unitID)
     local heats = {}
-
-    if #unitDef.weapons == 0 then
-        -- Nothing interesting for this unit
+    if spGetUnitIsDead(unitID) then
         return heats
     end
 
-    -- In the red channel we have the firepower, which is a combination of the
-    -- potential damage, d, the penetration, p, the reloading time, t, and the
-    -- inaccuracy, a:
+    local unitDefID = spGetUnitDefID(unitID)
+    local unitDef = UnitDefs[unitDefID]
+    if  #unitDef.weapons == 0 then
+        return heats
+    end
+
+    -- Ask intelligence if the unit can be parsed
+    -- ...
+
+    -- Get the firepower, which is a combination of the potential damage, d,
+    -- the penetration, p, the reloading time, t, and the inaccuracy, a:
     --   firepower = sqrt(F_D * (1 + F_P * p) * d / t - F_A * a)
     -- We need to create a heat source for each weapon
-    local unit_radius = 1
+    local allied = spGetUnitAllyTeam(unitID) == myAllyTeamID
     for i = 1, #unitDef.weapons do
         local name = tostring(unitID) .. "." .. tostring(i)
         local d = Spring.GetUnitWeaponDamages(
@@ -56,33 +65,15 @@ local function parse_unit_heat(unitID, unitDefID)
         local t = weaponDef.reload / (weaponDef.salvoSize * weaponDef.projectiles)
         local a = weaponDef.accuracy
         local radius = weaponDef.range
-        if radius > unit_radius then
-            unit_radius = radius
+
+        local firepower = math.sqrt(F_D * (1 + F_P * p) * d / t - F_A * a)
+
+        heats[#heats + 1] = {radius = radius}
+        if allied then
+            heats[#heats].color = {r = 0.0, g = firepower, b = 0.0, a = 0.0}
+        else
+            heats[#heats].color = {r = firepower, g = 0.0, b = 0.0, a = 0.0}
         end
-        local r = math.sqrt(F_D * (1 + F_P * p) * d / t - F_A * a)
-        heats[name] = {x = x, z = z, radius = radius,
-                       r = r, g = 0.0, b = 0.0, a = 0.0}
-    end
-
-    -- In the green channel we are storing the firepower to vanquish the unit,
-    -- as a function of the health points, h, and armors, af, as, ar
-    --   resistance = sqrt(F_H * (1 + F_P * (af + as + ar) / 3) * h)
-    local name = tostring(unitID) .. ".health"
-    local af = unitDef.customParams.armor_front or 0
-    local as = unitDef.customParams.armor_side or 0
-    local ar = unitDef.customParams.armor_rear or 0
-    local a = (af + as + ar) / 3.0
-    local h = Spring.GetUnitHealth(unitID)
-    local g = math.sqrt(F_H * (1 + F_P * a) * h)
-    heats[name] = {x = x, z = z, radius = unit_radius,
-                   r = 0.0, g = g, b = 0.0, a = 0.0}
-
-    -- In the blue channel we are storing the LOS
-    if Spring.GetUnitAllyTeam(unitID) == myAllyTeamID then
-        local name = tostring(unitID) .. ".los"
-        heats[name] = {x = x, z = z,
-                       radius = Spring.GetUnitSensorRadius(unitID, "los"),
-                       r = 0.0, g = 0.0, b = 1.0, a = 0.0}
     end
 
     return heats
@@ -98,57 +89,14 @@ function HeatmapMgr.GameStart()
     -- Try to create a Heatmap. This operation would fail if some other allied
     -- team already did the job
     HeatmapMgr.hmap_name = "craig." .. tostring(myAllyTeamID) .. ".ground"
-    GG.CreateAIHeatmap(HeatmapMgr.hmap_name, 64, 1.0, 1.0)
+    GG.HeatmapManager:AddHeatmap(HeatmapMgr.hmap_name, parse_unit, 64)
     intelligence = gadget.intelligences[myAllyTeamID]
 end
 
-local first_unit = 1
-
 function HeatmapMgr.GameFrame(f)
-    local parsing = {}
-
-    local team_units = Spring.GetTeamUnits(myTeamID)
-    if first_unit > #team_units then
-        first_unit = 1
-    end
-    local last_unit = math.min(first_unit + UNITS_PER_FRAME - 1, #team_units)
-    for i = first_unit, last_unit do
-        parsing[#parsing + 1] = team_units[i]
-    end
-
-    for _, unitID in ipairs(intelligence.GetUnits(UNITS_PER_FRAME)) do
-        parsing[#parsing + 1] = unitID
-    end
-
-    for _, unitID in ipairs(parsing) do
-        if Spring.ValidUnitID(unitID) and not Spring.GetUnitIsDead(unitID) then
-            local unitDefID = Spring.GetUnitDefID(unitID)
-            local heats = parse_unit_heat(unitID, unitDefID)
-            units[unitID] = heats
-            for name, heat in pairs(heats) do
-                GG.SetAIHeatmapPump(HeatmapMgr.hmap_name,
-                                    name,
-                                    heat.x, heat.z,
-                                    {r=heat.r, g=heat.g, b=heat.b, a=heat.a},
-                                    heat.radius)
-            end
-        end
-    end
-
-    first_unit = last_unit + 1
 end
 
 function HeatmapMgr.UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
-    if units[unitID] == nil then
-        return
-    end
-
-    for name, _ in pairs(units[unitID]) do
-        -- This opeartion may potentially fail since several teams handle the
-        -- same heatmap. We are just simply not checking for eventual errors
-        GG.UnsetAIHeatmapPump(HeatmapMgr.hmap_name, name)
-    end
-    units[unitID] = nil
 end
 
 --------------------------------------------------------------------------------
