846e9a2c19d74b5f8791df79ce128249294fb224
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2020-11-24 11:53:18 +0100

    Replace tabs

--- LuaUI/Widgets/cmd_defaultCommand.lua
+++ LuaUI/Widgets/cmd_defaultCommand.lua
@@ -1,13 +1,13 @@
 function widget:GetInfo()
-	return {
-		name = "1944 Default Commands",
-		desc = "Allows using the rightclick for some commands",
-		author = "KDR_11k (David Becker), Craig Lawrence",
-		date = "2008-06-24",
-		license = "Public Domain",
-		layer = 1,
-		enabled = true
-	}
+    return {
+        name = "1944 Default Commands",
+        desc = "Allows using the rightclick for some commands",
+        author = "KDR_11k (David Becker), Craig Lawrence",
+        date = "2008-06-24",
+        license = "Public Domain",
+        layer = 1,
+        enabled = true
+    }
 end
 
 local CMD_FIGHT = CMD.FIGHT
@@ -16,50 +16,50 @@ local CMD_AREA_ATTACK = 39954 -- as set in areaattack.lua gadget
 local defCom = {}
 
 function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-	if (not defCom[unitDefID]) then
-		local ud = UnitDefs[unitDefID]
-		-- mobile combat units except SPGs, AT guns, etc
-		--if (ud.speed > 0 and ud.canAttack and not ud.customParams.defaultmove) then
-		--	defCom[unitDefID] = CMD_FIGHT
-		-- Deployed howitzers with area attack
-		--[[else]]if (ud.speed == 0 and ud.customParams.canareaattack) then
-				defCom[unitDefID] = CMD_AREA_ATTACK
-		-- Deployed AT and AA guns
-		elseif (ud.speed == 0 and ud.canAttack and not ud.customParams.canareaattack and not ud.isBuilder) then
-				defCom[unitDefID] = CMD_ATTACK
-		end
-	end
+    if (not defCom[unitDefID]) then
+        local ud = UnitDefs[unitDefID]
+        -- mobile combat units except SPGs, AT guns, etc
+        --if (ud.speed > 0 and ud.canAttack and not ud.customParams.defaultmove) then
+        --    defCom[unitDefID] = CMD_FIGHT
+        -- Deployed howitzers with area attack
+        --[[else]]if (ud.speed == 0 and ud.customParams.canareaattack) then
+                defCom[unitDefID] = CMD_AREA_ATTACK
+        -- Deployed AT and AA guns
+        elseif (ud.speed == 0 and ud.canAttack and not ud.customParams.canareaattack and not ud.isBuilder) then
+                defCom[unitDefID] = CMD_ATTACK
+        end
+    end
 end
 
 function widget:Initialize()
-	WG.activeCommand=0 -- needed??
+    WG.activeCommand=0 -- needed??
 end
 
 function widget:DefaultCommand()
-	local type = false
-	for _,u in ipairs(Spring.GetSelectedUnits()) do
-		local unitDefCom = defCom[Spring.GetUnitDefID(u)]
-		if unitDefCom and type == false then
-			-- only default to fight for groups over 5
-			--[[if unitDefCom == CMD_FIGHT then 
-				if Spring.GetSelectedUnitsCount() >= 6 then
-					local mx, my = Spring.GetMouseState()
-					local s,t = Spring.TraceScreenRay(mx, my)
-					-- apply ATTACK if cursor over a unit
-					if s == "unit" then
-						type = CMD_ATTACK
-					-- apply default otherwise
-					else
-						type=unitDefCom
-					end
-				end
-			-- other default commands should always be applied
-			else]]
-				type=unitDefCom
-			--end
-		elseif type ~= unitDefCom then
-			type=nil
-		end
-	end
-	return type
+    local type = false
+    for _,u in ipairs(Spring.GetSelectedUnits()) do
+        local unitDefCom = defCom[Spring.GetUnitDefID(u)]
+        if unitDefCom and type == false then
+            -- only default to fight for groups over 5
+            --[[if unitDefCom == CMD_FIGHT then 
+                if Spring.GetSelectedUnitsCount() >= 6 then
+                    local mx, my = Spring.GetMouseState()
+                    local s,t = Spring.TraceScreenRay(mx, my)
+                    -- apply ATTACK if cursor over a unit
+                    if s == "unit" then
+                        type = CMD_ATTACK
+                    -- apply default otherwise
+                    else
+                        type=unitDefCom
+                    end
+                end
+            -- other default commands should always be applied
+            else]]
+                type=unitDefCom
+            --end
+        elseif type ~= unitDefCom then
+            type=nil
+        end
+    end
+    return type
 end
