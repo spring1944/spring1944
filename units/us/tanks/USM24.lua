4045e737c8a64e868c378f54299e24f40760dd43
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2021-01-16 10:54:59 +0100

    US24 (to be used in the future). Frontal MG is not working

---
+++ units/us/tanks/USM24.lua
@@ -0,0 +1,75 @@
+local USM24 = LightTank:New{
+	name				= "M24 Chaffee",
+	buildCostMetal		= 2400,
+	maxDamage			= 1200,
+	acceleration		= 0.054,
+	trackOffset			= 5,
+	trackWidth			= 18,
+	trackType			= "USStuart",
+
+	weapons = {
+		[1] = {
+			name				= "M375mmAP",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[2] = {
+			name				= "M375mmHE",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[3] = {
+			name				= "M1919A4Browning",
+		},
+		[4] = {
+			name				= ".50calproof",
+		},
+	},
+
+	customParams = {
+		armour = {
+			base = {
+				front = {
+					thickness		= 25,
+					slope			= 59,
+				},
+				rear = {
+					thickness		= 19,
+					slope			= -40,
+				},
+				side = {
+					thickness 		= 19,
+				},
+				top = {
+					thickness		= 13,
+				},
+			},
+			turret = {
+				front = {
+					thickness		= 25, -- not mantlet
+					slope			= 13,
+				},
+				rear = {
+					thickness		= 25,
+					slope			= 2,
+				},
+				side = {
+					thickness 		= 25,
+					slope			= 10,
+				},
+				top = {
+					thickness		= 13,
+				},
+			},
+		},
+
+		maxvelocitykmh		= 56,
+		maxammo				= 20,
+		turretturnspeed		= 30.0, -- 12s for 360
+
+	},
+}
+
+return lowerkeys({
+	["USM24"] = USM24,
+})
